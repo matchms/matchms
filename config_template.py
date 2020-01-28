4aaecfd7ca251f092515ec496988658ca0c8c5cc
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-01-28 14:34:13 +0100

    moved paths variables to config.py and added os.path.join to notebooks

---
+++ config_template.py
@@ -0,0 +1,9 @@
+# config.py template
+# Add the respective folder names here and save as config.py
+
+ROOT = "..."
+PATH_SPEC2VEC = ROOT + "..."
+PATH_MS_DATA = ROOT + "..."
+PATH_SAVE_MODEL = ROOT + "..."
+PATH_COMPUTED = ROOT + "..."
+PATH_OUTPUT = ROOT + "..."
