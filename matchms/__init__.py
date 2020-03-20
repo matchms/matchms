19e059f15ba8292b8fa3e6da7f52409eb0db9b44
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-03-20 12:02:50 +0100

    added module exports as they were in spec2vec

--- matchms/__init__.py
+++ matchms/__init__.py
@@ -2,6 +2,14 @@
 """Documentation about matchms"""
 import logging
 
+from .helper_functions import *
+from .MS_functions import *
+from .MS_library_search import *
+from .MS_similarity_classical import *
+from .networking import *
+from .plotting_functions import *
+from .similarity_measure import *
+
 from .__version__ import __version__
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
