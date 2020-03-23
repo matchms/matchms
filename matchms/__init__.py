989a7a4e20f3ab52375185a4acf69716243e81ed
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-03-23 20:37:10 +0100

    Update __init__.py

--- matchms/__init__.py
+++ matchms/__init__.py
@@ -2,13 +2,13 @@
 """Documentation about matchms"""
 import logging
 
-from .helper_functions import *
-from .MS_functions import *
-from .MS_library_search import *
-from .MS_similarity_classical import *
-from .networking import *
-from .plotting_functions import *
-from .similarity_measure import *
+from matchms import helper_functions
+from matchms import MS_functions
+from matchms import MS_library_search
+from matchms import MS_similarity_classical
+from matchms import networking
+from matchms import plotting_functions
+from matchms import similarity_measure
 
 from .__version__ import __version__
 
