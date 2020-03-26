384a79561502561ce6384096ea6de21c409d2ac0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-03-26 19:30:53 +0100

    everything in one namespace

--- matchms/__init__.py
+++ matchms/__init__.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 """Documentation about matchms"""
 
-import matchms.helper_functions
-import matchms.ms_functions
-import matchms.ms_library_search
-import matchms.ms_similarity_classical
-import matchms.networking
-import matchms.plotting_functions
-import matchms.similarity_measure
-
-from .similarity_measure import SimilarityMeasures
+from .helper_functions import *
+from .ms_functions import *
+from .ms_library_search import *
+from .ms_similarity_classical import *
+from .networking import *
+from .plotting_functions import *
+from .similarity_measure import *
+from .similarity_measure import *
 
 from .__version__ import __version__
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-# remove unnecessary members
-# del logging
 
 __author__ = "Netherlands eScience Center"
 __email__ = 'generalization@esciencecenter.nl'
