b5fe1bfe7a1211e0c6638f68fa25ecc655722d15
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-03-26 17:44:16 +0100

    removed matchms.py since we already have __init__.py

--- matchms/matchms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-"""Documentation about the matchms module."""
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-# FIXME: put actual code here
-def example():
-    logger.info('Providing information about the excecution of the function.')
