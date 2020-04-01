dfa7715341c7bf8c157b3dc2f131c8142c9addba
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-01 19:22:53 +0200

    removed plotting for the moment, dependencies problems

--- matchms/Spectrum.py
+++ matchms/Spectrum.py
@@ -1,8 +1,5 @@
 # file: matchms/Spectrum.py
 
-from spectrum_utils.spectrum import MsmsSpectrum
-from spectrum_utils.plot import spectrum as plot
-
 class Spectrum:
 
     def __init__(self, pyteomics_spectrum):
@@ -10,11 +7,3 @@ class Spectrum:
         self.mz = pyteomics_spectrum["m/z array"]
         self.intensities = pyteomics_spectrum["intensity array"]
         self.something_from_the_metadata = pyteomics_spectrum.get("params", None).get("something_from_the_metadata", None)
-
-    def plot(self):
-
-        s = MsmsSpectrum(identifier=None,
-                         precursor_mz=None,
-                         precursor_charge=None,
-                         mz=self.mz, intensity=self.intensities)
-        plot(s)
