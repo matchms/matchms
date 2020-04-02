4ded135f5a9b18d336ca1635a8d964b773176070
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-02 10:08:55 +0200

    add first test for Spectrum class (and/or load_mgf_file)

---
+++ tests/test_Spectrum.py
@@ -0,0 +1,26 @@
+# test functions
+
+import os
+import unittest
+
+from matchms.load_mgf_file import load_mgf_file
+
+# Use test data from following folder
+PATH_TEST = os.path.dirname(os.path.abspath(__file__))
+PATH_TESTDATA = os.path.join(PATH_TEST, 'testdata')
+
+class ModelGenerationSuite(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_load_mgf_data(self):
+        test_mgf_file = os.path.join(PATH_TESTDATA,
+                                     'GNPS-COLLECTIONS-PESTICIDES-NEGATIVE.mgf')
+        spectra = load_mgf_file(test_mgf_file)
+        # Test size and types
+        assert len(spectra) == len(spec_dict) == len(ms_docs) == 76, 'number of load spectra not correct'
+        assert type(spectra[0]) == Spectrum.Spectrum, 'expected list of Spectrum.Spectrum objects'
+        assert spectra[0].metadata['smiles'] == 'C1=CC=C2C(=C1)NC(=N2)C3=CC=CO3', 'expected other smiles entry'
+        assert spectra[-1].precursor_mz == 342.024
+
+if __name__ == '__main__':
+    unittest.main()
