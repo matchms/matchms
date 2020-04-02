3eab7c55dc2ba80553921203ebfeadeeeb877c19
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-02 11:56:41 +0200

    even more import/export

--- matchms/__init__.py
+++ matchms/__init__.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """Documentation about matchms"""
 
+from .calculate_scores import calculate_scores
+from .Scores import Scores
+from .Spectrum import Spectrum
+
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Netherlands eScience Center"
 __email__ = 'generalization@esciencecenter.nl'
+__all__ = [
+    "exporting",
+    "harmonization",
+    "importing",
+    "similarity",
+    "calculate_scores",
+    "Scores",
+    "Spectrum"
+]
