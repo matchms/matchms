17834d6cdf2af1e423240c588460ac4949c4e782
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-03 15:26:24 +0200

    Spectrum changed its constructor, therefore this needed to be updated as well

--- matchms/importing/load_from_mgf.py
+++ matchms/importing/load_from_mgf.py
@@ -4,4 +4,14 @@ from matchms.Spectrum import Spectrum
 
 def load_from_mgf(filename):
 
-    return [Spectrum(s) for s in list(MGF(filename, convert_arrays=1))]
+    spectrums = list()
+    for pyteomics_spectrum in MGF(filename, convert_arrays=1):
+
+        metadata = pyteomics_spectrum.get("params", None)
+        mz = pyteomics_spectrum["m/z array"]
+        intensities = pyteomics_spectrum["intensity array"]
+
+        spectrum = Spectrum(mz=mz, intensities=intensities, metadata=metadata)
+        spectrums.append(spectrum)
+
+    return spectrums
