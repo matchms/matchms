0609c67960008c9fedf50b45bd65215b7b175747
Faruk D <f.diblen@esciencecenter.nl>
2020-04-03 16:23:51 +0200

    - fix build command - clean up and organize conda dependencies in meta.yml - pin Python version to 3.7 in meta.yml - moved pytest and sphinx from requirements in setup.py

--- setup.py
+++ setup.py
@@ -35,26 +35,28 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
-    install_requires=[],  # FIXME: add your package's dependencies to this list
-    setup_requires=[
-        # dependency for `python setup.py test`
-        'pytest-runner',
-        # dependencies for `python setup.py build_sphinx`
-        'sphinx',
-        'sphinx_rtd_theme',
-        'recommonmark'
+    install_requires=[
     ],
+    setup_requires=[],
     tests_require=[
         'pytest',
         'pytest-cov',
         'pycodestyle',
     ],
     extras_require={
-        'dev':  ['prospector[with_pyroma]', 'yapf', 'isort'],
+        'dev':  [
+            'prospector[with_pyroma]',
+            'yapf',
+            'isort',
+            # dependency for `python setup.py test`
+            'pytest-runner',
+            # dependencies for `python setup.py build_sphinx`
+            'sphinx',
+            'sphinx_rtd_theme',
+            'recommonmark'
+        ]
     }
 )
