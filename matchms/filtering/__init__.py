b2d73991ff71fa5320d4e905620cca661a2191bc
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-06 14:55:59 +0200

    minor bug fix

--- matchms/filtering/__init__.py
+++ matchms/filtering/__init__.py
@@ -2,4 +2,4 @@ from .interpret_charge_mode import interpret_charge_mode
 from .filterfun1 import filterfun1
 from .filterfun2 import filterfun2
 
-__all__ = ["interpret_charge_mode", "interpret_charge", "filterfun1", "filterfun2"]
+__all__ = ["interpret_charge_mode", "filterfun1", "filterfun2"]
