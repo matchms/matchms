0700d8733c9e7aa69d85a62cdf7b21a1d561f63b
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-06 11:13:05 +0200

    layed out simplified spec2vec similarity module (incomplete)

---
+++ matchms/similarity/spec2vec/make_model.py
@@ -0,0 +1,5 @@
+# from spec2vec import Sentence
+
+# sentences = [Sentence(s) for s in spectrums]
+# construct the corpus out of the sentences
+# model = gensim.train(corpus)
---
+++ matchms/similarity/spec2vec/similarity/Spec2Vec.py
@@ -0,0 +1,11 @@
+class Spec2Vec:
+
+    def __init__(self, model=None):
+        self.model = model
+
+    def __call__(self, sentence, reference_sentence):
+
+        similarity = None # replace with actual calculation based
+                          # on sentence, reference_sentence, and self.model
+
+        return similarity
