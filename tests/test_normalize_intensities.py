396c0cc6a8736957a66fbda27d5624adb5236827
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-08 17:44:00 +0200

    avoid line that is too long

--- tests/test_normalize_intensities.py
+++ tests/test_normalize_intensities.py
@@ -12,7 +12,8 @@ def test_normalize_intensities():
     normalize_intensities(spectrum)
 
     assert max(spectrum.intensities) == 1.0, "Expected the spectrum to be scaled to 1.0."
-    assert numpy.array_equal(spectrum.mz, mz), "Expected the spectrum's intensity locations along the mz-axis to be unaffected."
+    assert numpy.array_equal(spectrum.mz, mz), "Expected the spectrum's intensity locations along "\
+                                               "the mz-axis to be unaffected."
 
 
 if __name__ == '__main__':
