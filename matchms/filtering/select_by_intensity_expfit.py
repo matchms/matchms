0b84f5c2e7c3a202baad9928b9c9b968fdcadc0e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-08 18:39:48 +0200

    partial

---
+++ matchms/filtering/select_by_intensity_expfit.py
@@ -0,0 +1,10 @@
+def select_by_intensity_expfit(spectrum, aggregation_strategy=None):
+
+    if aggregation_strategy is None:
+        pass
+    elif aggregation_strategy == "select_max_intensity":
+        pass
+    elif aggregation_strategy == "sum_itensities":
+        pass
+    else:
+        pass
---
+++ tests/test_select_by_intensity_expfit.py
@@ -0,0 +1,6 @@
+def test_select_by_intensity_expfit():
+    pass
+
+
+if __name__ == "__main__":
+    test_select_by_intensity_expfit()
