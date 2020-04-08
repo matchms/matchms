c0bda51d3630a84334520afa4e60acb6144b2014
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-08 15:58:52 +0200

    max function now from numpy; removed return to signify this function alters objects in-place

--- matchms/filtering/normalize_intensities.py
+++ matchms/filtering/normalize_intensities.py
@@ -1,5 +1,7 @@
+import numpy
+
+
 def normalize_intensities(spectrum):
     """Normalize intensities to unit height."""
-    scale_factor = max(spectrum.intensities)
+    scale_factor = numpy.max(spectrum.intensities)
     spectrum.intensities = spectrum.intensities / scale_factor
-    return spectrum
