fe24989dc62e143df0d17a21909183122a3c4c7f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-08 18:20:14 +0200

    fas

--- matchms/filtering/__init__.py
+++ matchms/filtering/__init__.py
@@ -1,4 +1,9 @@
 from .filterfun1 import filterfun1
 from .filterfun2 import filterfun2
+from .select_by_intensity_expfit import select_by_intensity_expfit
 
-__all__ = ["filterfun1", "filterfun2"]
+__all__ = [
+    "filterfun1",
+    "filterfun2",
+    "select_by_intensity_expfit"
+    ]
