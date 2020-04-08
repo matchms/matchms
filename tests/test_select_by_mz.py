d19c465cdcce68e643f29d6ec1497bdeb9957562
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-08 19:49:01 +0200

    changed test function names; sorted test functions

--- tests/test_select_by_mz.py
+++ tests/test_select_by_mz.py
@@ -3,7 +3,7 @@ from matchms.filtering import select_by_mz
 import numpy
 
 
-def test_select_by_mz_no_bounds():
+def test_select_by_mz_no_parameters_1():
 
     mz = numpy.array([10, 20, 30, 40], dtype="float")
     intensities = numpy.array([1, 10, 100, 1000], dtype="float")
@@ -17,49 +17,7 @@ def test_select_by_mz_no_bounds():
     assert numpy.array_equal(spectrum.intensities, numpy.array([1, 10, 100, 1000], dtype="float"))
 
 
-def test_select_by_mz_lower_bound_only():
-
-    mz = numpy.array([10, 20, 30, 40], dtype="float")
-    intensities = numpy.array([1, 10, 100, 1000], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities, metadata=dict())
-
-    select_by_mz(spectrum, mz_from=15.0)
-
-    assert len(spectrum.mz) == 3
-    assert len(spectrum.mz) == len(spectrum.intensities)
-    assert numpy.array_equal(spectrum.mz, numpy.array([20, 30, 40], dtype="float"))
-    assert numpy.array_equal(spectrum.intensities, numpy.array([10, 100, 1000], dtype="float"))
-
-
-def test_select_by_mz_upper_bound_only():
-
-    mz = numpy.array([10, 20, 30, 40], dtype="float")
-    intensities = numpy.array([1, 10, 100, 1000], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities, metadata=dict())
-
-    select_by_mz(spectrum, mz_to=35.0)
-
-    assert len(spectrum.mz) == 3
-    assert len(spectrum.mz) == len(spectrum.intensities)
-    assert numpy.array_equal(spectrum.mz, numpy.array([10, 20, 30], dtype="float"))
-    assert numpy.array_equal(spectrum.intensities, numpy.array([1, 10, 100], dtype="float"))
-
-
-def test_select_by_mz_lower_and_upper_bound():
-
-    mz = numpy.array([10, 20, 30, 40], dtype="float")
-    intensities = numpy.array([1, 10, 100, 1000], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities, metadata=dict())
-
-    select_by_mz(spectrum, mz_from=15.0, mz_to=35.0)
-
-    assert len(spectrum.mz) == 2
-    assert len(spectrum.mz) == len(spectrum.intensities)
-    assert numpy.array_equal(spectrum.mz, numpy.array([20, 30], dtype="float"))
-    assert numpy.array_equal(spectrum.intensities, numpy.array([10, 100], dtype="float"))
-
-
-def test_select_by_mz_no_bounds_2():
+def test_select_by_mz_no_parameters_2():
 
     mz = numpy.array([998, 999, 1000, 1001, 1002], dtype="float")
     intensities = numpy.array([1, 10, 100, 1000, 10000], dtype="float")
@@ -73,9 +31,51 @@ def test_select_by_mz_no_bounds_2():
     assert numpy.array_equal(spectrum.intensities, numpy.array([1, 10, 100], dtype="float"))
 
 
+def test_select_by_mz_with_from_parameter():
+
+    mz = numpy.array([10, 20, 30, 40], dtype="float")
+    intensities = numpy.array([1, 10, 100, 1000], dtype="float")
+    spectrum = Spectrum(mz=mz, intensities=intensities, metadata=dict())
+
+    select_by_mz(spectrum, mz_from=15.0)
+
+    assert len(spectrum.mz) == 3
+    assert len(spectrum.mz) == len(spectrum.intensities)
+    assert numpy.array_equal(spectrum.mz, numpy.array([20, 30, 40], dtype="float"))
+    assert numpy.array_equal(spectrum.intensities, numpy.array([10, 100, 1000], dtype="float"))
+
+
+def test_select_by_mz_with_to_parameter():
+
+    mz = numpy.array([10, 20, 30, 40], dtype="float")
+    intensities = numpy.array([1, 10, 100, 1000], dtype="float")
+    spectrum = Spectrum(mz=mz, intensities=intensities, metadata=dict())
+
+    select_by_mz(spectrum, mz_to=35.0)
+
+    assert len(spectrum.mz) == 3
+    assert len(spectrum.mz) == len(spectrum.intensities)
+    assert numpy.array_equal(spectrum.mz, numpy.array([10, 20, 30], dtype="float"))
+    assert numpy.array_equal(spectrum.intensities, numpy.array([1, 10, 100], dtype="float"))
+
+
+def test_select_by_mz_with_from_and_to_parameters():
+
+    mz = numpy.array([10, 20, 30, 40], dtype="float")
+    intensities = numpy.array([1, 10, 100, 1000], dtype="float")
+    spectrum = Spectrum(mz=mz, intensities=intensities, metadata=dict())
+
+    select_by_mz(spectrum, mz_from=15.0, mz_to=35.0)
+
+    assert len(spectrum.mz) == 2
+    assert len(spectrum.mz) == len(spectrum.intensities)
+    assert numpy.array_equal(spectrum.mz, numpy.array([20, 30], dtype="float"))
+    assert numpy.array_equal(spectrum.intensities, numpy.array([10, 100], dtype="float"))
+
+
 if __name__ == '__main__':
-    test_select_by_mz_lower_bound_only()
-    test_select_by_mz_upper_bound_only()
-    test_select_by_mz_lower_and_upper_bound()
-    test_select_by_mz_no_bounds()
-    test_select_by_mz_no_bounds_2()
+    test_select_by_mz_no_parameters_1()
+    test_select_by_mz_no_parameters_2()
+    test_select_by_mz_with_from_parameter()
+    test_select_by_mz_with_to_parameter()
+    test_select_by_mz_with_from_and_to_parameters()
