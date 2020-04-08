1810f67696784e8fb1facbfde48fe05b2b5341ae
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-08 19:23:00 +0200

    forgot a comma

--- matchms/filtering/__init__.py
+++ matchms/filtering/__init__.py
@@ -3,7 +3,7 @@ from .select_by_intensity import select_by_intensity
 from .select_by_mz import select_by_mz
 
 __all__ = [
-    "normalize_intensities"
+    "normalize_intensities",
     "select_by_intensity",
     "select_by_mz"
 ]
