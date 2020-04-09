93d3ccf6ead695e50cf3a2e541baceec05c72ec3
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-09 13:05:44 +0200

    added select by relative intensity filter; added tests

--- matchms/filtering/__init__.py
+++ matchms/filtering/__init__.py
@@ -1,5 +1,11 @@
 from .filterfun1 import filterfun1
 from .filterfun2 import filterfun2
 from .normalize_intensities import normalize_intensities
+from .select_by_relative_intensity import select_by_relative_intensity
 
-__all__ = ["filterfun1", "filterfun2", "normalize_intensities"]
+__all__ = [
+    "filterfun1",
+    "filterfun2",
+    "normalize_intensities",
+    "select_by_relative_intensity"
+]
