c7d1b5e3c1941717739d6d333c3f4a4731fe163a
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-14 16:57:20 +0200

    minor bug fix in test

--- tests/test_add_parent_mass.py
+++ tests/test_add_parent_mass.py
@@ -7,7 +7,7 @@ def test_add_parent_mass():
     """Test if parent mass is correctly derived."""
     mz = np.array([10, 20, 30, 40], dtype='float')
     intensities = np.array([0, 1, 10, 100], dtype='float')
-    metadata = {"pepmass": 444.0,
+    metadata = {"pepmass": (444.0, 10),
                 "charge": -1}
     spectrum_in = Spectrum(mz=mz,
                            intensities=intensities,
@@ -15,7 +15,7 @@ def test_add_parent_mass():
 
     spectrum = add_parent_mass(spectrum_in)
 
-    assert np.abs(spectrum.get("parent_mass") - 445.0) < .001, "Expected parent mass of about 445.0."
+    assert np.abs(spectrum.get("parent_mass") - 445.0) < .01, "Expected parent mass of about 445.0."
 
 
 if __name__ == '__main__':
