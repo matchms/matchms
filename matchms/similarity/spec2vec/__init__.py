0fb2d69b0707d7679d20a90a4dfde3561aa66ea0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-16 10:10:22 +0200

    removed stubs for functions that we won't use anyway

--- matchms/similarity/spec2vec/__init__.py
+++ matchms/similarity/spec2vec/__init__.py
@@ -1,5 +1,3 @@
-from .build_model_lda import build_model_lda
-from .build_model_lsi import build_model_lsi
 from .build_model_word2vec import build_model_word2vec
 from .convert_spectrum_to_document import convert_spectrum_to_document
 from .Document import Document
@@ -7,8 +5,6 @@ from .Spec2Vec import Spec2Vec
 
 
 __all__ = [
-    "build_model_lda",
-    "build_model_lsi",
     "build_model_word2vec",
     "convert_spectrum_to_document",
     "Document",
--- matchms/similarity/spec2vec/build_model_lda.py
+++
@@ -1,5 +0,0 @@
-# https://github.com/matchms/old-iomega-spec2vec/blob/216b8f8b5e4ffd320b4575326a05fb6c7cd28223/matchms/old/similarity_measure.py#L284-L325
-
-
-def build_model_lda():
-    pass
