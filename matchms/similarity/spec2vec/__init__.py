502b867a894deebb691c7243634981ffcbe5acea
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-16 19:08:00 +0200

    removed unnecessary file

--- matchms/similarity/spec2vec/__init__.py
+++ matchms/similarity/spec2vec/__init__.py
@@ -1,11 +1,9 @@
-from .build_model_word2vec import build_model_word2vec
 from .convert_spectrum_to_document import convert_spectrum_to_document
 from .Document import Document
 from .Spec2Vec import Spec2Vec
 
 
 __all__ = [
-    "build_model_word2vec",
     "convert_spectrum_to_document",
     "Document",
     "Spec2Vec"
