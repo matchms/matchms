e8d0a5cfd8cb7fed325a957cc0ae40fd5d6cb84e
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-16 14:29:57 +0200

    linting of test function

--- tests/test_clean_inchis.py
+++ tests/test_clean_inchis.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 from matchms import Spectrum
 from matchms.filtering import clean_inchis
@@ -8,8 +7,8 @@ def test_clean_inchis_misplaced_smiles():
     """Test if misplaced smiles are corrected.
     """
     spectrum_in = Spectrum(mz=np.array([10, 20, 30, 40], dtype='float'),
-                          intensities=np.array([0, 1, 10, 100], dtype='float'),
-                          metadata={"inchi": "C1CCCCC1"})
+                           intensities=np.array([0, 1, 10, 100], dtype='float'),
+                           metadata={"inchi": "C1CCCCC1"})
 
     spectrum = clean_inchis(spectrum_in)
     assert spectrum.get("inchi") == '"InChI=1S/C6H12/c1-2-4-6-5-3-1/h1-6H2"', "Expected different InChI"
@@ -19,12 +18,12 @@ def test_clean_inchis_harmonize_strings():
     """Test if inchi strings are made consistent in style.
     """
     spectrum_in1 = Spectrum(mz=np.array([10, 20, 30, 40], dtype='float'),
-                          intensities=np.array([0, 1, 10, 100], dtype='float'),
-                          metadata={"inchi": 'InChI=1S/C6H12'})
+                            intensities=np.array([0, 1, 10, 100], dtype='float'),
+                            metadata={"inchi": 'InChI=1S/C6H12'})
 
     spectrum_in2 = Spectrum(mz=np.array([10, 20, 30, 40], dtype='float'),
-                          intensities=np.array([0, 1, 10, 100], dtype='float'),
-                          metadata={"inchi": '1S/C6H12'})
+                            intensities=np.array([0, 1, 10, 100], dtype='float'),
+                            metadata={"inchi": '1S/C6H12'})
 
     spectrum1 = clean_inchis(spectrum_in1)
     spectrum2 = clean_inchis(spectrum_in2)
