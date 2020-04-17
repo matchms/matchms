a3fae290d5c63c3f8466a630de4165b34dc135f1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-17 18:25:40 +0200

    now importing proton mass constant from constants.py

--- matchms/filtering/add_parent_mass.py
+++ matchms/filtering/add_parent_mass.py
@@ -1,10 +1,12 @@
+from matchms.constants import PROTON_MASS
+
+
 def add_parent_mass(spectrum_in):
     """Add parentmass to metadata (if not present yet).
 
     Method to calculate the parent mass from given precursor mass
     and charge.
     """
-    proton_mass = 1.00727645199076  # TODO: where to put this constant?
     spectrum = spectrum_in.clone()
 
     if spectrum.get("parent_mass", None) is None:
@@ -12,7 +14,7 @@ def add_parent_mass(spectrum_in):
             int_charge = int(spectrum.get("charge"))
             precursor_mass = spectrum.get("pepmass")[0]
             parent_mass = precursor_mass * abs(int_charge)
-            parent_mass -= int_charge * proton_mass
+            parent_mass -= int_charge * PROTON_MASS
             if parent_mass:
                 spectrum.set("parent_mass", parent_mass)
         except KeyError:
