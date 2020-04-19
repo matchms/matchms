408af86849010452300485a7375f83f3332775c7
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-19 22:38:08 +0200

    add complete_compound_annotation to init

--- matchms/filtering/__init__.py
+++ matchms/filtering/__init__.py
@@ -1,6 +1,7 @@
 from .add_adduct import add_adduct
 from .add_parent_mass import add_parent_mass
 from .clean_inchis import clean_inchis
+from .complete_compound_annotation import complete_compound_annotation
 from .correct_charge import correct_charge
 from .derive_ionmode import derive_ionmode
 from .default_filters import default_filters
@@ -17,6 +18,7 @@ __all__ = [
     "add_adduct",
     "add_parent_mass",
     "clean_inchis",
+    "complete_compound_annotation",
     "correct_charge",
     "default_filters",
     "derive_ionmode",
