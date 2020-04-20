cc15141715542a4e30a42b9a410d6179714303d6
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-20 19:22:07 +0200

    replaced pytest's tmpdir with python's own tempfile

--- tests/test_save_as_mgf.py
+++ tests/test_save_as_mgf.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 from matchms import Spectrum
 from matchms.exporting import save_as_mgf
+import tempfile
 
 
-def test_save_as_mgf_single_spectrum(tmp_path):
+def test_save_as_mgf_single_spectrum():
     """Test saving spectrum to .mgf file"""
     spectrum = Spectrum(mz=np.array([100, 200, 300], dtype="float"),
                         intensities=np.array([10, 10, 500], dtype="float"),
@@ -13,17 +14,19 @@ def test_save_as_mgf_single_spectrum(tmp_path):
                                   "pepmass": (100, 10.0),
                                   "test_field": 'test'})
     # Write to test file
-    file = tmp_path.join('test.mgf')
-    save_as_mgf(spectrum, file)
-    assert os.path.isfile(file)
+    with tempfile.NamedTemporaryFile() as fp:
+        save_as_mgf(spectrum, fp.name)
 
-    # Test if content of mgf file is correct
-    with open(file, 'r') as f:
-        mgf_content = f.readlines()
-    assert mgf_content[0] == 'BEGIN IONS\n'
-    assert mgf_content[2] == 'CHARGE=1-\n'
-    assert mgf_content[4] == 'TEST_FIELD=test\n'
-    assert mgf_content[7].split(" ")[0] == '300.0'
+        # test if file exists
+        assert os.path.isfile(fp.name)
+
+        # Test if content of mgf file is correct
+        with open(fp.name, 'r') as f:
+            mgf_content = f.readlines()
+        assert mgf_content[0] == 'BEGIN IONS\n'
+        assert mgf_content[2] == 'CHARGE=1-\n'
+        assert mgf_content[4] == 'TEST_FIELD=test\n'
+        assert mgf_content[7].split(" ")[0] == '300.0'
 
 
 def test_save_as_mgf_spectrum_list():
@@ -36,17 +39,18 @@ def test_save_as_mgf_spectrum_list():
                          intensities=np.array([10, 10, 500], dtype="float"),
                          metadata={"test_field": 'test2'})
     # Write to test file
-    save_as_mgf([spectrum1, spectrum2], 'test.mgf')
-    assert os.path.isfile('test.mgf')
+    with tempfile.NamedTemporaryFile() as fp:
+        save_as_mgf([spectrum1, spectrum2], fp.name)
 
-    # Test if content of mgf file is correct
-    with open('test.mgf', 'r') as f:
-        mgf_content = f.readlines()
-    assert mgf_content[5] == mgf_content[12] == 'END IONS\n'
-    assert mgf_content[1].split("=")[1] == 'test1\n'
-    assert mgf_content[8].split("=")[1] == 'test2\n'
-    # Remove testfile again
-    os.remove('test.mgf')
+        # test if file exists
+        assert os.path.isfile(fp.name)
+
+        # Test if content of mgf file is correct
+        with open(fp.name, 'r') as f:
+            mgf_content = f.readlines()
+        assert mgf_content[5] == mgf_content[12] == 'END IONS\n'
+        assert mgf_content[1].split("=")[1] == 'test1\n'
+        assert mgf_content[8].split("=")[1] == 'test2\n'
 
 
 if __name__ == "__main__":
