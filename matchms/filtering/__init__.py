3faeb53effb15b8b64b6c2ef783fc33fe3cf7d69
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-21 17:13:27 +0200

    added new functions to init

--- matchms/filtering/__init__.py
+++ matchms/filtering/__init__.py
@@ -3,8 +3,11 @@ from .add_parent_mass import add_parent_mass
 from .clean_inchis import clean_inchis
 from .complete_compound_annotation import complete_compound_annotation
 from .correct_charge import correct_charge
-from .derive_ionmode import derive_ionmode
 from .default_filters import default_filters
+from .derive_inchi_from_smiles import derive_inchi_from_smiles
+from .derive_inchikey_from_inchi import derive_inchikey_from_inchi
+from .derive_ionmode import derive_ionmode
+from .derive_smiles_from_inchi import derive_smiles_from_inchi
 from .make_charge_scalar import make_charge_scalar
 from .make_ionmode_lowercase import make_ionmode_lowercase
 from .set_ionmode_na_when_missing import set_ionmode_na_when_missing
@@ -21,7 +24,10 @@ __all__ = [
     "complete_compound_annotation",
     "correct_charge",
     "default_filters",
+    "derive_inchi_from_smiles",
+    "derive_inchikey_from_inchi",
     "derive_ionmode",
+    "derive_smiles_from_inchi",
     "make_charge_scalar",
     "make_ionmode_lowercase",
     "normalize_intensities",
