52fcef2c08371465b0cbed655cc895d982236c69
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-21 10:48:51 +0200

    change 'spectra' to 'spectrums'

--- matchms/exporting/save_as_mgf.py
+++ matchms/exporting/save_as_mgf.py
@@ -1,23 +1,23 @@
 import pyteomics.mgf as py_mgf
 
 
-def save_as_mgf(spectra, filename):
-    """Save spectra as mgf file.
+def save_as_mgf(spectrums, filename):
+    """Save spectrum(s) as mgf file.
 
     Args:
     ----
-    spectra: list of Spectrum() objects, Spectrum() object
+    spectrums: list of Spectrum() objects, Spectrum() object
         Expected input are match.Spectrum.Spectrum() objects.
     filename: str
-        Provide filename to save spectra.
+        Provide filename to save spectrum(s).
     """
-    if not isinstance(spectra, list):
+    if not isinstance(spectrums, list):
         # Assume that input was single Spectrum
-        spectra = [spectra]
+        spectrums = [spectrums]
 
     # Convert matchms.Spectrum() into dictionaries for pyteomics
     spectrum_dicts = []
-    for spectrum in spectra:
+    for spectrum in spectrums:
         spectrum_dict = {"m/z array": spectrum.mz,
                          "intensity array": spectrum.intensities,
                          "params": spectrum.metadata}
