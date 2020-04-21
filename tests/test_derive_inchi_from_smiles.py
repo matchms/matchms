a19d49118481938c27e8d80c731165543c121969
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-21 17:21:57 +0200

    add test for derive_inchi_from_smiles

---
+++ tests/test_derive_inchi_from_smiles.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from matchms import Spectrum
+from matchms.filtering import derive_inchi_from_smiles
+
+
+def test_derive_inchi_from_smiles():
+    """Test if conversion to inchi and inchikey works when only smiles is given.
+    """
+    spectrum_in = Spectrum(mz=np.array([], dtype='float'),
+                           intensities=np.array([], dtype='float'),
+                           metadata={"smiles": "C1CCCCC1",
+                                     "inchikey": 'n\a'})
+
+    spectrum = derive_inchi_from_smiles(spectrum_in)
+    inchi = spectrum.get("inchi").replace('"', '')
+    assert inchi == 'InChI=1S/C6H12/c1-2-4-6-5-3-1/h1-6H2', "Expected different InChI"
+    assert spectrum.get("inchikey")[:14] == 'XDTMQSROBMDMFD', "Expected different inchikey"
+
+
+if __name__ == '__main__':
+    test_derive_inchi_from_smiles()
