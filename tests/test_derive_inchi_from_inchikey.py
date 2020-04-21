5ca5fb7f3c99594fdbbb440b428f4f30f21b4406
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-21 20:45:53 +0200

    fix bug in test

--- tests/test_derive_inchi_from_inchikey.py
+++ tests/test_derive_inchi_from_inchikey.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 from matchms import Spectrum
-from matchms.filtering import derive_inchi_from_inchikey
+from matchms.filtering import derive_inchikey_from_inchi
 
 
-def test_derive_inchi_from_inchikey():
+def test_derive_inchikey_from_inchi():
     """Test if conversion from inchi and inchikey works."""
     spectrum_in = Spectrum(mz=np.array([], dtype='float'),
                            intensities=np.array([], dtype='float'),
                            metadata={"inchikey": 'n/a'})
 
-    spectrum = derive_inchi_from_inchikey(spectrum_in)
+    spectrum = derive_inchikey_from_inchi(spectrum_in)
     assert spectrum.get("inchikey")[:14] == 'XDTMQSROBMDMFD', "Expected different inchikey"
 
 
 if __name__ == '__main__':
-    test_derive_inchi_from_inchikey()
+    test_derive_inchikey_from_inchi()
