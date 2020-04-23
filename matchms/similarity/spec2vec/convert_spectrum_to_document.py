8c4a562d3294636d1dc026a03dda039aa8a49edc
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-23 13:34:53 +0200

    updated conversion to document with new spectrum way of using .peaks

--- matchms/similarity/spec2vec/convert_spectrum_to_document.py
+++ matchms/similarity/spec2vec/convert_spectrum_to_document.py
@@ -4,6 +4,6 @@ from .Document import Document
 def convert_spectrum_to_document(spectrum, n_decimals=1):
 
     format_string = "peak@{:." + "{}".format(n_decimals) + "f}"
-    words = [format_string.format(i) for i in spectrum.mz]
+    words = [format_string.format(i) for i in spectrum.peaks.mz]
 
     return Document(obj=spectrum, words=words)
