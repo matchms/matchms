081102aefef2f5fae600955342e046e2507cee87
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-25 11:21:46 +0200

    now checks for spectrum is not None and for spectrum.losses is not None

--- matchms/similarity/spec2vec/convert_spectrum_to_document.py
+++ matchms/similarity/spec2vec/convert_spectrum_to_document.py
@@ -3,7 +3,14 @@ from .Document import Document
 
 def convert_spectrum_to_document(spectrum, n_decimals=1):
 
-    format_string = "peak@{:." + "{}".format(n_decimals) + "f}"
-    words = [format_string.format(i) for i in spectrum.peaks.mz]
+    if spectrum is None:
+        return None
 
-    return Document(obj=spectrum, words=words)
+    format_string = "{}@{:." + "{}".format(n_decimals) + "f}"
+    peak_words = [format_string.format("peak", mz) for mz in spectrum.peaks.mz]
+    if spectrum.losses is not None:
+        loss_words = [format_string.format("loss", mz) for mz in spectrum.losses.mz]
+    else:
+        loss_words = []
+
+    return Document(obj=spectrum, words=peak_words + loss_words)
