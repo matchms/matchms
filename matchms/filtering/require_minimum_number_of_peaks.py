f1a6c7548323428b06ef81512f9675d92fce9998
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-25 11:06:37 +0200

    somehow this change from an earlier commit was lost again

--- matchms/filtering/require_minimum_number_of_peaks.py
+++ matchms/filtering/require_minimum_number_of_peaks.py
@@ -9,7 +9,7 @@ def require_minimum_number_of_peaks(spectrum_in, n_required=10, ratio_required=N
     spectrum = spectrum_in.clone()
 
     parent_mass = spectrum.get("parent_mass", None)
-    if parent_mass:
+    if parent_mass and ratio_required:
         n_required_by_mass = int(ceil(ratio_required * parent_mass))
         threshold = max(n_required, n_required_by_mass)
     else:
