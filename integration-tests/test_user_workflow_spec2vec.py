681d7f14003d0ac55a98995241259723e97f61bd
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-25 11:05:08 +0200

    made spec2vec user workflow more challenging to pass

--- integration-tests/test_user_workflow_spec2vec.py
+++ integration-tests/test_user_workflow_spec2vec.py
@@ -1,17 +1,33 @@
 import os
 import gensim
 from matchms.importing import load_from_mgf
-from matchms.filtering import default_filters
+from matchms.filtering import default_filters, require_minimum_number_of_peaks, add_parent_mass, normalize_intensities
+from matchms.filtering import select_by_relative_intensity, select_by_mz, add_losses
 from matchms.similarity.spec2vec import convert_spectrum_to_document, Spec2Vec
 from matchms import calculate_scores
 
 
 def test_user_workflow_spec2vec():
 
-    module_root = os.path.join(os.path.dirname(__file__), '..')
-    references_file = os.path.join(module_root, 'tests', 'pesticides.mgf')
+    def apply_my_filters(s):
+        s = default_filters(s)
+        s = require_minimum_number_of_peaks(s, n_required=5)
+        s = add_parent_mass(s)
+        s = add_losses(s)
+        s = normalize_intensities(s)
+        s = select_by_relative_intensity(s, intensity_from=0.0, intensity_to=1.0)
+        s = select_by_mz(s, mz_from=0, mz_to=1000)
+        return s
 
-    spectrums = [default_filters(s) for s in load_from_mgf(references_file)]
+    module_root = os.path.join(os.path.dirname(__file__), '..')
+    spectrums_file = os.path.join(module_root, 'tests', 'pesticides.mgf')
+
+    # apply my filters to the data
+    spectrums = [apply_my_filters(s) for s in load_from_mgf(spectrums_file)]
+
+    # omit spectrums that didn't qualify for analysis
+    spectrums = [s for s in spectrums if s is not None]
+
     documents = [convert_spectrum_to_document(s) for s in spectrums]
 
     # create and train model
