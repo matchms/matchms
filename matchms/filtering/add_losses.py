1f384383bad20adaeb9da875faddaa324a4c876e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-04-25 11:04:08 +0200

    now checks for existence of precursor_mz

--- matchms/filtering/add_losses.py
+++ matchms/filtering/add_losses.py
@@ -8,9 +8,10 @@ def add_losses(spectrum_in):
     spectrum = spectrum_in.clone()
 
     precursor_mz = spectrum.get("precursor_mz")
-    peaks_mz, peaks_intensities = spectrum.peaks
-    losses_mz = (precursor_mz - peaks_mz)[::-1]
-    losses_intensities = peaks_intensities
-    spectrum.losses = Spikes(mz=losses_mz, intensities=losses_intensities)
+    if precursor_mz is not None:
+        peaks_mz, peaks_intensities = spectrum.peaks
+        losses_mz = (precursor_mz - peaks_mz)[::-1]
+        losses_intensities = peaks_intensities
+        spectrum.losses = Spikes(mz=losses_mz, intensities=losses_intensities)
 
     return spectrum
