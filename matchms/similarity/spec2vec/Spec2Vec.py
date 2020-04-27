65220e9069a0d9207ba6836f283b5ad3281a9cb2
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-27 13:51:22 +0200

    made calc_vector() a callable class function for Spec2Vec

--- matchms/similarity/spec2vec/Spec2Vec.py
+++ matchms/similarity/spec2vec/Spec2Vec.py
@@ -10,16 +10,16 @@ class Spec2Vec:
         self.dictionary = gensim.corpora.Dictionary([d.words for d in documents])
 
     def __call__(self, query, reference):
-
-        def calc_vector(document):
-            bag_of_words = self.dictionary.doc2bow(document.words)
-            words = [self.dictionary[item[0]] for item in bag_of_words]
-            word_vectors = self.model.wv[words]
-
-            return numpy.mean(word_vectors, 0)
-
-        query_vector = calc_vector(query)
-        reference_vector = calc_vector(reference)
+        query_vector = self.calc_vector(query)
+        reference_vector = self.calc_vector(reference)
         cdist = scipy.spatial.distance.cosine(query_vector, reference_vector)
 
         return 1 - cdist
+
+    def calc_vector(self, document):
+        """Derive latent space vector for entire document."""
+        bag_of_words = self.dictionary.doc2bow(document.words)
+        words = [self.dictionary[item[0]] for item in bag_of_words]
+        word_vectors = self.model.wv[words]
+
+        return numpy.mean(word_vectors, 0)
