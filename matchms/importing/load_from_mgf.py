9fbd9a5074c9978f9e1fd32ce6d686cbe0a89a81
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-28 13:39:10 +0200

    add mz sorting (to fullfill Spikes criteria)

--- matchms/importing/load_from_mgf.py
+++ matchms/importing/load_from_mgf.py
@@ -1,10 +1,11 @@
+import numpy as np
 from typing import List
 from pyteomics.mgf import MGF
 from ..Spectrum import Spectrum
 
 
 def load_from_mgf(filename: str) -> List[Spectrum]:
-    """load from mgf file"""
+    """Load spectrum(s) from mgf file."""
 
     spectrums = list()
     for pyteomics_spectrum in MGF(filename, convert_arrays=1):
@@ -12,6 +13,12 @@ def load_from_mgf(filename: str) -> List[Spectrum]:
         metadata = pyteomics_spectrum.get("params", None)
         mz = pyteomics_spectrum["m/z array"]
         intensities = pyteomics_spectrum["intensity array"]
+
+        # Sort by mz (if not sorted already)
+        if not np.all(mz[:-1] < mz[1:]):
+            idx_sorted = np.argsort(mz)
+            mz = mz[idx_sorted]
+            intensities = intensities[idx_sorted]
 
         spectrum = Spectrum(mz=mz, intensities=intensities, metadata=metadata)
         spectrums.append(spectrum)
