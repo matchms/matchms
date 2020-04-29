084d6d580d15099af8fd9e70ce616320a15e4ec4
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-29 16:38:00 +0200

    add tests for entry testing functions

---
+++ tests/test_metadata_entry_testing.py
@@ -0,0 +1,44 @@
+import numpy as np
+from matchms import Spectrum
+from matchms.metadata_entry_testing import entry_is_empty, is_valid_inchikey
+
+
+def test_entry_is_empty_inchi_key():
+    """Test if empty inchi and inchikey fields are recognized."""
+    spectrum_in = Spectrum(mz=np.array([], dtype='float'),
+                           intensities=np.array([], dtype='float'),
+                           metadata={"inchi": '"InChI="'})
+
+    assert entry_is_empty(spectrum_in, "inchi"), "Expected empty entry"
+    assert entry_is_empty(spectrum_in, "inchikey"), "Expected empty entry"
+
+
+def test_entry_is_empty_smiles():
+    """Test if empty smiles field is recognized."""
+    spectrum_in = Spectrum(mz=np.array([], dtype='float'),
+                           intensities=np.array([], dtype='float'),
+                           metadata={"smiles": "n/a"})
+    assert entry_is_empty(spectrum_in, "smiles"), "Expected empty entry"
+
+
+def test_is_valid_inchikey():
+    """Test if strings are correctly classified."""
+    inchikeys_true = ["XYLJNLCSTIOKRM-UHFFFAOYSA-N",
+                      "XYLJNLCSTIOKRM-aaaaaaaaaa-a"]
+    inchikeys_false = ["XYLJNLCSTIOKRM-UHFFFAOYSA",
+                       "XYLJNLCSTIOKRMRUHFFFAOYSASN",
+                       "XYLJNLCSTIOKR-MUHFFFAOYSA-N",
+                       "XYLJNLCSTIOKRM-UHFFFAOYSA-NN",
+                       "Brcc(NC2=NCN2)-ccc3nccnc1-3",
+                       "2YLJNLCSTIOKRM-UHFFFAOYSA-N"]
+
+    for inchikey in inchikeys_true:
+        assert is_valid_inchikey(inchikey), "Expected inchikey is True."
+    for inchikey in inchikeys_false:
+        assert not is_valid_inchikey(inchikey), "Expected inchikey is False."
+
+
+if __name__ == '__main__':
+    test_entry_is_empty_inchi_key()
+    test_entry_is_empty_smiles()
+    test_is_valid_inchikey()
