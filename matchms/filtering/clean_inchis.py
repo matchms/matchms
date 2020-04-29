1dfa3715c14529327f4d0d7c04b9fb59cab26308
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-04-29 17:19:46 +0200

    larger changes of clean_inchis, addition of misplaced inchikey test

--- matchms/filtering/clean_inchis.py
+++ matchms/filtering/clean_inchis.py
@@ -1,6 +1,6 @@
 from ..utils import mol_converter
 from ..typing import SpectrumType
-from ..metadata_entry_testing import entry_is_empty
+from ..metadata_entry_testing import entry_is_empty, is_valid_inchikey
 
 
 def clean_inchis(spectrum_in: SpectrumType, rescue_smiles=True) -> SpectrumType:
@@ -16,37 +16,49 @@ def clean_inchis(spectrum_in: SpectrumType, rescue_smiles=True) -> SpectrumType:
 
     Read spectrum, look for inchi. Then:
     1) Make line with inchi homogeneously looking like: '"InChI=..."'
-    2) if rescue_smiles is True then try to detect inchi that are actually smiles
+    2) Test if inchi is actually inchikey. If so, add as inchikey to metadata
+    unless inchikey already exists.
+    3) if rescue_smiles is True then try to detect inchi that are actually smiles
     and convert to proper inchi.
     """
+    if spectrum_in is None:
+        return None
 
     spectrum = spectrum_in.clone()
 
     if entry_is_empty(spectrum, "inchi"):
-        inchi = 'n/a'
-    else:
-        inchi = spectrum.get("inchi").replace(" ", "")
-        if inchi.split('InChI=')[-1][0] in ['C', 'c', 'O', 'N']:
-            if rescue_smiles:
-                # Try to 'rescue' given inchi which are actually smiles!
-                assumed_smile = inchi.split('InChI=')[-1].replace('"', '')
-                inchi = mol_converter(assumed_smile, "smi", "inchi")
-                if not inchi:
-                    inchi = 'n/a'
-                if len(inchi) < 12:
-                    inchi = 'n/a'
-                print("New inchi:", inchi.replace('\n', ''))
-                print("Derived inchi from assumed smile:", assumed_smile)
+        spectrum.set("inchi", "N/A")
+        return spectrum
 
-        # Make inchi string style consistent
-        inchi = inchi.strip().split('InChI=')[-1]
-        if inchi.endswith('"'):
-            inchi = '"InChI=' + inchi
-        elif inchi.endswith('\n'):
-            inchi = '"InChI=' + inchi[:-2] + '"'
-        elif inchi.endswith('\n"'):
-            inchi = '"InChI=' + inchi[:-3] + '"'
-        else:
-            inchi = '"InChI=' + inchi + '"'
+    inchi = spectrum.get("inchi").replace(" ", "")
+    inchi_core = inchi.strip().split("InChI=")[-1].replace('"', '')
+
+    # Test if inchi is actually inchikey
+    if is_valid_inchikey(inchi_core):
+        print("Inchikey", inchi_core, "found instead of InChI.")
+        if not is_valid_inchikey(spectrum.get("inchikey")):
+            spectrum.set("inchikey", inchi_core)
+
+        spectrum.set("inchi", "N/A")
+        return spectrum
+
+    # Test if inchi is actually smiles
+    if inchi_core[0] in ["C", "c", "O", "N", "n", "B"] and rescue_smiles:
+        inchi = mol_converter(inchi_core, "smi", "inchi")
+        if not inchi or len(inchi) < 12:
+            spectrum.set("inchi", "N/A")
+            return spectrum
+        print("New inchi added:", inchi.replace("\n", ""))
+        print("Derived inchi from assumed smile:", inchi_core)
+
+    # Make inchi string style consistent
+    if inchi.endswith('"'):
+        inchi = '"InChI=' + inchi
+    elif inchi.endswith('\n'):
+        inchi = '"InChI=' + inchi[:-2] + '"'
+    elif inchi.endswith('\n"'):
+        inchi = '"InChI=' + inchi[:-3] + '"'
+    else:
+        inchi = '"InChI=' + inchi + '"'
     spectrum.set("inchi", inchi)
     return spectrum
