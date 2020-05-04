b4d47c4e0446166244fce5827e4845e7245f271f
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-04 21:54:45 +0200

    add default entry for empty entries, too

--- matchms/filtering/harmonize_undefined_inchi.py
+++ matchms/filtering/harmonize_undefined_inchi.py
@@ -19,6 +19,7 @@ def harmonize_undefined_inchi(spectrum_in: SpectrumType, undefined="", aliases=N
     inchi = spectrum.get("inchi")
     if inchi is None:
         # spectrum does not have an "inchi" key in its metadata
+        spectrum.set("inchi", undefined)
         return spectrum
 
     if inchi in aliases:
