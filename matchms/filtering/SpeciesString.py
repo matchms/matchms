e99fd3b1e7f17d134ff7e7012c52d4e35e6ebc94
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-04 15:44:57 +0200

    add cleaning and is_valid functions

--- matchms/filtering/SpeciesString.py
+++ matchms/filtering/SpeciesString.py
@@ -1,4 +1,5 @@
 import re
+from rdkit import Chem
 
 
 class SpeciesString:
@@ -32,13 +33,38 @@ class SpeciesString:
         return self
 
     def clean_as_inchi(self):
-        self.cleaned = self.dirty
+        """Search for valid inchi and harmonize it."""
+        inchi_found = re.search("(1S\/|1\/)[0-9, A-Z, a-z,\.]{2,}\/(c|h)[0-9].*$",
+                                self.dirty)
+        if not inchi_found:
+            self.cleaned = "unable to clean"
+        else:
+            inchi_cleaned = "InChI=" + inchi_found[0].replace('"', "")
+            if self.is_valid_inchi(inchi_cleaned):
+                self.cleaned = inchi_cleaned
+            else:
+                self.cleaned = "unable to clean"
 
     def clean_as_inchikey(self):
-        self.cleaned = self.dirty
+        """Search for valid inchikey and harmonize it."""
+        inchikey_found = re.search("[A-Z]{14}-[A-Z]{10}-[A-Z]", self.dirty)
+        if inchikey_found:
+            self.cleaned = inchikey_found[0]
+        else:
+            self.cleaned = "unable to clean"
 
     def clean_as_smiles(self):
-        self.cleaned = self.dirty
+        """Search for valid smiles and harmonize it."""
+        smiles_found = re.search("^([^J][0-9BCOHNSOPrIFla@+\-\[\]\(\)\\\/%=#$,.~&!]{6,})$",
+                                self.dirty)
+        if not smiles_found:
+            self.cleaned = "unable to clean"
+        else:
+            smiles_cleaned = smiles_found[0]
+            if self.is_valid_smiles(smiles_cleaned):
+                self.cleaned = smiles_cleaned
+            else:
+                self.cleaned = "unable to clean"
 
     def guess_target(self):
 
@@ -54,14 +80,51 @@ class SpeciesString:
         return self
 
     def looks_like_an_inchi(self):
-        return \
-            self.dirty.lower().startswith("inchi=") or \
-            self.dirty.lower().startswith('"inchi=')
+        """Search for first piece of InChI."""
+        if re.search("(InChI=1|1)(S\/|\/)[0-9, A-Z, a-z,\.]{2,}\/(c|h)[0-9]",
+                     self.dirty):
+            return True
+        return False
 
     def looks_like_an_inchikey(self):
-        return re.fullmatch("[A-Z]{14}-[A-Z]{10}-[A-Z]", self.dirty)
+        """Return True if string has format of inchikey."""
+        if re.search("[A-Z]{14}-[A-Z]{10}-[A-Z]", self.dirty):
+            return True
+        return False
 
     def looks_like_a_smiles(self):
-        # maybe something like
-        # https://gist.github.com/lsauer/1312860/264ae813c2bd2c27a769d261c8c6b38da34e22fb#file-smiles_inchi_annotated-js-L31
-        return "@" in self.dirty
+        """Return True if string is made of allowed charcters for smiles."""
+        if re.search("^([^J][0-9BCOHNSOPIFKcons@+\-\[\]\(\)\\\/%=#$,.~&!|Si|Se|Br|Mg|Na|Cl|Al]{3,})$",
+                    self.dirty):
+            return True
+        return False
+
+    def is_valid_inchi(self, inchi):
+        """Return True if input string is valid InChI.
+    
+        This functions test if string can be read by rdkit as InChI.
+    
+        Args:
+        ----
+        inchi: str
+            Input string to test if it has format of InChI.
+        """
+        mol = Chem.MolFromInchi(inchi.replace('"', ""))
+        if mol:
+            return True
+        return False
+
+    def is_valid_smiles(smiles):
+        """Return True if input string is valid smiles.
+    
+        This functions test if string can be read by rdkit as smiles.
+    
+        Args:
+        ----
+        inchi: str
+            Input string to test if it can be imported as smiles.
+        """
+        mol = Chem.MolFromSmiles(smiles)
+        if mol:
+            return True
+        return False
