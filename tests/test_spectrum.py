9e152eb5235a4dfbd935ead3f4eca1b8547c48ba
Christiaan Meijer <c.meijer@esciencecenter.nl>
2020-05-11 13:55:43 +0200

    refactor spectrum tests

--- tests/test_spectrum.py
+++ tests/test_spectrum.py
@@ -1,117 +1,70 @@
+from unittest import TestSuite
+
 import numpy
 from matchms import Spectrum
 from matplotlib import pyplot as plt
 
 
-def test_spectrum_plot_with_histogram_unspecified():
-    mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
-    intensities = numpy.array([1, 1, 5, 5, 5, 5, 7, 7, 7, 9, 9], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities)
+class PlotTestSuite(TestSuite):
+    def test_spectrum_plot_with_histogram_unspecified(self):
+        spectrum = self._create_test_spectrum()
 
-    fig = spectrum.plot()
+        fig = spectrum.plot()
 
-    assert fig is not None
-    assert hasattr(fig, "axes")
-    assert isinstance(fig.axes, list)
-    assert len(fig.axes) == 1
-    assert isinstance(fig.axes[0], plt.Axes)
-    assert hasattr(fig.axes[0], "lines")
-    assert isinstance(fig.axes[0].lines, list)
-    assert len(fig.axes[0].lines) == 11
-    assert isinstance(fig.axes[0].lines[0], plt.Line2D)
-    assert hasattr(fig.axes[0].lines[0], "_x")
+        self._asserts_plots_ok(fig, n_plots=1)
 
+    def test_spectrum_plot_with_histogram_false(self):
+        spectrum = self._create_test_spectrum()
 
-def test_spectrum_plot_with_histogram_false():
-    mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
-    intensities = numpy.array([1, 1, 5, 5, 5, 5, 7, 7, 7, 9, 9], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities)
+        fig = spectrum.plot(with_histogram=False)
 
-    fig = spectrum.plot(with_histogram=False)
+        self._asserts_plots_ok(fig, n_plots=1)
 
-    assert fig is not None
-    assert hasattr(fig, "axes")
-    assert isinstance(fig.axes, list)
-    assert len(fig.axes) == 1
-    assert isinstance(fig.axes[0], plt.Axes)
-    assert hasattr(fig.axes[0], "lines")
-    assert isinstance(fig.axes[0].lines, list)
-    assert len(fig.axes[0].lines) == 11
-    assert isinstance(fig.axes[0].lines[0], plt.Line2D)
-    assert hasattr(fig.axes[0].lines[0], "_x")
+    def test_spectrum_plot_with_histogram_true(self):
+        spectrum = self._create_test_spectrum()
 
+        fig = spectrum.plot(with_histogram=True)
 
-def test_spectrum_plot_with_histogram_true():
-    mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
-    intensities = numpy.array([1, 1, 5, 5, 5, 5, 7, 7, 7, 9, 9], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities)
+        self._asserts_plots_ok(fig, n_plots=2)
 
-    fig = spectrum.plot(with_histogram=True)
+    def test_spectrum_plot_with_histogram_true_and_intensity_limit(self):
+        spectrum = self._create_test_spectrum()
 
-    assert fig is not None
-    assert hasattr(fig, "axes")
-    assert isinstance(fig.axes, list)
-    assert len(fig.axes) == 2
-    assert isinstance(fig.axes[0], plt.Axes)
-    assert hasattr(fig.axes[0], "lines")
-    assert isinstance(fig.axes[0].lines, list)
-    assert len(fig.axes[0].lines) == 11
-    assert isinstance(fig.axes[0].lines[0], plt.Line2D)
-    assert hasattr(fig.axes[0].lines[0], "_x")
+        fig = spectrum.plot(with_histogram=True, intensity_to=10.0)
 
+        self._asserts_plots_ok(fig, n_plots=2)
 
-def test_spectrum_plot_with_histogram_true_and_intensity_limit():
-    mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
-    intensities = numpy.array([1, 1, 5, 5, 5, 5, 7, 7, 7, 9, 9], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities)
+    def test_spectrum_plot_with_histogram_true_and_expfit_true_and_intensity_limit(self):
+        spectrum = self._create_test_spectrum()
 
-    fig = spectrum.plot(with_histogram=True, intensity_to=10.0)
+        fig = spectrum.plot(with_histogram=True, with_expfit=True, intensity_to=10.0)
 
-    assert fig is not None
-    assert hasattr(fig, "axes")
-    assert isinstance(fig.axes, list)
-    assert len(fig.axes) == 2
-    assert isinstance(fig.axes[0], plt.Axes)
-    assert hasattr(fig.axes[0], "lines")
-    assert isinstance(fig.axes[0].lines, list)
-    assert len(fig.axes[0].lines) == 11
-    assert isinstance(fig.axes[0].lines[0], plt.Line2D)
-    assert hasattr(fig.axes[0].lines[0], "_x")
+        self._asserts_plots_ok(fig, n_plots=2)
 
+    def test_spectrum_plot_without_variance(self):
+        intensities_without_variance = numpy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype="float")
+        spectrum = self._create_test_spectrum_with_intensities(intensities_without_variance)
 
-def test_spectrum_plot_with_histogram_true_and_expfit_true_and_intensity_limit():
-    mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
-    intensities = numpy.array([1, 1, 5, 5, 5, 5, 7, 7, 7, 9, 9], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities)
+        fig = spectrum.plot(with_histogram=True, with_expfit=True, intensity_to=10.0)
 
-    fig = spectrum.plot(with_histogram=True, with_expfit=True, intensity_to=10.0)
+        self.assert_stuff(fig)
 
-    assert fig is not None
-    assert hasattr(fig, "axes")
-    assert isinstance(fig.axes, list)
-    assert len(fig.axes) == 2
-    assert isinstance(fig.axes[0], plt.Axes)
-    assert hasattr(fig.axes[0], "lines")
-    assert isinstance(fig.axes[0].lines, list)
-    assert len(fig.axes[0].lines) == 11
-    assert isinstance(fig.axes[0].lines[0], plt.Line2D)
-    assert hasattr(fig.axes[0].lines[0], "_x")
+    def _asserts_plots_ok(self, fig, n_plots):
+        assert len(fig.axes) == n_plots
+        assert fig is not None
+        assert hasattr(fig, "axes")
+        assert isinstance(fig.axes, list)
+        assert isinstance(fig.axes[0], plt.Axes)
+        assert hasattr(fig.axes[0], "lines")
+        assert isinstance(fig.axes[0].lines, list)
+        assert len(fig.axes[0].lines) == 11
+        assert isinstance(fig.axes[0].lines[0], plt.Line2D)
+        assert hasattr(fig.axes[0].lines[0], "_x")
 
+    def _create_test_spectrum(self):
+        intensities = numpy.array([1, 1, 5, 5, 5, 5, 7, 7, 7, 9, 9], dtype="float")
+        return self._create_test_spectrum_with_intensities(intensities)
 
-def test_spectrum_plot_without_variance():
-    mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
-    intensities = numpy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], dtype="float")
-    spectrum = Spectrum(mz=mz, intensities=intensities)
-
-    fig = spectrum.plot(with_histogram=True, with_expfit=True, intensity_to=10.0)
-
-    assert fig is not None
-    assert hasattr(fig, "axes")
-    assert isinstance(fig.axes, list)
-    assert len(fig.axes) == 2
-    assert isinstance(fig.axes[0], plt.Axes)
-    assert hasattr(fig.axes[0], "lines")
-    assert isinstance(fig.axes[0].lines, list)
-    assert len(fig.axes[0].lines) == 11
-    assert isinstance(fig.axes[0].lines[0], plt.Line2D)
-    assert hasattr(fig.axes[0].lines[0], "_x")
+    def _create_test_spectrum_with_intensities(self, intensities):
+        mz = numpy.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110], dtype="float")
+        return Spectrum(mz=mz, intensities=intensities)
