98c927eb846674a9044ebfa5d11cd8d9b5271c72
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-12 16:31:58 +0200

    forgot to commit a bunch of files

---
+++ matchms/calculate_scores_parallel.py
@@ -0,0 +1,10 @@
+from typing import Callable
+from typing import List
+from .Scores import Scores
+
+
+def calculate_scores_parallel(references: List[object], queries: List[object], similarity_function: Callable):
+
+    return Scores(references=references,
+                  queries=queries,
+                  similarity_function=similarity_function).calculate_parallel()
