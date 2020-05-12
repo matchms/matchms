f14593b81fbceade5534a679384101cf9988e87c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-05-12 17:49:59 +0200

    now using numpy.sum

--- matchms/similarity/spec2vec/calc_vector.py
+++ matchms/similarity/spec2vec/calc_vector.py
@@ -6,5 +6,5 @@ def calc_vector(model, document, intensity_weighting_power=0):
     weights = numpy.asarray(document.weights).reshape(len(document), 1)
     weights_raised = numpy.power(weights, intensity_weighting_power)
     weights_raised_tiled = numpy.tile(weights_raised, (1, model.wv.vector_size))
-    vector = sum(word_vectors * weights_raised_tiled, 0)
+    vector = numpy.sum(word_vectors * weights_raised_tiled, 0)
     return vector
