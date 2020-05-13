26b116c13ba3fffa14180409798b5a636a542137
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-13 10:50:18 +0200

    make possible to import GNPS json files

--- matchms/importing/load_from_json.py
+++ matchms/importing/load_from_json.py
@@ -1,3 +1,4 @@
+import ast
 import json
 import numpy as np
 from matchms import Spectrum
@@ -12,11 +13,23 @@ def load_from_json(filename):
         Provide filename for json file containing spectrum(s).
     """
     not_metadata_fields = ["peaks_json"]
+    parse_fieldnames = dict(inchi_aux = "inchiaux",
+                            ion_mode = "ionmode")
 
     def parse_fieldname(key):
         """Add options to read GNPS style json files."""
         key_parsed = key.lower()
+        if key_parsed in parse_fieldnames:
+            key_parsed = parse_fieldnames[key_parsed]
         return key_parsed
+
+    def get_peaks_list(spectrum_dict, fieldname):
+        peaks_list = spectrum_dict.get(fieldname)
+        if isinstance(peaks_list, list):
+            return peaks_list
+        # Handle peaks list when stored as string
+        if isinstance(peaks_list, str):
+            return ast.literal_eval(peaks_list)
 
     # Load from json file
     with open(filename, 'rb') as fin:
@@ -27,10 +40,13 @@ def load_from_json(filename):
 
         metadata_dict = {parse_fieldname(key): spectrum_dict[key]
                          for key in spectrum_dict if key not in not_metadata_fields}
-        peaks_array = np.array(spectrum_dict.get("peaks_json"))
-        spectrum = Spectrum(mz=peaks_array[:, 0],
-                            intensities=peaks_array[:, 1],
-                            metadata=metadata_dict)
-        spectrums.append(spectrum)
+        peaks_list = get_peaks_list(spectrum_dict, "peaks_json")
+        if isinstance(peaks_list, list) and len(peaks_list) > 0:
+            spectrum = Spectrum(mz=np.array(peaks_list)[:, 0],
+                                intensities=np.array(peaks_list)[:, 1],
+                                metadata=metadata_dict)
+            spectrums.append(spectrum)
+        else:
+            print("Empty spectrum found (no peaks in 'peaks_json').")
 
     return spectrums
