0fdccd9b8b1d7a3c492f7be3bd220719acc3f83f
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-05-13 17:18:53 +0200

    Add logo to readthedocs

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-.. image:: matchms.png
+.. image:: readthedocs/_static/matchms.png
   :width: 600
   :alt: matchms logo
 
--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -71,6 +71,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'readthedocs/conf.rst']
 html_theme = 'alabaster'
 
 html_theme_options = {
+    'logo': 'matchms.png',
     'github_user': 'matchms',
     'github_repo': 'matchms',
 }
@@ -78,7 +79,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
 
