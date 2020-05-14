ca19df2f8b5939bc788efaba5f3910c1daa52f2d
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-14 13:11:36 +0200

    include string to float conversion for precursor_mz field

--- matchms/filtering/harmonize_precursor_mz.py
+++ matchms/filtering/harmonize_precursor_mz.py
@@ -2,13 +2,19 @@ from ..typing import SpectrumType
 
 
 def harmonize_precursor_mz(spectrum_in: SpectrumType) -> SpectrumType:
-    """Harmonize precursor_mz field."""
+    """Harmonize precursor_mz field.
+    
+    For missing precursor_mz field: check if there is "pepmass"" entry instead.
+    For string parsed as precursor_mz: convert to float.
+    """
     if spectrum_in is None:
         return None
 
     spectrum = spectrum_in.clone()
 
-    if spectrum.get("precursor_mz", None) is None:
+    if isinstance(spectrum.get("precursor_mz", None), str):
+        spectrum.set("precursor_mz", float(spectrum.get("precursor_mz").strip()))
+    elif spectrum.get("precursor_mz", None) is None:
         pepmass = spectrum.get("pepmass")
         if pepmass:
             spectrum.set("precursor_mz", pepmass[0])
