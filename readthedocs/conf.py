9e7b8be16ae26c0d0cf84ac5931507aa446b8284
Christiaan Meijer <c.meijer@esciencecenter.nl>
2020-05-18 16:54:05 +0200

    run `isort --recursive .`

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -12,10 +12,12 @@
 #
 import os
 import sys
+import matchms
+
+
 d = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(d,'..'))
 
-import matchms
 
 # -- Project information -----------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-
 from setuptools import setup
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 
