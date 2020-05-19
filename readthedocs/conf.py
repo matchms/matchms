7c8532c4c40f6d5f6984b5d9b6f11dca9ed5597d
Christiaan Meijer <c.meijer@esciencecenter.nl>
2020-05-19 10:48:53 +0200

    refert sorting imports because path insert is needed before imports

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -12,12 +12,10 @@
 #
 import os
 import sys
-import matchms
-
-
 d = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(d,'..'))
 
+import matchms
 
 # -- Project information -----------------------------------------------------
 
