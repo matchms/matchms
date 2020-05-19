525a18355b55e19d586b732bddf3cd0abe988376
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-19 17:19:32 +0200

    fixed assert bug and added test case for it

--- tests/test_derive_adduct_from_name.py
+++ tests/test_derive_adduct_from_name.py
@@ -35,3 +35,14 @@ def test_derive_adduct_from_name_dont_remove_from_name():
 
     assert spectrum.get("adduct") == "[M+H+K]", "Expected different adduct."
     assert spectrum.get("compound_name") == spectrum_in.get("compound_name"), "Expected no change to name."
+
+
+def test_derive_adduct_no_compound_name_empty_name():
+    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
+                           intensities=numpy.array([], dtype="float"),
+                           metadata={"name": ""})
+
+    spectrum = derive_adduct_from_name(spectrum_in)
+
+    assert spectrum.get("adduct", None) is None, "Expected None for adduct."
+    assert spectrum.get("compound_name", None) is None, "Expected None for name."
