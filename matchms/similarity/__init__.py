fd8d3f8ae19aeaa9616760c041107ef209b138ff
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-20 12:59:21 +0200

    add CosineHungarian similarity

--- matchms/similarity/__init__.py
+++ matchms/similarity/__init__.py
@@ -1,9 +1,11 @@
 """similarity module"""
 from .CosineGreedy import CosineGreedy
+from .CosineHungarian import CosineHungarian
 from .IntersectMz import IntersectMz
 
 
 __all__ = [
     "CosineGreedy",
+    "CosineHungarian",
     "IntersectMz"
 ]
