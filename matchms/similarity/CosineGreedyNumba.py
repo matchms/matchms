93459677d798a506bb9d6e4744f06e57bf45cd8a
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-28 15:04:42 +0200

    move collect_peak_pairs to own file

--- matchms/similarity/CosineGreedyNumba.py
+++ matchms/similarity/CosineGreedyNumba.py
@@ -1,6 +1,6 @@
 from typing import Tuple
-import numba
 import numpy
+from matchms.similarity.collect_peak_pairs import collect_peak_pairs
 from matchms.typing import SpectrumType
 
 
@@ -47,7 +47,7 @@ class CosineGreedyNumba:
 
         def get_matching_pairs():
             """Get pairs of peaks that match within the given tolerance."""
-            matching_pairs = find_pairs_numba(spec1, spec2, self.tolerance, shift=0.0)
+            matching_pairs = collect_peak_pairs(spec1, spec2, self.tolerance, shift=0.0)
             matching_pairs = sorted(matching_pairs, key=lambda x: x[2], reverse=True)
             return matching_pairs
 
@@ -70,34 +70,3 @@ class CosineGreedyNumba:
         spec1, spec2 = get_peaks_arrays()
         matching_pairs = get_matching_pairs()
         return calc_score()
-
-
-@numba.njit
-def find_pairs_numba(spec1, spec2, tolerance, shift=0):
-    """Find matching pairs between two spectra.
-
-    Args
-    ----
-    spec1: numpy array
-        Spectrum peaks and intensities as numpy array.
-    spec2: numpy array
-        Spectrum peaks and intensities as numpy array.
-    tolerance : float
-        Peaks will be considered a match when <= tolerance appart.
-    shift : float, optional
-        Shift spectra peaks by shift. The default is 0.
-
-    Returns
-    -------
-    matching_pairs : list
-        List of found matching peaks.
-    """
-    matching_pairs = []
-
-    for idx in range(len(spec1)):
-        intensity = spec1[idx, 1]
-        matches = numpy.where((numpy.abs(spec2[:, 0] - spec1[idx, 0] + shift) <= tolerance))[0]
-        for match in matches:
-            matching_pairs.append((idx, match, intensity*spec2[match][1]))
-
-    return matching_pairs
