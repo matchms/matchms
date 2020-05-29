1c0e99391324e2fcb09b9e2a9e72286612b8ead6
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-05-29 10:55:59 +0200

    add test for saving and loading spectrum to json

---
+++ tests/test_save_as_json_load_from_json.py
@@ -0,0 +1,50 @@
+import os
+import tempfile
+import numpy
+from matchms import Spectrum
+from matchms.exporting import save_as_json
+from matchms.importing import load_from_json
+
+
+def test_save_and_load_json_single_spectrum():
+    """Test saving spectrum to .json file"""
+    spectrum = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
+                        intensities=numpy.array([10, 10, 500], dtype="float"),
+                        metadata={"charge": -1,
+                                  "inchi": '"InChI=1S/C6H12"',
+                                  "precursor_mz": 222.2,
+                                  "test_field": "test"})
+    # Write to test file
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, "test.json")
+        save_as_json(spectrum, filename)
+
+        # test if file exists
+        assert os.path.isfile(filename)
+
+        # Test if content of json file is correct
+        spectrum_import = load_from_json(filename)[0]
+        assert spectrum_import == spectrum, "Original and saved+loaded spectrum not identical"
+
+
+def test_save_and_load_json_spectrum_list():
+    """Test saving spectrum list to .json file"""
+    spectrum1 = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
+                         intensities=numpy.array([10, 10, 500], dtype="float"),
+                         metadata={"test_field": "test1"})
+
+    spectrum2 = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
+                         intensities=numpy.array([10, 10, 500], dtype="float"),
+                         metadata={"test_field": "test2"})
+    # Write to test file
+    with tempfile.TemporaryDirectory() as d:
+        filename = os.path.join(d, "test.json")
+        save_as_json([spectrum1, spectrum2], filename)
+
+        # test if file exists
+        assert os.path.isfile(filename)
+
+        # Test if content of json file is correct
+        spectrum_imports = load_from_json(filename)
+        assert spectrum_import[0] == spectrum1, "Original and saved+loaded spectrum not identical"
+        assert spectrum_import[1] == spectrum1, "Original and saved+loaded spectrum not identical"
