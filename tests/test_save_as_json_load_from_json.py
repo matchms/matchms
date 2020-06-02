fafd24c5bf1f9eb3e035da244486c2489ade493d
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2020-06-02 17:15:55 +0200

    Update tests/test_save_as_json_load_from_json.py
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- tests/test_save_as_json_load_from_json.py
+++ tests/test_save_as_json_load_from_json.py
@@ -48,3 +48,19 @@ def test_save_and_load_json_spectrum_list():
         spectrum_imports = load_from_json(filename)
         assert spectrum_imports[0] == spectrum1, "Original and saved+loaded spectrum not identical"
         assert spectrum_imports[1] == spectrum2, "Original and saved+loaded spectrum not identical"
+
+def test_load_from_json_zero_peaks(tmp_path):
+    spectrum1 = Spectrum(mz=numpy.array([], dtype="float"),
+                         intensities=numpy.array([], dtype="float"),
+                         metadata={"test_field": "test1"})
+
+    filename = tmp_path / "test.json"
+
+    save_as_json([spectrum1], filename)
+
+    # test if file exists
+    assert os.path.isfile(filename)
+
+    # Test if content of json file is correct
+    spectrum_imports = load_from_json(filename)
+    assert len(spectrum_imports) == 0, "Spectrum without peaks should be skipped"                
