b40321e05f13446b09d6b5879bbd69931b6c66ae
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2020-06-02 17:14:41 +0200

    Update matchms/importing/load_from_json.py
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- matchms/importing/load_from_json.py
+++ matchms/importing/load_from_json.py
@@ -15,15 +15,48 @@ JSON document formatted like the `GNPS Spectra library <https://gnps-external.uc
     filename: str
         Provide filename for json file containing spectrum(s).
     """
+    with open(filename, 'rb') as fin:
+        spectrums = []
+        for spectrum in json.load(fin, object_hook=as_spectrum):
+            if spectrum is not None:
+                spectrums.append(spectrum)
+
+    return spectrums
+
+
+def as_spectrum(dct: dict) -> Union[dict, Spectrum, None]:
+    """A :py:func:`json.load` object_hook to convert dictionary shaped like
+    spectrum into :py:class:`~matchms.Spectrum.Spectrum` object.
+
+    Parameters
+    ----------
+    dct
+        Dictionary shaped like spectrum
+    Returns
+    -------
+    A Spectrum or None when no peaks where found.
+    """
+    # Recognize Spectrum by peaks_json key
+    if 'peaks_json' in dct:
+        return dict2spectrum(dct)
+    return None
+
+
+def dict2spectrum(spectrum_dict: dict) -> Union[Spectrum, None]:
+    """Convert dictionary to a :py:class:`~matchms.Spectrum.Spectrum` object.
+
+    Parameters
+    ----------
+    spectrum_dict
+        Dictionary shaped like a single JSON object from the `GNPS Spectra library <https://gnps-external.ucsd.edu/gnpslibrary>`_
+
+    Returns
+    -------
+    A Spectrum or None when no peaks where found.
+    """
     not_metadata_fields = ["peaks_json"]
     parse_fieldnames = dict(inchi_aux="inchiaux",
                             ion_mode="ionmode")
-
-    def parse_fieldname(key):
-        """Add options to read GNPS style json files."""
-        key_parsed = key.lower()
-        key_parsed = parse_fieldnames.get(key_parsed, key_parsed)
-        return key_parsed
 
     def get_peaks_list(spectrum_dict, fieldname):
         peaks_list = spectrum_dict.get(fieldname)
@@ -34,31 +67,27 @@ JSON document formatted like the `GNPS Spectra library <https://gnps-external.uc
             return ast.literal_eval(peaks_list)
         return []
 
-    # Load from json file
-    with open(filename, 'rb') as fin:
-        spectrum_dicts = json.load(fin)
+    def parse_fieldname(key):
+        """Add options to read GNPS style json files."""
+        key_parsed = key.lower()
+        key_parsed = parse_fieldnames.get(key_parsed, key_parsed)
+        return key_parsed
 
-    spectrums = []
-    for spectrum_dict in spectrum_dicts:
+    metadata_dict = {parse_fieldname(key): spectrum_dict[key]
+                     for key in spectrum_dict if key not in not_metadata_fields}
+    peaks_list = get_peaks_list(spectrum_dict, "peaks_json")
+    if len(peaks_list) > 0 and metadata_dict:
+        mz = numpy.array(peaks_list)[:, 0]
+        intensities = numpy.array(peaks_list)[:, 1]
 
-        metadata_dict = {parse_fieldname(key): spectrum_dict[key]
-                         for key in spectrum_dict if key not in not_metadata_fields}
-        peaks_list = get_peaks_list(spectrum_dict, "peaks_json")
-        if len(peaks_list) > 0 and metadata_dict:
-            mz = numpy.array(peaks_list)[:, 0]
-            intensities = numpy.array(peaks_list)[:, 1]
-
-            # Sort by mz (if not sorted already)
-            if not numpy.all(mz[:-1] <= mz[1:]):
-                idx_sorted = numpy.argsort(mz)
-                mz = mz[idx_sorted]
-                intensities = intensities[idx_sorted]
-            spectrum = Spectrum(mz=mz,
-                                intensities=intensities,
-                                metadata=metadata_dict)
-            spectrums.append(spectrum)
-        else:
-            print("Empty spectrum found (no peaks in 'peaks_json').",
-                  "Will not be imported.")
-
-    return spectrums
+        # Sort by mz (if not sorted already)
+        if not numpy.all(mz[:-1] <= mz[1:]):
+            idx_sorted = numpy.argsort(mz)
+            mz = mz[idx_sorted]
+            intensities = intensities[idx_sorted]
+        return Spectrum(mz=mz,
+                        intensities=intensities,
+                        metadata=metadata_dict)
+    else:
+        print("Empty spectrum found (no peaks in 'peaks_json').",
+              "Will not be imported.")
