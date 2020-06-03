16e68c00c2f41ebe5c795dd5c8ca82de774f2ba8
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-06-03 16:52:25 +0200

    change save/load test to use tmp_path

--- tests/test_save_as_json_load_from_json.py
+++ tests/test_save_as_json_load_from_json.py
@@ -1,12 +1,11 @@
 import os
-import tempfile
 import numpy
 from matchms import Spectrum
 from matchms.exporting import save_as_json
 from matchms.importing import load_from_json
 
 
-def test_save_and_load_json_single_spectrum():
+def test_save_and_load_json_single_spectrum(tmp_path):
     """Test saving spectrum to .json file"""
     spectrum = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
                         intensities=numpy.array([10, 10, 500], dtype="float"),
@@ -15,19 +14,19 @@ def test_save_and_load_json_single_spectrum():
                                   "precursor_mz": 222.2,
                                   "test_field": "test"})
     # Write to test file
-    with tempfile.TemporaryDirectory() as d:
-        filename = os.path.join(d, "test.json")
-        save_as_json(spectrum, filename)
+    # with tempfile.TemporaryDirectory() as d:
+    filename = os.path.join(tmp_path, "test.json")
+    save_as_json(spectrum, filename)
 
-        # test if file exists
-        assert os.path.isfile(filename)
+    # test if file exists
+    assert os.path.isfile(filename)
 
-        # Test if content of json file is correct
-        spectrum_import = load_from_json(filename)[0]
-        assert spectrum_import == spectrum, "Original and saved+loaded spectrum not identical"
+    # Test if content of json file is correct
+    spectrum_import = load_from_json(filename)[0]
+    assert spectrum_import == spectrum, "Original and saved+loaded spectrum not identical"
 
 
-def test_save_and_load_json_spectrum_list():
+def test_save_and_load_json_spectrum_list(tmp_path):
     """Test saving spectrum list to .json file"""
     spectrum1 = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
                          intensities=numpy.array([10, 10, 500], dtype="float"),
@@ -37,17 +36,16 @@ def test_save_and_load_json_spectrum_list():
                          intensities=numpy.array([10, 10, 500], dtype="float"),
                          metadata={"test_field": "test2"})
     # Write to test file
-    with tempfile.TemporaryDirectory() as d:
-        filename = os.path.join(d, "test.json")
-        save_as_json([spectrum1, spectrum2], filename)
+    filename = os.path.join(tmp_path, "test.json")
+    save_as_json([spectrum1, spectrum2], filename)
 
-        # test if file exists
-        assert os.path.isfile(filename)
+    # test if file exists
+    assert os.path.isfile(filename)
 
-        # Test if content of json file is correct
-        spectrum_imports = load_from_json(filename)
-        assert spectrum_imports[0] == spectrum1, "Original and saved+loaded spectrum not identical"
-        assert spectrum_imports[1] == spectrum2, "Original and saved+loaded spectrum not identical"
+    # Test if content of json file is correct
+    spectrum_imports = load_from_json(filename)
+    assert spectrum_imports[0] == spectrum1, "Original and saved+loaded spectrum not identical"
+    assert spectrum_imports[1] == spectrum2, "Original and saved+loaded spectrum not identical"
 
 
 def test_load_from_json_zero_peaks(tmp_path):
