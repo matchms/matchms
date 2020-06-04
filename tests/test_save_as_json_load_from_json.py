61a424b045046ee44b0e457c65a3b19b20ed8977
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2020-06-04 16:37:54 +0200

    Update tests/test_save_as_json_load_from_json.py
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- tests/test_save_as_json_load_from_json.py
+++ tests/test_save_as_json_load_from_json.py
@@ -62,3 +62,33 @@ def test_load_from_json_zero_peaks(tmp_path):
     # Test if content of json file is correct
     spectrum_imports = load_from_json(filename)
     assert len(spectrum_imports) == 0, "Spectrum without peaks should be skipped"
+
+
+def test_load_from_json_with_minimal_json(tmp_path):
+    filename = tmp_path / "test.json"
+    body = '[{"test_field": "test1", "peaks_json": [[100.0, 10.0], [200.0, 10.0], [300.0, 500.0]]}]'
+
+    with open(filename, 'w') as f:
+        f.write(body)
+
+    spectrum_imports = load_from_json(filename)
+
+    expected = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
+                        intensities=numpy.array([10, 10, 500], dtype="float"),
+                        metadata={"test_field": "test1"})
+    assert spectrum_imports == [expected], "Loaded JSON document not identical to expected Spectrum"
+
+
+def test_save_as_json_with_minimal_json(tmp_path):
+    filename = tmp_path / "test.json"
+    spectrum1 = Spectrum(mz=numpy.array([100, 200, 300], dtype="float"),
+                         intensities=numpy.array([10, 10, 500], dtype="float"),
+                         metadata={"test_field": "test1"})
+
+    save_as_json([spectrum1], filename)
+
+    with open(filename) as f:
+        spectrum_imports = json.load(f)
+
+    expected = [{"test_field": "test1", "peaks_json": [[100.0, 10.0], [200.0, 10.0], [300.0, 500.0]]}]
+    assert spectrum_imports == expected, "Saved Spectrum not identical to expected JSON Document"
