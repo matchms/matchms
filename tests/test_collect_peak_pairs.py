6369c0e4d8bb78d2e5c464879e74269d721073f7
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-06-22 15:10:49 +0200

    fix test (again)

--- tests/test_collect_peak_pairs.py
+++ tests/test_collect_peak_pairs.py
@@ -7,7 +7,7 @@ from matchms.similarity.collect_peak_pairs import collect_peak_pairs
 
 @pytest.mark.parametrize("shift, expected_pairs",
                          [(0.0, [(2, 2, 1.0), (3, 3, 1.0)]),
-                          (-5.0, [(0, 0, 1.0), (1, 1, 1.0)])])
+                          (-5.0, [(0, 0, 0.01), (1, 1, 0.01)])])
 def test_cosine_hungarian_compiled(shift, expected_pairs):
     """Test finding expected peak matches for given tolerance."""
     spec1 = numpy.array([[100, 200, 300, 500],
@@ -23,7 +23,7 @@ def test_cosine_hungarian_compiled(shift, expected_pairs):
 
 @pytest.mark.parametrize("shift, expected_pairs",
                          [(0.2, [(2, 2, 1.0), (3, 3, 1.0)]),
-                          (5.0, [(0, 0, 1.0), (1, 1, 1.0)])])
+                          (-5.0, [(0, 0, 0.01), (1, 1, 0.01)])])
 def test_cosine_hungarian(shift, expected_pairs):
     """Test finding expected peak matches for tolerance=0.2 and given shift."""
     spec1 = numpy.array([[100, 200, 300, 500],
@@ -34,4 +34,4 @@ def test_cosine_hungarian(shift, expected_pairs):
 
     matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=0.2, shift=shift)
     assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == expected_pairs, "Expected different matchin pairs."
+    assert matching_pairs == pytest.approx(expected_pairs, 1e-9), "Expected different matchin pairs."
