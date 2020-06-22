42e0e3bdb4053a5f7f0462443b8df5f0bc38b873
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-06-22 14:34:21 +0200

    changes based on PR review

--- tests/test_collect_peak_pairs.py
+++ tests/test_collect_peak_pairs.py
@@ -1,56 +1,37 @@
 """Test function to collect matching peaks. Run tests both on numba compiled and
 pure Python version."""
 import numpy
+import pytest
 from matchms.similarity.collect_peak_pairs import collect_peak_pairs
 
 
-def test_cosine_hungarian_tolerance_01_compiled():
-    """Test finding expected peak matches within tolerance=0.2."""
+@pytest.mark.parametrize("tolerance, expected_pairs",
+                         [(0.2, [(2, 2, 1.0), (3, 3, 1.0)]),
+                          (5.0, [(0, 0, 1.0), (1, 1, 1.0)])])
+def test_cosine_hungarian_compiled(tolerance, expected_pairs):
+    """Test finding expected peak matches for given tolerance."""
     spec1 = numpy.array([[100, 200, 300, 500],
                          [0.1, 0.1, 1.0, 1.0]], dtype="float").T
 
     spec2 = numpy.array([[105, 205.1, 300, 500.1],
                          [0.1, 0.1, 1.0, 1.0]], dtype="float").T
 
-    matching_pairs = collect_peak_pairs(spec1, spec2, tolerance=0.2)
+    matching_pairs = collect_peak_pairs(spec1, spec2, tolerance=tolerance)
     assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == [(2, 2, 1.0), (3, 3, 1.0)], "Expected different matchin pairs."
+    assert matching_pairs == expected_pairs, "Expected different matchin pairs."
 
 
-def test_cosine_hungarian_tolerance_01_shift_min5_compiled():
-    """Test finding expected peak matches when given a mass_shift of -5.0."""
-    spec1 = numpy.array([[100, 200, 300, 500],
-                         [1.0, 1.0, 0.1, 0.1]], dtype="float").T
-
-    spec2 = numpy.array([[105, 205.1, 300, 500.1],
-                         [1.0, 1.0, 0.1, 0.1]], dtype="float").T
-
-    matching_pairs = collect_peak_pairs(spec1, spec2, tolerance=0.2, shift=-5.0)
-    assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == [(0, 0, 1.0), (1, 1, 1.0)], "Expected different matchin pairs."
-
-
-def test_cosine_hungarian_tolerance_01():
-    """Test finding expected peak matches within tolerance=0.2."""
+@pytest.mark.parametrize("tolerance, expected_pairs",
+                         [(0.2, [(2, 2, 1.0), (3, 3, 1.0)]),
+                          (5.0, [(0, 0, 1.0), (1, 1, 1.0)])])
+def test_cosine_hungarian(tolerance, expected_pairs):
+    """Test finding expected peak matches for given tolerance."""
     spec1 = numpy.array([[100, 200, 300, 500],
                          [0.1, 0.1, 1.0, 1.0]], dtype="float").T
 
     spec2 = numpy.array([[105, 205.1, 300, 500.1],
                          [0.1, 0.1, 1.0, 1.0]], dtype="float").T
 
-    matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=0.2)
+    matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=tolerance)
     assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == [(2, 2, 1.0), (3, 3, 1.0)], "Expected different matchin pairs."
-
-
-def test_cosine_hungarian_tolerance_01_shift_min5():
-    """Test finding expected peak matches when given a mass_shift of -5.0."""
-    spec1 = numpy.array([[100, 200, 300, 500],
-                         [1.0, 1.0, 0.1, 0.1]], dtype="float").T
-
-    spec2 = numpy.array([[105, 205.1, 300, 500.1],
-                         [1.0, 1.0, 0.1, 0.1]], dtype="float").T
-
-    matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=0.2, shift=-5.0)
-    assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == [(0, 0, 1.0), (1, 1, 1.0)], "Expected different matchin pairs."
+    assert matching_pairs == expected_pairs, "Expected different matchin pairs."
