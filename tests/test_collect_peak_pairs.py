afec823a6cd040c66306d5e0e4ef0dc5b8eef23a
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-06-22 18:20:03 +0200

    add pytest.approx

--- tests/test_collect_peak_pairs.py
+++ tests/test_collect_peak_pairs.py
@@ -18,11 +18,11 @@ def test_cosine_hungarian_compiled(shift, expected_pairs):
 
     matching_pairs = collect_peak_pairs(spec1, spec2, tolerance=0.2, shift=shift)
     assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == expected_pairs, "Expected different matchin pairs."
+    assert matching_pairs == [pytest.approx(x, 1e-9) for x in expected_pairs], "Expected different pairs."
 
 
 @pytest.mark.parametrize("shift, expected_pairs",
-                         [(0.2, [(2, 2, 1.0), (3, 3, 1.0)]),
+                         [(0.0, [(2, 2, 1.0), (3, 3, 1.0)]),
                           (-5.0, [(0, 0, 0.01), (1, 1, 0.01)])])
 def test_cosine_hungarian(shift, expected_pairs):
     """Test finding expected peak matches for tolerance=0.2 and given shift."""
@@ -34,4 +34,4 @@ def test_cosine_hungarian(shift, expected_pairs):
 
     matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=0.2, shift=shift)
     assert len(matching_pairs) == 2, "Expected different number of matching peaks"
-    assert matching_pairs == pytest.approx(expected_pairs, 1e-9), "Expected different matchin pairs."
+    assert matching_pairs == [pytest.approx(x, 1e-9) for x in expected_pairs], "Expected different pairs."
