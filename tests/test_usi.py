ed3b98e52d99c3fd68143fd9d5d696b619f47ad3
Simon Rogers <simon.d.rogers@gmail.com>
2020-07-02 17:52:49 +0100

    style..

--- tests/test_usi.py
+++ tests/test_usi.py
@@ -7,10 +7,10 @@ from matchms.importing.load_from_usi import load_from_usi
 @patch('matchms.importing.load_from_usi.requests.get')
 def normal_test(mock_get):
     mock_get.return_value = Mock(ok=True)
-    mock_get.return_value.json.return_value = {'peaks': [[1., 2.],[3., 4.]]}
+    mock_get.return_value.json.return_value = {'peaks': [[1., 2.], [3., 4.]]}
     spec = load_from_usi('something')
     expected_metadata = {'usi': 'something', 'url': 'https://metabolomics-usi.ucsd.edu/json/?usi=something', 'precursor_mz': None}
-    expected = Spectrum(np.array([1.,3.]),np.array([2.,4.]),expected_metadata)
+    expected = Spectrum(np.array([1., 3.]), np.array([2., 4.]), expected_metadata)
     assert spec == expected
 
 
