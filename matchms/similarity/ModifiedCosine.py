fcb1a6f1168e2df248e37162f063be3f3d7ceb9c
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-07-16 11:53:13 +0200

    update documentation code examples to fixed cosine score

--- matchms/similarity/ModifiedCosine.py
+++ matchms/similarity/ModifiedCosine.py
@@ -43,7 +43,7 @@ class ModifiedCosine:
 
     .. testoutput::
 
-        Modified cosine score is 0.52 with 1 matched peaks
+        Modified cosine score is 0.83 with 1 matched peaks
 
     """
     def __init__(self, tolerance: float = 0.1):
