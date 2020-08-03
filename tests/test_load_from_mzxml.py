1bd08f84fdab5b851b32ba76e64f08e24f920b8d
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-08-03 12:08:38 +0200

    add load_from_mzxml

---
+++ tests/test_load_from_mzxml.py
@@ -0,0 +1,14 @@
+import os
+from matchms.importing import load_from_mzxml
+
+
+def test_load_from_mzxml():
+    """Test parsing of mzxml file to spectrum objects"""
+
+    module_root = os.path.dirname(__file__)
+    mzxml_file = os.path.join(module_root, "testdata.mzxml")
+    spectrum = next(load_from_mzxml(mzxml_file))
+
+    assert int(spectrum.get("precursor_mz")) == 343, "Expected different precursor m/z"
+    assert spectrum.get("charge") == -1, "Expected different charge."
+    assert len(spectrum.peaks) == 50
