cfc3743ed2288c78e53e051555a83bdc98f862b5
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2020-08-18 12:28:06 +0200

    Update matchms/exporting/save_as_mgf.py
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- matchms/exporting/save_as_mgf.py
+++ matchms/exporting/save_as_mgf.py
@@ -12,6 +12,7 @@ def save_as_mgf(spectrums: List[Spectrum], filename: str):
 
     .. code-block:: python
 
+        import numpy
         from matchms import Spectrum
         from matchms.exporting import save_as_mgf
 
