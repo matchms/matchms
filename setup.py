44e38713975078e435b943f2dfed064288d72fd2
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-10-08 21:28:00 +0200

    remove outdated imrequirements

--- setup.py
+++ setup.py
@@ -59,7 +59,6 @@ setup(
                             "prospector[with_pyroma]",
                             "pytest",
                             "pytest-cov",
-                            "recommonmark",
                             "sphinx>=3.0.0,!=3.2.0,<4.0.0",
                             "sphinx_rtd_theme",
                             "sphinxcontrib-apidoc",
