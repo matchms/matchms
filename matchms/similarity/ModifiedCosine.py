2d484cac822f21428b5b51b4af02f3c10d7b9dc2
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-11-30 17:05:15 +0100

    linting

--- matchms/similarity/ModifiedCosine.py
+++ matchms/similarity/ModifiedCosine.py
@@ -96,9 +96,9 @@ class ModifiedCosine(BaseSimilarity):
                                                intensity_power=self.intensity_power)
 
             if zero_pairs.shape[1] != 3:
-                zero_pairs = numpy.zeros((0,3))
+                zero_pairs = numpy.zeros((0, 3))
             if nonzero_pairs.shape[1] != 3:
-                nonzero_pairs = numpy.zeros((0,3))
+                nonzero_pairs = numpy.zeros((0, 3))
             matching_pairs = numpy.concatenate((zero_pairs, nonzero_pairs), axis=0)
             if matching_pairs.shape[0] > 0:
                 matching_pairs = matching_pairs[numpy.argsort(matching_pairs[:, 2])[::-1], :]
