95d04e73c32bdf383737ee5a76b012010068f2f1
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-12-01 10:20:51 +0100

    update test function

--- tests/test_spectrum_similarity_functions.py
+++ tests/test_spectrum_similarity_functions.py
@@ -8,8 +8,7 @@ from matchms.similarity.spectrum_similarity_functions import score_best_matches
 
 @pytest.mark.parametrize("shift, expected_pairs, expected_matches",
                          [(0.0, [[2., 2., 1.], [3., 3., 1.]], (2, 3)),
-                          (-5.0, [[0., 0., 0.01], [1., 1., 0.01]], (2, 3)),
-                          (-20.0, [], (0, 0))])
+                          (-5.0, [[0., 0., 0.01], [1., 1., 0.01]], (2, 3))])
 def test_collect_peak_pairs_compiled(shift, expected_pairs, expected_matches):
     """Test finding expected peak matches for given tolerance."""
     spec1 = numpy.array([[100, 200, 300, 500],
@@ -25,8 +24,7 @@ def test_collect_peak_pairs_compiled(shift, expected_pairs, expected_matches):
 
 @pytest.mark.parametrize("shift, expected_pairs, expected_matches",
                          [(0.0, [[2., 2., 1.], [3., 3., 1.]], (2, 3)),
-                          (-5.0, [[0., 0., 0.01], [1., 1., 0.01]], (2, 3)),
-                          (-20.0, [], (0, 0))])
+                          (-5.0, [[0., 0., 0.01], [1., 1., 0.01]], (2, 3))])
 def test_collect_peak_pairs(shift, expected_pairs, expected_matches):
     """Test finding expected peak matches for tolerance=0.2 and given shift."""
     spec1 = numpy.array([[100, 200, 300, 500],
@@ -38,6 +36,22 @@ def test_collect_peak_pairs(shift, expected_pairs, expected_matches):
     matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=0.2, shift=shift)
     assert matching_pairs.shape == expected_matches, "Expected different number of matching peaks"
     assert numpy.allclose(matching_pairs, numpy.array(expected_pairs), atol=1e-8), "Expected different values."
+
+
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_collect_peak_pairs_no_matches(numba_compiled):
+    shift = -20.0
+    """Test function for no matching peaks."""
+    spec1 = numpy.array([[100, 200, 300, 500],
+                         [0.1, 0.1, 1.0, 1.0]], dtype="float").T
+
+    spec2 = numpy.array([[105, 205.1, 300, 500.1],
+                         [0.1, 0.1, 1.0, 1.0]], dtype="float").T
+    if numba_compiled:
+        matching_pairs = collect_peak_pairs(spec1, spec2, tolerance=0.2, shift=shift)
+    else:
+        matching_pairs = collect_peak_pairs.py_func(spec1, spec2, tolerance=0.2, shift=shift)
+    assert matching_pairs is None, "Expected pairs to be None."
 
 
 @pytest.mark.parametrize("matching_pairs, expected_score",
