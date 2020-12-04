f28fda4df15038770a8044962993b3329585404d
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-12-04 09:12:58 +0100

    add and update tests

---
+++ tests/test_add_precursor_mz.py
@@ -0,0 +1,51 @@
+import numpy
+from matchms import Spectrum
+from matchms.filtering import add_precursor_mz
+
+
+def test_add_precursor_mz():
+    """Test if precursor_mz is correctly derived. Here nothing should change."""
+    mz = numpy.array([], dtype='float')
+    intensities = numpy.array([], dtype='float')
+    metadata = {"precursor_mz": 444.0}
+    spectrum_in = Spectrum(mz=mz,
+                           intensities=intensities,
+                           metadata=metadata)
+
+    spectrum = add_precursor_mz(spectrum_in)
+
+    assert spectrum.get("precursor_mz") == 444.0, "Expected different precursor_mz."
+
+
+def test_add_precursor_mz_only_pepmass_present():
+    """Test if precursor_mz is correctly derived if only pepmass is present."""
+    mz = numpy.array([], dtype='float')
+    intensities = numpy.array([], dtype='float')
+    metadata = {"pepmass": (444.0, 10)}
+    spectrum_in = Spectrum(mz=mz,
+                           intensities=intensities,
+                           metadata=metadata)
+
+    spectrum = add_precursor_mz(spectrum_in)
+
+    assert spectrum.get("precursor_mz") == 444.0, "Expected different precursor_mz."
+
+
+def test_add_precursor_mz_no_precursor_mz():
+    """Test if precursor_mz is correctly derived if "precursor_mz" is str."""
+    mz = numpy.array([], dtype='float')
+    intensities = numpy.array([], dtype='float')
+    metadata = {"precursor_mz": "444.0"}
+    spectrum_in = Spectrum(mz=mz,
+                           intensities=intensities,
+                           metadata=metadata)
+
+    spectrum = add_precursor_mz(spectrum_in)
+
+    assert spectrum.get("precursor_mz") == 444.0, "Expected different precursor_mz."
+
+def test_empty_spectrum():
+    spectrum_in = None
+    spectrum = add_precursor_mz(spectrum_in)
+
+    assert spectrum is None, "Expected different handling of None spectrum."
