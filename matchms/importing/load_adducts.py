8768e76bbd9345f6452d79a7026fc6bd41a80c2f
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-02-14 20:31:36 +0100

    linting and added test for coverage

--- matchms/importing/load_adducts.py
+++ matchms/importing/load_adducts.py
@@ -7,7 +7,7 @@ import numpy
 
 
 @lru_cache(maxsize=4)
-def load_adducts_dict() -> Dict:
+def load_adducts_dict() -> Dict[dict]:
     """Load dictionary of known adducts containing the adduct mass and charge.
     Makes sure that file loading is cached.
 
@@ -15,43 +15,8 @@ def load_adducts_dict() -> Dict:
     https://fiehnlab.ucdavis.edu/staff/kind/metabolomics/ms-adduct-calculator/
     and was extended by F.Huber and JJJ.v.d.Hooft.
     """
-    def convert_if_possible(entry, expected_type=float):
-        try:
-            entry = expected_type(entry)
-        except ValueError:
-            pass
-        return entry
-
-    def convert_and_fill_dict(adduct_dict):
-        """Convert string entries to int/float and fill missing entries ('n/a')
-        with best basic guesses."""
-        filled_dict = dict()
-        for adduct, values in adduct_dict.items():
-            ionmode = values["ionmode"]
-            charge = convert_if_possible(values["charge"], int)
-            mass_multiplier = convert_if_possible(values["mass_multiplier"], float)
-            correction_mass = convert_if_possible(values["correction_mass"], float)
-
-            if not isinstance(charge, int):
-                charge = 1 * (ionmode == "positive") - 1 * (ionmode == "negative")
-            values["charge"] = charge
-
-            if not isinstance(mass_multiplier, float):
-                mass_multiplier = 1.0
-            values["mass_multiplier"] = mass_multiplier
-
-            if not isinstance(correction_mass, float):
-                correction_mass = 1.007276 * numpy.sign(charge)
-            values["correction_mass"] = correction_mass
-
-            filled_dict[adduct] = values
-        return filled_dict
-
     known_adducts_file = os.path.join(os.path.dirname(__file__), "..", "data", "known_adducts_table.csv")
-
-    if not os.path.isfile(known_adducts_file):
-        print("Could not find .csv file with known adducts.")
-        return None
+    assert os.path.isfile(known_adducts_file), "Could not find known_adducts_table.csv."
 
     with open(known_adducts_file, newline='', encoding='utf-8-sig') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -60,19 +25,52 @@ def load_adducts_dict() -> Dict:
             assert "adduct" in row
             adducts_dict[row["adduct"]] = {x[0]: x[1] for x in row.items() if x[0] != "adduct"}
 
-    return convert_and_fill_dict(adducts_dict)
+    return _convert_and_fill_dict(adducts_dict)
 
 
 @lru_cache(maxsize=4)
-def load_known_adduct_conversions() -> Dict:
+def load_known_adduct_conversions() -> Dict[dict]:
     """Load dictionary of known adduct conversions. Makes sure that file loading is cached.
     """
     adduct_conversions_file = os.path.join(os.path.dirname(__file__), "..", "data", "known_adduct_conversions.json")
-
-    if not os.path.isfile(adduct_conversions_file):
-        print("Could not find json file with known adduct conversions.")
-        return None
+    assert os.path.isfile(adduct_conversions_file), "Could not find known_adduct_conversions.json."
 
     known_adduct_conversions = json.load(open(adduct_conversions_file, "r"))
 
     return known_adduct_conversions
+
+
+def _convert_and_fill_dict(adduct_dict: Dict[dict]) -> Dict[dict]:
+    """Convert string entries to int/float and fill missing entries ('n/a')
+    with best basic guesses."""
+    def _convert_if_possible(entry, expected_type=float):
+        """Convert *entry* to expected_type if possible.
+        If not returns unchanged entry.
+        """
+        try:
+            entry = expected_type(entry)
+        except ValueError:
+            pass
+        return entry
+
+    filled_dict = dict()
+    for adduct, values in adduct_dict.items():
+        ionmode = values["ionmode"]
+        charge = _convert_if_possible(values["charge"], int)
+        mass_multiplier = _convert_if_possible(values["mass_multiplier"], float)
+        correction_mass = _convert_if_possible(values["correction_mass"], float)
+
+        if not isinstance(charge, int):
+            charge = 1 * (ionmode == "positive") - 1 * (ionmode == "negative")
+        values["charge"] = charge
+
+        if not isinstance(mass_multiplier, float):
+            mass_multiplier = 1.0
+        values["mass_multiplier"] = mass_multiplier
+
+        if not isinstance(correction_mass, float):
+            correction_mass = 1.007276 * numpy.sign(charge)
+        values["correction_mass"] = correction_mass
+
+        filled_dict[adduct] = values
+    return filled_dict
--- tests/test_load_adducts.py
+++ tests/test_load_adducts.py
@@ -1,6 +1,7 @@
 import numpy
 from matchms.importing import load_adducts_dict
 from matchms.importing import load_known_adduct_conversions
+from matchms.importing.load_adducts import _convert_and_fill_dict
 
 
 def test_load_adducts_dict():
@@ -15,6 +16,21 @@ def test_load_adducts_dict():
     assert known_adducts["[M]+"]["charge"] == 1, "Expected different added entry"
     assert numpy.allclose(known_adducts["[M]-"]["correction_mass"], -1.007276, atol=1e-5), \
         "Expected different added entry"
+
+
+def test_convert_and_fill_dict():
+    """Test if conversion is done right."""
+    test_dict = dict()
+    test_dict["[M]+"] = {'ionmode': 'negative',
+                         'charge': "n/a",
+                         'mass_multiplier': "n/a",
+                         'correction_mass': "n/a"}
+    converted_dict = _convert_and_fill_dict(test_dict)
+    assert isinstance(converted_dict["[M]+"], dict), "Expected dictionary"
+    assert converted_dict["[M]+"]["charge"] == -1, "Expected charge of -1"
+    assert converted_dict["[M]+"]["mass_multiplier"] == 1, "Expected mass_multiplier of 1"
+    assert numpy.allclose(converted_dict["[M]+"]["correction_mass"], -1.007276, atol=1e-5), \
+        "Expected correction_mass to be changed to -1.007276"
 
 
 def test_load_known_adduct_conversions():
