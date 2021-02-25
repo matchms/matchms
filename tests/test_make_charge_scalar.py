8ecf09327c966358f490000c8c2921ccc607466a
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-02-25 10:14:45 +0100

    add test for make_charge_scalar

---
+++ tests/test_make_charge_scalar.py
@@ -0,0 +1,16 @@
+import pytest
+from matchms import Spectrum
+from matchms.filtering import make_charge_scalar
+
+
+@pytest.mark.parametrize("input_charge, corrected_charge", [('+1', 1),
+                                                            ('1', 1),
+                                                            (' 1 ', 1),
+                                                            ('-1', -1)])
+def test_make_charge_scalar(input_charge, corrected_charge):
+    spectrum_in = Spectrum(mz=numpy.array([100, 200.]),
+                          intensities=numpy.array([0.7, 0.1]),
+                          metadata={'charge': input_charge})
+
+    spectrum = make_charge_scalar(spectrum_in)
+    assert(spectrum.get("charge") == corrected_charge), "Expected different charge integer"
