5fcdc50c407854b3a347851a14cdec0b74afa352
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-02-25 10:35:49 +0100

    linting

--- tests/test_make_charge_scalar.py
+++ tests/test_make_charge_scalar.py
@@ -10,8 +10,8 @@ from matchms.filtering import make_charge_scalar
                                                             ('-1', -1)])
 def test_make_charge_scalar(input_charge, corrected_charge):
     spectrum_in = Spectrum(mz=numpy.array([100, 200.]),
-                          intensities=numpy.array([0.7, 0.1]),
-                          metadata={'charge': input_charge})
+                           intensities=numpy.array([0.7, 0.1]),
+                           metadata={'charge': input_charge})
 
     spectrum = make_charge_scalar(spectrum_in)
     assert(spectrum.get("charge") == corrected_charge), "Expected different charge integer"
