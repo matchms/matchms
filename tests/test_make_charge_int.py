ca1d2ee7ce2032036d2d557d0271b95ca7e77d02
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-03-01 11:12:54 +0100

    add new make_charge_int

---
+++ tests/test_make_charge_int.py
@@ -0,0 +1,28 @@
+import numpy
+import pytest
+from matchms import Spectrum
+from matchms.filtering import make_charge_int
+
+
+@pytest.mark.parametrize("input_charge, corrected_charge", [('+1', 1),
+                                                            ('1', 1),
+                                                            (' 1 ', 1),
+                                                            ('-2', -2),
+                                                            ([-1, "stuff"], -1),
+                                                            (['-3'], -3),
+                                                            ('n/a', 'n/a')])
+def test_make_charge_int(input_charge, corrected_charge):
+    """Test if example inputs are correctly converted"""
+    spectrum_in = Spectrum(mz=numpy.array([100, 200.]),
+                           intensities=numpy.array([0.7, 0.1]),
+                           metadata={'charge': input_charge})
+
+    spectrum = make_charge_int(spectrum_in)
+    assert(spectrum.get("charge") == corrected_charge), "Expected different charge integer"
+
+
+def test_empty_spectrum():
+    spectrum_in = None
+    spectrum = make_charge_int(spectrum_in)
+
+    assert spectrum is None, "Expected different handling of None spectrum."
