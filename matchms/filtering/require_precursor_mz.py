c0b0b7086e0696f72f6cb72a154a8d4f75f3dd7a
Niek de Jonge <n.dejonge@esciencecenter.nl>
2021-03-04 13:40:31 +0100

    Added assert function to check if add_precursor_mz was run

--- matchms/filtering/require_precursor_mz.py
+++ matchms/filtering/require_precursor_mz.py
@@ -19,7 +19,12 @@ def require_precursor_mz(spectrum_in: SpectrumType
 
     precursor_mz = spectrum.get("precursor_mz", None)
     if precursor_mz is None:
+        pepmass = spectrum.get("pepmass", None)
+        assert pepmass is None or not isinstance(pepmass[0], (float, int)), \
+            "Found 'pepmass' but no 'precursor_mz'. " \
+            "Consider applying 'add_precursor_mz' filter first."
         return None
+
     assert isinstance(precursor_mz, (float, int)), \
         ("Expected 'precursor_mz' to be a scalar number.",
          "Consider applying 'add_precursor_mz' filter first.")
