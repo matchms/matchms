9d2ae50cf474b7b45d28999d4057473adcb33b7b
hechth <helge.hecht@gmx.de>
2021-04-13 16:18:29 +0200

    Added list of accepted keys to read precursor mz values

--- matchms/filtering/add_precursor_mz.py
+++ matchms/filtering/add_precursor_mz.py
@@ -1,4 +1,17 @@
+from typing import Iterable
+from typing import TypeVar
 from ..typing import SpectrumType
+
+
+_T = TypeVar('_T')
+_accepted_keys = ["precursor_mz", "precursormz", "precursor_mass"]
+
+
+def get_first_common_element(first: Iterable[_T], second: Iterable[_T]) -> _T:
+    """ Get first common element from two lists.
+    Returns 'None' if there are no common elements.
+    """
+    return next((item for item in first if item in second), None)
 
 
 def add_precursor_mz(spectrum_in: SpectrumType) -> SpectrumType:
@@ -12,10 +25,14 @@ def add_precursor_mz(spectrum_in: SpectrumType) -> SpectrumType:
 
     spectrum = spectrum_in.clone()
 
-    if isinstance(spectrum.get("precursor_mz", None), str):
-        spectrum.set("precursor_mz", float(spectrum.get("precursor_mz").strip()))
-    elif spectrum.get("precursor_mz", None) is None:
-        pepmass = spectrum.get("pepmass", None)
+    precursor_mz_key = get_first_common_element(spectrum.metadata.keys(), _accepted_keys)
+    precursor_mz = spectrum.get(precursor_mz_key)
+
+    if isinstance(precursor_mz, str):
+        precursor_mz = float(precursor_mz.strip())
+        spectrum.set("precursor_mz", precursor_mz)
+    elif precursor_mz is None:
+        pepmass = spectrum.get("pepmass")
         if pepmass is not None and isinstance(pepmass[0], float):
             spectrum.set("precursor_mz", pepmass[0])
         else:
