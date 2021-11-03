68aa8cf72af98be9473707455c7b0c1d2d827a49
bernardolk <bernardo.knackfuss@gmail.com>
2021-11-03 10:57:44 -0300

    changed types, test and docstring

--- matchms/importing/load_from_mgf.py
+++ matchms/importing/load_from_mgf.py
@@ -1,30 +1,30 @@
-from io import BytesIO
-from typing import Generator, Union
+from typing import Generator, Union, TextIO
 import numpy
 from pyteomics.mgf import MGF
 from ..Spectrum import Spectrum
 
 
-def load_from_mgf(source: Union[str, BytesIO]) -> Generator[Spectrum, None, None]:
+def load_from_mgf(source: Union[str, TextIO]) -> Generator[Spectrum, None, None]:
     """Load spectrum(s) from mgf file.
 
-    source param accepts both a path to a mgf file or a bytesIO object from a preloaded MGF file
+    source param accepts both a path to a mgf file or a file-like object from a preloaded MGF file
 
-    Example:
+    Examples:
 
     .. code-block:: python
 
         from matchms.importing import load_from_mgf
 
         file_mgf = "pesticides.mgf"
-        spectrums = list(load_from_mgf(file_mgf))
+        spectra_from_path = list(load_from_mgf(file_mgf))
+
+        // or you can read the file in your application
+        with open(file_mgf, 'r') as spectra_file:
+            spectra_from_file = list(load_from_mgf(spectra_file))
 
     """
 
-
-    mgf = MGF(source, convert_arrays=1)
-
-    for pyteomics_spectrum in mgf:
+    for pyteomics_spectrum in MGF(source, convert_arrays=1):
 
         metadata = pyteomics_spectrum.get("params", None)
         mz = pyteomics_spectrum["m/z array"]
--- tests/test_load_from_mgf.py
+++ tests/test_load_from_mgf.py
@@ -1,24 +1,26 @@
 import os
-from io import BytesIO
 
+from matchms import Spectrum
 from matchms.importing import load_from_mgf
 
 
-def test_load_from_mgf():
+def test_load_from_mgf_using_filepath():
     module_root = os.path.join(os.path.dirname(__file__), "..")
     spectra_file = os.path.join(module_root, "tests", "pesticides.mgf")
 
-    spectra = load_from_mgf(spectra_file)
+    spectra = list(load_from_mgf(spectra_file))
 
-    assert len(list(spectra)) > 0
+    assert len(spectra) > 0
+    assert isinstance(spectra[0], Spectrum)
 
 
-def test_load_from_mgf_file_like_obj():
+def test_load_from_mgf_using_file():
     module_root = os.path.join(os.path.dirname(__file__), "..")
-    spectra_file = os.path.join(module_root, "tests", "pesticides.mgf")
+    spectra_filepath = os.path.join(module_root, "tests", "pesticides.mgf")
 
-    with open(spectra_file, 'rb') as spectra_bytes:
-        buf = BytesIO(spectra_bytes.read())
-        spectra = load_from_mgf(spectra_bytes)
+    with open(spectra_filepath, 'r') as spectra_file:
+        spectra = list(load_from_mgf(spectra_file))
 
-        assert len(list(spectra)) > 0
+        assert len(spectra) > 0
+        assert isinstance(spectra[0], Spectrum)
+
