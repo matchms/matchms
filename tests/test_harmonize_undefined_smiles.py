5fce457b42ecdc30c19b047b761e5ddbf5ecfdf5
hechth <helge.hecht@recetox.muni.cz>
2021-11-15 10:21:26 +0100

    Refactored harmonize undefined smiles

--- tests/test_harmonize_undefined_smiles.py
+++ tests/test_harmonize_undefined_smiles.py
@@ -1,76 +1,24 @@
 import numpy
+import pytest
 from matchms import Spectrum
 from matchms.filtering import harmonize_undefined_smiles
+from .builder_Spectrum import SpectrumBuilder
 
 
-def test_harmonize_undefined_smiles_empty_string():
+@pytest.mark.parametrize("metadata, aliases, undefined, expected", [
+    [{"smiles": ""}, ["", "N/A", "NA", "n/a", "no data"], "", ""],
+    [{"smiles": "n/a"}, ["", "N/A", "NA", "n/a", "no data"], "", ""],
+    [{"smiles": "N/A"}, ["", "N/A", "NA", "n/a", "no data"], "", ""],
+    [{"smiles": "NA"}, ["", "N/A", "NA", "n/a", "no data"], "", ""],
+    [{"smiles": "no data"}, ["", "N/A", "NA", "n/a", "no data"], "", ""],
+    [{"smiles": "nan"}, ["nodata", "NaN", "Nan", "nan"], "", ""],
+    [{"smiles": "nan"}, ["nodata", "NaN", "Nan", "nan"], "n/a", "n/a"]
+])
+def test_harmonize_undefined_smiles(metadata, aliases, undefined, expected):
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
 
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": ""})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in)
-    assert spectrum.get("smiles") == ""
-
-
-def test_harmonize_undefined_smiles_na_1():
-
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": "n/a"})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in)
-    assert spectrum.get("smiles") == ""
-
-
-def test_harmonize_undefined_smiles_na_2():
-
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": "N/A"})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in)
-    assert spectrum.get("smiles") == ""
-
-
-def test_harmonize_undefined_smiles_na_3():
-
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": "NA"})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in)
-    assert spectrum.get("smiles") == ""
-
-
-def test_harmonize_undefined_smiles_no_data():
-
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": "no data"})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in)
-    assert spectrum.get("smiles") == ""
-
-
-def test_harmonize_undefined_smiles_alias_nan():
-
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": "nan"})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in, aliases=["nodata", "NaN", "Nan", "nan"])
-    assert spectrum.get("smiles") == ""
-
-
-def test_harmonize_undefined_smiles_alias_nan_undefined_is_na():
-
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"smiles": "nan"})
-
-    spectrum = harmonize_undefined_smiles(spectrum_in, aliases=["nodata", "NaN", "Nan", "nan"], undefined="n/a")
-    assert spectrum.get("smiles") == "n/a"
+    spectrum = harmonize_undefined_smiles(spectrum_in, aliases=aliases, undefined=undefined)
+    assert spectrum.get("smiles") == expected
 
 
 def test_empty_spectrum():
