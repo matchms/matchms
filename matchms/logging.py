1076a086ed21a3e276746e259e01f6317f7c310b
florian-huber <florian.huber@hs-duesseldorf.de>
2021-11-29 12:45:56 +0100

    add doc

--- matchms/logging.py
+++ matchms/logging.py
@@ -1,3 +1,26 @@
+"""Matchms logger.
+
+Matchms functions and method report unexpected or undesired behavior as
+logging WARNING, and additional information as INFO.
+The default logging level is set to WARNING. If you want to output additional
+logging messages, you can lower the logging level to INFO using setLevel:
+
+.. code-block:: python
+
+    import logging
+    from matchms import calculate_scores, Spectrum
+
+
+    logging.getLogger("matchms").setLevel(logging.INFO)
+
+If you want to suppress logging warnings, you can also raise the logging level
+to ERROR by:
+
+.. code-block:: python
+
+    logging.getLogger("matchms").setLevel(logging.ERROR)
+
+"""
 import logging
 import logging.config
 import os
