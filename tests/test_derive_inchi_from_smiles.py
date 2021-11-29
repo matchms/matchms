97651bca50bdcf5f9d7fd0cb7974ae0460c43295
florian-huber <florian.huber@hs-duesseldorf.de>
2021-11-29 15:01:15 +0100

    add test cases

--- tests/test_derive_inchi_from_smiles.py
+++ tests/test_derive_inchi_from_smiles.py
@@ -1,5 +1,6 @@
 import numpy
 import pytest
+from testfixtures import LogCapture
 from matchms import Spectrum
 from matchms.filtering import derive_inchi_from_smiles
 
@@ -15,6 +16,23 @@ def test_derive_inchi_from_smiles():
     spectrum = derive_inchi_from_smiles(spectrum_in)
     inchi = spectrum.get("inchi").replace('"', '')
     assert inchi == 'InChI=1S/C6H12/c1-2-4-6-5-3-1/h1-6H2', "Expected different InChI"
+
+
+def test_derive_inchi_from_defect_smiles():
+    """Test if conversion to inchi works when only smiles is given.
+    """
+    pytest.importorskip("rdkit")
+    spectrum_in = Spectrum(mz=numpy.array([], dtype='float'),
+                           intensities=numpy.array([], dtype='float'),
+                           metadata={"smiles": "CX1CCCCC1"})
+
+    with LogCapture() as log:
+        spectrum = derive_inchi_from_smiles(spectrum_in)
+    inchi = spectrum.get("inchi", None)
+    assert inchi is None, "Expected no InChI"
+    log.check(
+        ('matchms', 'WARNING', "Could not convert smiles CX1CCCCC1 to InChI.")
+    )
 
 
 def test_empty_spectrum():
--- tests/test_derive_smiles_from_inchi.py
+++ tests/test_derive_smiles_from_inchi.py
@@ -1,5 +1,6 @@
 import numpy
 import pytest
+from testfixtures import LogCapture
 from matchms import Spectrum
 from matchms.filtering import derive_smiles_from_inchi
 
@@ -15,6 +16,23 @@ def test_derive_smiles_from_inchi():
 
     spectrum = derive_smiles_from_inchi(spectrum_in)
     assert spectrum.get("smiles") == "C1CCCCC1", "Expected different smiles"
+
+
+def test_derive_smiles_from_defect_inchi():
+    """Test if conversion to smiles works when only inchi is given.
+    """
+    pytest.importorskip("rdkit")
+    spectrum_in = Spectrum(mz=numpy.array([], dtype='float'),
+                           intensities=numpy.array([], dtype='float'),
+                           metadata={"inchi": '"InChI=1S/C6H12/c1-2-XA4-6-5-3-1/h1-6H2"',
+                                     "smiles": ""})
+
+    with LogCapture() as log:
+        spectrum = derive_smiles_from_inchi(spectrum_in)
+    assert spectrum.get("smiles", None) == "", "Expected no smiles"
+    log.check(
+        ('matchms', 'WARNING', "Could not convert InChI InChI=1S/C6H12/c1-2-XA4-6-5-3-1/h1-6H2 to smiles.")
+    )
 
 
 def test_empty_spectrum():
