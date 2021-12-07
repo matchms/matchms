97f1ce61161c8a3d8cbe9e02c8ae5179b4c96a88
hechth <helge.hecht@recetox.muni.cz>
2021-12-07 17:21:50 +0100

    refactored add_compound_name

--- tests/test_add_compound_name.py
+++ tests/test_add_compound_name.py
@@ -1,44 +1,27 @@
 import numpy
+import pytest
 from testfixtures import LogCapture
-from matchms import Spectrum
 from matchms.filtering import add_compound_name
+from matchms.logging_functions import reset_matchms_logger
+from .builder_Spectrum import SpectrumBuilder
 
 
-def test_add_compound_name_entry_name():
-    """Test filled name field."""
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"name": "Testospectrum"})
-
-    spectrum = add_compound_name(spectrum_in)
-
-    assert spectrum.get("compound_name") == "Testospectrum", "Expected different compound name."
-
-
-def test_add_compound_name_entry_title():
-    """Test filled name field."""
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"title": "Testospectrum"})
-
-    spectrum = add_compound_name(spectrum_in)
-
-    assert spectrum.get("compound_name") == "Testospectrum", "Expected different compound name."
-
-
-def test_add_compound_name_missing_entry():
-    """Test filled name field."""
-    spectrum_in = Spectrum(mz=numpy.array([], dtype="float"),
-                           intensities=numpy.array([], dtype="float"),
-                           metadata={"othername": "Testospectrum"})
+@pytest.mark.parametrize("metadata, expected, check_log", [
+    [{"name": "Testospectrum"}, "Testospectrum", False],
+    [{"title": "Testospectrum"}, "Testospectrum", False],
+    [{"othername": "Testospectrum"}, None, True]
+])
+def test_add_compound_name(metadata, expected, check_log):
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
 
     with LogCapture() as log:
         spectrum = add_compound_name(spectrum_in)
 
-    assert spectrum.get("compound_name", None) is None, "Expected no compound name."
-    log.check(
-        ('matchms', 'WARNING', 'No compound name found in metadata.')
-    )
+    assert spectrum.get(
+        "compound_name") == expected, "Expected no compound name."
+    if check_log:
+        log.check(('matchms', 'WARNING', 'No compound name found in metadata.'))
+        reset_matchms_logger()
 
 
 def test_empty_spectrum():
