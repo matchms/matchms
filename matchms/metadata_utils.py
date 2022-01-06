d23f6f4fe3ec3ad5715702f339855571c80cb2d7
florian-huber <florian.huber@hs-duesseldorf.de>
2022-01-06 21:43:56 +0100

    remove duplicates from regexp

--- matchms/metadata_utils.py
+++ matchms/metadata_utils.py
@@ -96,7 +96,7 @@ def is_valid_inchi(inchi: str) -> bool:
     if inchi is None:
         return False
     inchi = inchi.strip('"')
-    regexp = r"(InChI=1|1)(S\/|\/)[0-9a-zA-Z\.]{2,}\/(c|h)[0-9]"
+    regexp = r"(InChI=1|1)(S\/|\/)[0-9a-zA-Z\.]{2,}\/[ch]{1}[0-9]"
     if not re.search(regexp, inchi):
         return False
     # Proper chemical test
@@ -123,7 +123,7 @@ def is_valid_smiles(smiles: str) -> bool:
     if smiles is None:
         return False
 
-    regexp = r"^([^J][0-9BCOHNSPIFKcons@+\-\[\]\(\)\\\/%=#$,.~&!|Si|Se|Br|Mg|Na|Cl|Al]{3,})$"
+    regexp = r"^([^J][0-9ABCOHNMSPIFKiergalcons@+\-\[\]\(\)\\\/%=#$,.~&!]{3,})$"
     if not re.match(regexp, smiles):
         return False
 
@@ -256,7 +256,7 @@ def looks_like_adduct(adduct):
         return True
 
     # Expect format like: "[2M-H]" or "[2M+Na]+"
-    regexp1 = r"^\[(([0-4]M)|(M[0-9])|(M))((Br)|(Br81)|(Cl)|(Cl37)|(S)){0,}[+-0-9][A-Z0-9\+\-\(\)|(Na)|(Ca)|(Mg)|(Cl)|(Li)|(Br)|(Ser)]{1,}[\]0-4+-]{1,4}"
+    regexp1 = r"^\[(([0-4]M)|(M[0-9])|(M))((Br)|(Br81)|(Cl)|(Cl37)|(S)){0,}[+-0-9][A-Z0-9\+\-\(\)NaCMglLiBrSe]{1,}[\]0-4+-]{1,4}"
     return re.search(regexp1, adduct) is not None
 
 
