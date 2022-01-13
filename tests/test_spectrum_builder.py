71fc13ef2055535c0912fcd45c60870952bd2bb4
florian-huber <florian.huber@hs-duesseldorf.de>
2022-01-13 11:46:30 +0100

    linting

--- tests/test_spectrum_builder.py
+++ tests/test_spectrum_builder.py
@@ -20,7 +20,7 @@ def test_spectrum_builder_only_metadata(metadata):
 
 @pytest.mark.parametrize("mz, intensities, metadata", [
     [[10.1, 20.2], [0.5, 1], {}],
-    [[10.1, 20.2], [0.5, 1], {"precursor_mz" : 150.5}],
+    [[10.1, 20.2], [0.5, 1], {"precursor_mz": 150.5}],
 ])
 def test_spectrum_builder_all(mz, intensities, metadata):
     spectrum_1 = SpectrumBuilder().with_mz(mz).with_intensities(intensities).with_metadata(metadata).build()
