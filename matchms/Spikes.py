e33ea0da8d7012fd30164414b7a61db82d004776
florian-huber <florian.huber@hs-duesseldorf.de>
2022-01-25 09:26:30 +0100

    linting

--- matchms/Spikes.py
+++ matchms/Spikes.py
@@ -5,4 +5,3 @@ from matchms.Fragments import Fragments
 @deprecated(version='0.14.0', reason="Class was renamed to ``Fragments``.")
 class Spikes(Fragments):
     """Deprecated! Please use :class:`~matchms.Fragments` instead."""
-    pass
