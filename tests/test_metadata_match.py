ee5ad6a003233ff3a208c2d71021a42fcbfcaf2a
florian-huber <florian.huber@hs-duesseldorf.de>
2022-02-09 15:05:34 +0100

    add tests for numerical comparisons

--- tests/test_metadata_match.py
+++ tests/test_metadata_match.py
@@ -14,7 +14,7 @@ def spectrums():
     metadata3 = {"instrument_type": "orbitrap",
                  "retention_time": 105.}
     metadata4 = {"instrument_type": "unknown",
-                 "retention_time": 99.}
+                 "retention_time": 99.1}
 
     s1 = SpectrumBuilder().with_metadata(metadata1).build()
     s2 = SpectrumBuilder().with_metadata(metadata2).build()
@@ -43,3 +43,20 @@ def test_metadata_match_strings_wrong_method(spectrums, caplog):
     assert np.all(scores.scores == [[0, 0], [0, 0]]), "Expected different scores."
     msg = "Matching_type was set to 'difference' but no difference could be computed between"
     assert msg in caplog.text
+
+
+@pytest.mark.parametrize("tolerance, expected", [
+    [1.0, [[0, 1], [0, 0]]],
+    [2.0, [[0, 1], [0, 1]]],
+    [10.0, [[1, 1], [1, 1]]],
+    [0.1, [[0, 0], [0, 0]]]
+])
+def test_metadata_match_numerical(spectrums, tolerance, expected):
+    """Test basic metadata matching between numerical entries."""
+    references = spectrums[:2]
+    queries = spectrums[2:]
+
+    similarity_score = MetadataMatch(field="retention_time",
+                                     matching_type="difference", tolerance=tolerance)
+    scores = calculate_scores(references, queries, similarity_score)
+    assert np.all(scores.scores == expected), "Expected different scores."
