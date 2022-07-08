a0022cc11a32b57183782d6c3d13af127c6df148
maximskorik <maximskoryk@gmail.com>
2022-07-08 20:48:59 +0200

    harmonize argument name across `importing` subpackage

--- matchms/importing/load_scores.py
+++ matchms/importing/load_scores.py
@@ -2,19 +2,19 @@ import pickle
 from matchms.Scores import ScoresBuilder
 
 
-def scores_from_json(file_path):
+def scores_from_json(filename):
     """
     Load :py:class:`~matchms.Score.Score` object from a json file.
 
     Parameters
     ----------
-    file_path : str
+    filename : str
         Path to json file containing scores.
     """
-    return ScoresBuilder().from_json(file_path).build()
+    return ScoresBuilder().from_json(filename).build()
 
 
-def scores_from_pickle(file_path):
+def scores_from_pickle(filename):
     """
     Load :py:class:`~matchms.Score.Score` object from a pickle file.
 
@@ -23,8 +23,8 @@ def scores_from_pickle(file_path):
 
     Parameters
     ----------
-    file_path : str
+    filename : str
         Path to pickle file containing scores.
     """
-    with open(file_path, "rb") as f:
+    with open(filename, "rb") as f:
         return pickle.load(f)
