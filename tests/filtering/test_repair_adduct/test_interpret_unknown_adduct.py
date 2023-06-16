803128a1efa3a1c6abf3681e2593626e933b533c
niekdejonge <niekdejonge@gmail.com>
2023-06-16 14:23:38 +0200

    Added test_get_multiplier_and_mass

---
+++ tests/filtering/test_repair_adduct/test_interpret_unknown_adduct.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matchms.filtering.repair_adduct.clean_adduct import load_adducts_dict
+from matchms.filtering.repair_adduct.interpret_unknown_adduct import get_multiplier_and_mass
+
+
+def test_get_multiplier_and_mass():
+    """Test if correct dict is imported."""
+    known_adducts = load_adducts_dict()
+    for adduct, adduct_info in known_adducts.items():
+        exp_multiplier = adduct_info["mass_multiplier"]
+        exp_corr_mass = adduct_info["correction_mass"]
+        multiplier, correction_mass = get_multiplier_and_mass(adduct)
+        assert exp_multiplier == multiplier
+        np.testing.assert_almost_equal(exp_corr_mass, correction_mass, decimal=5)
