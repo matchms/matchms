e69838813aa1d5de470ef16ecb2ebbb256957975
hechth <helge.hecht@recetox.muni.cz>
2023-06-19 14:41:47 +0200

    Add metadata harmonization options to `load_spectra` function Fixes #442

--- matchms/importing/load_spectra.py
+++ matchms/importing/load_spectra.py
@@ -6,7 +6,7 @@ from matchms.importing import (load_from_json, load_from_mgf, load_from_msp,
 from matchms.typing import SpectrumType
 
 
-def load_spectra(file: str, ftype: Optional[str] = None) -> Generator[SpectrumType, None, None]:
+def load_spectra(file: str, metadata_harmonization: bool = True, ftype: Optional[str] = None) -> Generator[SpectrumType, None, None]:
     """Loads spectra from your spectrum file into memory as matchms Spectrum object
 
     The following file extensions can be loaded in with this function:
@@ -29,26 +29,24 @@ def load_spectra(file: str, ftype: Optional[str] = None) -> Generator[SpectrumTy
         ftype = ftype.lower()
 
     if ftype == "mzml":
-        return load_from_mzml(file)
+        return load_from_mzml(file, metadata_harmonization=metadata_harmonization)
     if ftype == "json":
-        return load_from_json(file)
+        return load_from_json(file, metadata_harmonization=metadata_harmonization)
     if ftype == "mgf":
-        return load_from_mgf(file)
+        return load_from_mgf(file, metadata_harmonization=metadata_harmonization)
     if ftype == "msp":
-        return load_from_msp(file)
+        return load_from_msp(file, metadata_harmonization=metadata_harmonization)
     if ftype == "mzxml":
-        return load_from_mzxml(file)
+        return load_from_mzxml(file, metadata_harmonization=metadata_harmonization)
     if ftype == "usi":
-        return load_from_usi(file)
+        return load_from_usi(file, metadata_harmonization=metadata_harmonization)
     if ftype == "pickle":
-        spectra = load_from_pickle(file)
-        assert isinstance(spectra, list), "Expected list of spectra"
-        assert isinstance(spectra[0], SpectrumType), "Expected list of spectra"
-        return spectra
-    assert False, f"File extension of file: {file} is not recognized"
+        return load_from_pickle(file, metadata_harmonization)
+        
+    raise TypeError(f"File extension of file: {file} is not recognized")
 
 
-def load_from_pickle(filename: str) -> Any:
+def load_from_pickle(filename: str, metadata_harmonization: bool) -> SpectrumType:
     """Load spectra stored in pickle
 
     Args:
@@ -59,4 +57,10 @@ def load_from_pickle(filename: str) -> Any:
     """
     with open(filename, 'rb') as file:
         loaded_object = pickle.load(file)
+    if not isinstance(loaded_object, list) or not isinstance(loaded_object[0], SpectrumType):
+        raise TypeError("Expected list of spectra")
+        
+    if metadata_harmonization:
+        for x in loaded_object:
+            x._metadata.harmonize_values()
     return loaded_object
