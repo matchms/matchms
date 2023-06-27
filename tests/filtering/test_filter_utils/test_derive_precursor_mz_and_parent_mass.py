d482d153ee333b251230ea727659915b1b907fe7
niekdejonge <niekdejonge@gmail.com>
2023-06-27 10:56:59 +0200

    moved test_derive_precursor_mz_and_parent_mass.py to correct folder

--- tests/filtering/test_filter_utils/test_derive_precursor_mz_and_parent_mass.py
+++ tests/filtering/test_filter_utils/test_derive_precursor_mz_and_parent_mass.py
@@ -3,7 +3,7 @@ import pytest
 from matchms.constants import PROTON_MASS
 from matchms.filtering.filter_utils.derive_precursor_mz_and_parent_mass import (
     derive_parent_mass_from_precursor_mz, derive_precursor_mz_from_parent_mass)
-from ..builder_Spectrum import SpectrumBuilder
+from tests.builder_Spectrum import SpectrumBuilder
 
 
 @pytest.mark.parametrize("adduct, expected", [("[M+2Na-H]+", 399.02884),
