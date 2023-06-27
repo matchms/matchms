89ec425cc7d8332d03e3ff261798952b32a51fe5
niekdejonge <niekdejonge@gmail.com>
2023-06-27 11:51:30 +0200

    add extra tests

--- tests/filtering/test_repair_smiles_from_compound_name.py
+++ tests/filtering/test_repair_smiles_from_compound_name.py
@@ -17,16 +17,27 @@ def csv_file_annotated_compound_names(tmp_path):
     return csv_file_name
 
 
-@pytest.mark.parametrize("compound_name, parent_mass, expected_smiles", [
-    ("compound_1", 100.01, 'smile_1'),
-    ("compound_2", 100.01, 'test_smile'),
-    ("compound_1", 100.5, 'test_smile'),
+@pytest.mark.parametrize("compound_name, smile, inchi, inchikey, parent_mass, expected_smiles", [
+    ("compound_1", "input_smile_1", "inchi_1", "inchikey_1", 100.01, 'smile_1'),
+    ("compound_2", "input_smile_2", "inchi_2", "inchikey_2", 100.01, 'input_smile_2'),
+    ("compound_1", "input_smile_1", "inchi_1", "inchikey_1", 200.01, 'input_smile_1'),
+    ("compound_3", "input_smile_1", "inchi_1", "inchikey_1", 200.01, 'input_smile_1'),
+    # test that correct annotations are skipped
+    ("compound_1", "C1CSSC1CCCCC(=O)O",
+     "InChI=1S/C8H14O2S2/c9-8(10)4-2-1-3-7-5-6-11-12-7/h7H,1-6H2,(H,9,10)", "AGBQKNBQESQNJD-UHFFFAOYSA-N", 100.01,
+     'C1CSSC1CCCCC(=O)O'),
+    ("compound_1", "C1CSSC1CCCCC(=O)O",
+     "InChI=1S/C8H14O2S2/c9-8(10)4-2-1-3-7-5-6-11-12-7/h7H,1-6H2,(H,9,10)", "AGBQKNBQESQNJD-UHFFFAOYSA_bla_bla", 100.01,
+     "smile_1"),
 ])
-def test_clean_compound_name(compound_name, parent_mass, expected_smiles, csv_file_annotated_compound_names):
+def test_clean_compound_name(compound_name, parent_mass, smile, inchi, inchikey,
+                             expected_smiles,
+                             csv_file_annotated_compound_names):
     builder = SpectrumBuilder()
     spectrum_in = builder.with_metadata({"compound_name": compound_name,
                                          "parent_mass": parent_mass,
-                                         "smiles": "test_smile"}).build()
+                                         "smiles": smile,
+                                         "inchi": inchi,
+                                         "inchikey": inchikey}).build()
     spectrum = repair_smiles_from_compound_name(spectrum_in, csv_file_annotated_compound_names, mass_tolerance=0.1)
-
     assert spectrum.get("smiles") == expected_smiles, "Expected different smiles."
