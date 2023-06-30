1d003dcde8794b17ef8267641ad9a52c853ac943
niekdejonge <niekdejonge@gmail.com>
2023-06-30 15:07:16 +0200

    Fix test name

--- tests/filtering/test_require_correct_ionmode.py
+++ tests/filtering/test_require_correct_ionmode.py
@@ -12,7 +12,7 @@ from ..builder_Spectrum import SpectrumBuilder
     ("negative", "positive", True),
     ("n/a", "both", True),
 ])
-def test_clean_compound_name(ionmode, ionmode_to_keep, spectrum_removed):
+def test_require_correct_ionmode(ionmode, ionmode_to_keep, spectrum_removed):
     builder = SpectrumBuilder()
     spectrum_in = builder.with_metadata({"ionmode": ionmode}).build()
     spectrum = require_correct_ionmode(spectrum_in, ionmode_to_keep)
