6a58aa4b571fb1811ca91990bf8cd008fe411c96
niekdejonge <niekdejonge@gmail.com>
2023-06-30 09:55:02 +0200

    Updated test_repair_adduct_based_on_smiles to also check parent mass repair

--- tests/filtering/test_repair_adduct/test_repair_adduct_based_on_smiles.py
+++ tests/filtering/test_repair_adduct/test_repair_adduct_based_on_smiles.py
@@ -11,15 +11,28 @@ from tests.builder_Spectrum import SpectrumBuilder
                           (15.0, "[M-H]-", "negative"),
                           (51.0, "[M+Cl]-", "negative"),
                           (4.33333, "[M-3H]3-", "negative"),
-                          (1000.0, None, "negative"),
                           ])
 def test_repair_adduct_based_on_smiles_not_mol_wt(precursor_mz, expected_adduct, ionmode):
     pytest.importorskip("rdkit")
 
     # CH4 is used as smiles, this has a mass of 16
-    spectrum_in = SpectrumBuilder().with_metadata({"smiles": "C", "precursor_mz": precursor_mz, "ionmode": ionmode}).build()
+    spectrum_in = SpectrumBuilder().with_metadata({"smiles": "C",
+                                                   "precursor_mz": precursor_mz,
+                                                   "ionmode": ionmode}).build()
     spectrum_out = repair_adduct_based_on_smiles(spectrum_in, mass_tolerance=0.1, accept_parent_mass_is_mol_wt=False)
     assert spectrum_out.get("adduct") == expected_adduct
+    assert abs(spectrum_out.get("parent_mass") - 15.9589) < 0.1
+
+
+def test_repair_adduct_based_on_smiles_not_repaired():
+    pytest.importorskip("rdkit")
+
+    # CH4 is used as smiles, this has a mass of 16
+    spectrum_in = SpectrumBuilder().with_metadata({"smiles": "C",
+                                                   "precursor_mz": 1000.0,
+                                                   "ionmode": "negative"}).build()
+    spectrum_out = repair_adduct_based_on_smiles(spectrum_in, mass_tolerance=0.1, accept_parent_mass_is_mol_wt=False)
+    assert spectrum_out.get("adduct") == None
 
 
 @pytest.mark.parametrize("precursor_mz, expected_adduct, ionmode",
