1af76f0fed81e8dac343b5bd2c88af09faaf77cc
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2023-06-30 18:31:47 +0200

    revert changes from SpectrumType to Spectrum (might change later again)

--- matchms/importing/load_spectra.py
+++ matchms/importing/load_spectra.py
@@ -3,7 +3,6 @@ import pickle
 from typing import Generator, Optional
 from matchms.importing import (load_from_json, load_from_mgf, load_from_msp,
                                load_from_mzml, load_from_mzxml, load_from_usi)
-from matchms.Spectrum import Spectrum
 from matchms.typing import SpectrumType
 
 
