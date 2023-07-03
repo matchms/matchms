f795ac2132e5444aefd1d8dace4041b71843393f
florian-huber <florian.huber@hs-duesseldorf.de>
2023-07-03 14:57:32 +0200

    minor refactoring

--- matchms/filtering/SpectrumProcessor.py
+++ matchms/filtering/SpectrumProcessor.py
@@ -1,4 +1,4 @@
-from matchms.filtering import *
+import matchms.filtering as msfilters
 
 
 class SpectrumProcessor:
@@ -69,40 +69,40 @@ class SpectrumProcessor:
 
 
 # List all filters in a functionally working order
-ALL_FILTERS = [make_charge_int,
-               add_compound_name,
-               derive_adduct_from_name,
-               derive_formula_from_name,
-               clean_compound_name,
-               interpret_pepmass,
-               add_precursor_mz,
-               add_retention_index,
-               add_retention_time,
-               derive_ionmode,
-               correct_charge,
-               derive_adduct_from_name,  # run again
-               derive_formula_from_name,  # run again
-               require_precursor_mz,
-               add_parent_mass,
-               harmonize_undefined_inchikey,
-               harmonize_undefined_inchi,
-               harmonize_undefined_smiles,
-               repair_inchi_inchikey_smiles,
-               repair_parent_mass_match_smiles_wrapper,
-               require_correct_ionmode,
-               require_precursor_below_mz,
-               require_parent_mass_match_smiles,
-               require_valid_annotation,
-               normalize_intensities,
-               select_by_intensity,
-               select_by_mz,
-               select_by_relative_intensity,
-               remove_peaks_around_precursor_mz,
-               remove_peaks_outside_top_k,
-               require_minimum_number_of_peaks,
-               require_minimum_of_high_peaks,
-               add_fingerprint,
-               add_losses,
+ALL_FILTERS = [msfilters.make_charge_int,
+               msfilters.add_compound_name,
+               msfilters.derive_adduct_from_name,
+               msfilters.derive_formula_from_name,
+               msfilters.clean_compound_name,
+               msfilters.interpret_pepmass,
+               msfilters.add_precursor_mz,
+               msfilters.add_retention_index,
+               msfilters.add_retention_time,
+               msfilters.derive_ionmode,
+               msfilters.correct_charge,
+               # msfilters.derive_adduct_from_name,  # run again? Or improve those filters?
+               # msfilters.derive_formula_from_name,  # run again? Or improve those filters?
+               msfilters.require_precursor_mz,
+               msfilters.add_parent_mass,
+               msfilters.harmonize_undefined_inchikey,
+               msfilters.harmonize_undefined_inchi,
+               msfilters.harmonize_undefined_smiles,
+               msfilters.repair_inchi_inchikey_smiles,
+               msfilters.repair_parent_mass_match_smiles_wrapper,
+               msfilters.require_correct_ionmode,
+               msfilters.require_precursor_below_mz,
+               msfilters.require_parent_mass_match_smiles,
+               msfilters.require_valid_annotation,
+               msfilters.normalize_intensities,
+               msfilters.select_by_intensity,
+               msfilters.select_by_mz,
+               msfilters.select_by_relative_intensity,
+               msfilters.remove_peaks_around_precursor_mz,
+               msfilters.remove_peaks_outside_top_k,
+               msfilters.require_minimum_number_of_peaks,
+               msfilters.require_minimum_of_high_peaks,
+               msfilters.add_fingerprint,
+               msfilters.add_losses,
               ]
 
 FILTER_FUNCTIONS = {x.__name__: x for x in ALL_FILTERS}
