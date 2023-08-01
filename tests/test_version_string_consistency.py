4c491b7be78d052174e539d40a6c2a652b0db863
hechth <helge.hecht@recetox.muni.cz>
2023-08-01 10:47:28 +1000

    Added test paths and fixed version test case

--- tests/test_version_string_consistency.py
+++ tests/test_version_string_consistency.py
@@ -3,17 +3,17 @@ import re
 from matchms import __version__ as expected_version
 
 
-def test_version_string_consistency_setup_cfg():
-    """Check whether version in setup.cfg is consistent with that in matchms.__version__"""
+def test_version_string_consistency_pyproject_toml():
+    """Check whether version in pyproject.toml is consistent with that in matchms.__version__"""
 
     repository_root = os.path.join(os.path.dirname(__file__), '..')
-    fixture = os.path.join(repository_root, "setup.cfg")
+    fixture = os.path.join(repository_root, "pyproject.toml")
 
     with open(fixture, "r", encoding="utf-8") as f:
         contents = f.read()
 
-    match = re.search(r"^current_version = (?P<semver>.*)$", contents, re.MULTILINE)
-    actual_version = match["semver"]
+    match = re.search(r"^version = (?P<semver>.*)$", contents, re.MULTILINE)
+    actual_version = match["semver"].strip('"')
 
-    assert expected_version == actual_version, "Expected version string used in setup.cfg to be consistent with" \
+    assert expected_version == actual_version, "Expected version string used in pyproject.toml to be consistent with" \
                                                " that in matchms.__version__"
