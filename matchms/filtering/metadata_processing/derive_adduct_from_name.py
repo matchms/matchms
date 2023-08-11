031f80e134c4bfef737628fdc3f9e948ed53a8ec
niekdejonge <niekdejonge@gmail.com>
2023-08-11 15:57:27 +0200

    Add "_" to looks_like_adduct

--- matchms/filtering/metadata_processing/derive_adduct_from_name.py
+++ matchms/filtering/metadata_processing/derive_adduct_from_name.py
@@ -36,7 +36,7 @@ def derive_adduct_from_name(spectrum_in: SpectrumType,
     adduct_from_name = None
     name_split = name.split(" ")
     for name_part in name_split[::-1][:2]:
-        if looks_like_adduct(name_part):
+        if _looks_like_adduct(name_part):
             adduct_from_name = name_part
             break
 
@@ -47,7 +47,7 @@ def derive_adduct_from_name(spectrum_in: SpectrumType,
         logger.info("Removed adduct %s from compound name.", adduct_from_name)
 
     # Add found adduct to metadata (if not present yet)
-    if adduct_from_name and not looks_like_adduct(spectrum.get("adduct")):
+    if adduct_from_name and not _looks_like_adduct(spectrum.get("adduct")):
         adduct_cleaned = _clean_adduct(adduct_from_name)
         spectrum.set("adduct", adduct_cleaned)
         logger.info("Added adduct %s from the compound name to metadata.", spectrum.get('adduct'))
@@ -55,7 +55,7 @@ def derive_adduct_from_name(spectrum_in: SpectrumType,
     return spectrum
 
 
-def looks_like_adduct(adduct):
+def _looks_like_adduct(adduct):
     """Return True if input string has expected format of an adduct."""
     if not isinstance(adduct, str):
         return False
