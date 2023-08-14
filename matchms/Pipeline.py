86206ec3114cbe36a2c84c4ea032cce8b923e7f4
niekdejonge <niekdejonge@gmail.com>
2023-08-14 16:10:46 +0200

    Rename import spectra

--- matchms/Pipeline.py
+++ matchms/Pipeline.py
@@ -208,8 +208,7 @@ class Pipeline:
         self.write_to_logfile("--- Start running matchms pipeline. ---")
         self.write_to_logfile(f"Start time: {str(datetime.now())}")
         self.check_score_computation()
-        self.import_data(query_files,
-                         reference_files)
+        self.import_spectra(query_files, reference_files)
 
         # Processing
         self.write_to_logfile("--- Processing spectra ---")
@@ -326,7 +325,7 @@ class Pipeline:
             with open(self.logging_file, "a", encoding="utf-8") as f:
                 f.write(line + '\n')
 
-    def import_data(self, query_files, reference_files=None):
+    def import_spectra(self, query_files, reference_files=None):
         """Import spectra from file(s).
 
         Parameters
