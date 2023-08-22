c275da75b7a50cd904083c84f15f678580b4e6a7
niekdejonge <niekdejonge@gmail.com>
2023-08-22 17:46:20 +0200

    Implement repair_valid_annotation

--- matchms/filtering/metadata_processing/require_valid_annotation.py
+++ matchms/filtering/metadata_processing/require_valid_annotation.py
@@ -3,10 +3,58 @@ from matchms import Spectrum
 from matchms.filtering.filter_utils.smile_inchi_inchikey_conversions import (
     convert_inchi_to_inchikey,
     convert_smiles_to_inchi, is_valid_inchi, is_valid_inchikey,
-    is_valid_smiles)
-
+    is_valid_smiles, _check_fully_annotated, convert_inchi_to_smiles)
+from matchms.filtering.metadata_processing.require_parent_mass_match_smiles import _check_smiles_and_parent_mass_match
 
 logger = logging.getLogger("matchms")
+
+
+def repair_valid_annotation(spectrum_in: Spectrum):
+    """When smiles and inchi do not match the annotation that matches parent mass will be kept"""
+    if spectrum_in is None:
+        return None
+    spectrum = spectrum_in.clone()
+    # Check if smiles, inchi and inchikey are valid
+    if _check_fully_annotated(spectrum):
+        logger.warning("First run derive inchi_from_smiles, derive_inchikey_from_inchi and derive_smiles_from_inchi")
+    smiles = spectrum.get("smiles")
+    inchi = spectrum.get("inchi")
+    inchikey = spectrum.get("inchikey")
+
+    # Check if smiles and inchi match
+    if convert_smiles_to_inchi(smiles) == inchi:
+        if inchikey[:14] == convert_inchi_to_inchikey(inchi)[:14]:
+            # The inchi, smiles and inchikey already match
+            return spectrum
+        else:
+            # The smiles and inchi match, but the inchikey is wrong
+            # Repair inchikey
+            new_inchikey = convert_inchi_to_inchikey(inchi)
+            logger.info(f"The inchikey has been changed from {inchikey} to {new_inchikey}")
+            spectrum.set("inchikey", new_inchikey)
+            return spectrum
+    else:
+        # There is a mismatch between smiles and inchi
+        parent_mass = spectrum.get("parent_mass")
+        smiles_correct = _check_smiles_and_parent_mass_match(smiles, parent_mass, 0.1)
+        inchi_correct = _check_smiles_and_parent_mass_match(convert_inchi_to_smiles(inchi), parent_mass, 0.1)
+        if smiles_correct and inchi_correct:
+            logger.warning(f"The smiles and inchi are not matching, but both match the parent mass. "
+                           f"Smiles = {smiles}, inchi = {inchi}, inchikey = {inchikey}")
+            return spectrum
+        if smiles_correct and not inchi_correct:
+            # repair the inchi from the smiles
+            new_inchi = convert_smiles_to_inchi(smiles)
+            spectrum.set("inchi", new_inchi)
+            logger.info(f"The inchi has been changed from {inchi} to {new_inchi}"
+                        f"The new inchi matches the parent mass, while the old one did not")
+        else:
+            # repair the smiles from the inchi
+            new_smiles = convert_inchi_to_smiles(inchi)
+            spectrum.set("smiles", new_smiles)
+            logger.info(f"The smiles has been changed from {smiles} to {new_smiles}, to match the inchi. "
+                        f"The new smiles matches the parent mass, while the old one did not")
+
 
 
 def require_valid_annotation(spectrum: Spectrum):
