55cb3ac0d3db462b441a83c3d5558ca033350bec
niekdejonge <niekdejonge@gmail.com>
2023-08-22 16:46:08 +0200

    Change _check_smiles_inchi_inchikey_match

--- matchms/filtering/metadata_processing/require_valid_annotation.py
+++ matchms/filtering/metadata_processing/require_valid_annotation.py
@@ -24,28 +24,19 @@ def require_valid_annotation(spectrum: Spectrum):
     if not is_valid_inchi(inchi):
         logger.info("Removed spectrum since inchi is not valid. Incorrect inchi = %s", inchi)
         return None
-    if not _check_smiles_inchi_inchikey_match(spectrum):
+    if not _check_smiles_inchi_inchikey_match(smiles, inchi, inchikey):
+        logger.info(f"Removed spectrum since smiles, inchi and inchikey do not match. "
+                    f"Smiles = {smiles}, inchi = {inchi}, inchikey = {inchikey}")
         return None
     return spectrum
 
 
-def _check_smiles_inchi_inchikey_match(spectrum) -> bool:
-    """Checks if a spectrum"""
-    if spectrum is None:
-        return False
-    smiles = spectrum.get("smiles")
-    inchi = spectrum.get("inchi")
-    inchikey = spectrum.get("inchikey")
+def _check_smiles_inchi_inchikey_match(smiles, inchi, inchikey) -> bool:
+    """Checks if smiles inchi and inchikey match"""
     # check if inchi matches the inchikey
     if not inchikey[:14] == convert_inchi_to_inchikey(inchi)[:14]:
-        logger.warning("Removed spectrum since inchi and inchikey do not match. "
-                       "inchi = %s, inchikey = %s, expected_inchikey = %s",
-                       inchi, inchikey, convert_inchi_to_inchikey(inchi))
         return False
     # check if smiles matches the inchikey (first convert to inchi followed by converting to inchikey)
     if not inchikey[:14] == convert_inchi_to_inchikey(convert_smiles_to_inchi(smiles))[:14]:
-        logger.warning("Removed spectrum since smiles does not match the inchikey. "
-                       "inchikey = %s, smiles = %s, expected_inchikey = %s",
-                       inchikey, smiles, convert_inchi_to_inchikey(convert_smiles_to_inchi(smiles)))
         return False
     return True
