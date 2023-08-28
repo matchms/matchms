833c684583dc46489d3dfcd212f836ad35dde7dc
niekdejonge <niekdejonge@gmail.com>
2023-08-28 12:05:23 +0200

    Remove warning that is not applicable

--- matchms/filtering/metadata_processing/repair_smiles_of_salts.py
+++ matchms/filtering/metadata_processing/repair_smiles_of_salts.py
@@ -20,7 +20,6 @@ def repair_smiles_of_salts(spectrum_in,
 
     smiles = spectrum.get("smiles")
     if not is_valid_smiles(smiles):
-        logger.warning("No valid smiles, please run require_valid_annotation before")
         return spectrum
     parent_mass = spectrum.get("parent_mass")
     possible_ion_combinations = _create_possible_ions(smiles)
