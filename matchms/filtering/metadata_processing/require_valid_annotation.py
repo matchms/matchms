9485b4dd1d684ef36901687a748068cbb16260c9
niekdejonge <niekdejonge@gmail.com>
2023-09-04 13:42:20 +0200

    Add removed imports in merge

--- matchms/filtering/metadata_processing/require_valid_annotation.py
+++ matchms/filtering/metadata_processing/require_valid_annotation.py
@@ -1,7 +1,7 @@
 import logging
 from matchms import Spectrum
 from matchms.filtering.filter_utils.smile_inchi_inchikey_conversions import (
-    convert_inchi_to_inchikey, convert_smiles_to_inchi)
+    convert_inchi_to_inchikey, convert_smiles_to_inchi, is_valid_smiles, is_valid_inchi, is_valid_inchikey)
 
 
 logger = logging.getLogger("matchms")
