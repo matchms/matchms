9de9be515c523f289957286abd774a5f6b93513e
niekdejonge <niekdejonge@gmail.com>
2023-09-06 12:08:29 +0200

    Add additional tests

--- tests/filtering/test_derive_adduct_from_name.py
+++ tests/filtering/test_derive_adduct_from_name.py
@@ -14,9 +14,11 @@ from ..builder_Spectrum import SpectrumBuilder
     [{"compound_name": "peptideXYZ [M+H+K]", "adduct": "M+H"}, True, "M+H", "peptideXYZ", "[M+H+K]"],
     [{"compound_name": "peptideXYZ [M+H+K]"}, False, "[M+H+K]", "peptideXYZ [M+H+K]", None],
     [{"Name": "peptideXYZ [M+H+K]", "adduct": "M+H"}, True, "M+H", "peptideXYZ", "[M+H+K]"],
+    [{"compound_name": "peptideXYZ [M+H+K] C16H12"}, True, "[M+H+K]", "peptideXYZ C16H12", "[M+H+K]"],
     [{"name": ""}, True, None, "", None]
 ])
-def test_derive_adduct_from_name_parametrized(metadata, remove_adduct_from_name, expected_adduct, expected_name, removed_adduct):
+def test_derive_adduct_from_name_parametrized(metadata, remove_adduct_from_name,
+                                              expected_adduct, expected_name, removed_adduct):
     set_matchms_logger_level("INFO")
     spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
 
@@ -28,12 +30,32 @@ def test_derive_adduct_from_name_parametrized(metadata, remove_adduct_from_name,
 
     expected_log = []
     if spectrum.get("compound_name") != spectrum_in.get("compound_name"):
-        expected_log.append(('matchms', 'INFO', f'Removed adduct {removed_adduct} from compound name.'))
+        expected_log.append(('matchms', 'INFO', f"Removed adduct ['{removed_adduct}'] from compound name."))
     if spectrum.get("adduct") != spectrum_in.get("adduct"):
         expected_log.append(('matchms', 'INFO', f'Added adduct {expected_adduct} from the compound name to metadata.'))
 
     log.check(*expected_log)
     reset_matchms_logger()
+
+
+@pytest.mark.parametrize("metadata, expected_adduct, expected_name", [
+    # Test removing two adducts and selecting the interpretable one
+    [{"compound_name": "peptideXYZ [M+H+K] C16H12 [M+H+K]2+"}, "[M+H+K]2+", "peptideXYZ C16H12"],
+    # Test removing two adducts and not adding if not interpretable
+    [{"compound_name": "peptideXYZ [M+H+K] C16H12 [M+2H+K]"}, None, "peptideXYZ C16H12"],
+    # Test removing two adducts and check that clean adduct is run (to recover known adducts)
+    [{"compound_name": "peptideXYZ [M+H+K] C16H12 [M+H]"}, "[M+H]+", "peptideXYZ C16H12"]
+
+])
+def test_derive_adduct_from_name_multiple_adducts_in_name(metadata,
+                                                          expected_adduct, expected_name):
+    set_matchms_logger_level("INFO")
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
+
+    spectrum = derive_adduct_from_name(spectrum_in, remove_adduct_from_name=True)
+
+    assert spectrum.get("adduct") == expected_adduct, "Expected different adduct."
+    assert spectrum.get("compound_name") == expected_name, "Expected different cleaned name."
 
 
 def test_empty_spectrum():
