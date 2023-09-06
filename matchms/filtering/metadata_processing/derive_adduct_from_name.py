96ca439cbaaacfaff866e7a307f704307d2fb0fa
niekdejonge <niekdejonge@gmail.com>
2023-09-06 12:00:51 +0200

    Remove multiple adducts when found at any location. Select the adduct that is actually fully interpretable (both charge and existing elements)

--- matchms/filtering/metadata_processing/derive_adduct_from_name.py
+++ matchms/filtering/metadata_processing/derive_adduct_from_name.py
@@ -1,8 +1,10 @@
 import logging
 import re
+from typing import List, Optional
 from matchms.typing import SpectrumType
 from ..filter_utils.load_known_adducts import load_known_adducts
 from .clean_adduct import _clean_adduct
+from matchms.filtering.filter_utils.interpret_unknown_adduct import get_multiplier_and_mass_from_adduct
 
 
 logger = logging.getLogger("matchms")
@@ -26,34 +28,56 @@ def derive_adduct_from_name(spectrum_in: SpectrumType,
 
     spectrum = spectrum_in.clone()
 
-    if spectrum.get("compound_name", None) is not None:
-        name = spectrum.get("compound_name")
-    else:
-        assert spectrum.get("name", None) in [None, ""], ("Found 'name' but not 'compound_name' in metadata",
-                                                          "Apply 'add_compound_name' filter first.")
+    compound_name = spectrum.get("compound_name", None)
+    if compound_name is None:
+        if spectrum.get("name", None) not in [None, ""]:
+            logger.warning("Found 'name' but not 'compound_name' in metadata",
+                           "Apply 'add_compound_name' filter first.")
         return spectrum
-
     # Detect adduct in compound name
-    adduct_from_name = None
-    name_split = name.split(" ")
-    for name_part in name_split[::-1][:2]:
+    parts_that_look_like_adduct = []
+    name_split = compound_name.split(" ")
+    for name_part in name_split:
         if _looks_like_adduct(name_part):
-            adduct_from_name = name_part
-            break
+            # Some adducts occur more than once. So they are all removed.
+            parts_that_look_like_adduct.append(name_part)
 
-    if adduct_from_name and remove_adduct_from_name:
-        name_adduct_removed = " ".join([x for x in name_split if x != adduct_from_name])
+    if remove_adduct_from_name and len(parts_that_look_like_adduct) > 0:
+        name_adduct_removed = " ".join([x for x in name_split if x not in parts_that_look_like_adduct])
         name_adduct_removed = name_adduct_removed.strip("; ")
         spectrum.set("compound_name", name_adduct_removed)
-        logger.info("Removed adduct %s from compound name.", adduct_from_name)
+        logger.info("Removed adduct %s from compound name.", parts_that_look_like_adduct)
 
-    # Add found adduct to metadata (if not present yet)
-    if adduct_from_name and not _looks_like_adduct(spectrum.get("adduct")):
-        adduct_cleaned = _clean_adduct(adduct_from_name)
-        spectrum.set("adduct", adduct_cleaned)
-        logger.info("Added adduct %s from the compound name to metadata.", spectrum.get('adduct'))
+    if len(parts_that_look_like_adduct) > 0 and not _looks_like_adduct(spectrum.get("adduct")):
+        best_adduct = _select_best_adduct(parts_that_look_like_adduct)
+        if best_adduct:
+            # Add found adduct to metadata (if not present yet)
+            spectrum.set("adduct", best_adduct)
+            logger.info("Added adduct %s from the compound name to metadata.", spectrum.get('adduct'))
 
     return spectrum
+
+
+def _select_best_adduct(list_of_adducts: List[str]) -> Optional[str]:
+    """Selects an adduct that can actually be interpreted (complete with charge and known elements)"""
+    unique_cleaned_adducts = list({_clean_adduct(adduct) for adduct in list_of_adducts})
+    if len(unique_cleaned_adducts) == 1:
+        return unique_cleaned_adducts[0]
+
+    completely_correct_adduct = []
+    for adduct in unique_cleaned_adducts:
+        multiplier, correction_mass = get_multiplier_and_mass_from_adduct(adduct)
+        # check if both multiplier and correction mass are not None
+        if multiplier and correction_mass:
+            completely_correct_adduct.append(adduct)
+    if len(completely_correct_adduct) == 0:
+        return None
+    if len(completely_correct_adduct) == 1:
+        return completely_correct_adduct[0]
+    else:
+        logger.warning("Two potential adducts were found in the compound name that are both valid adducts. "
+                       "The first adduct is used. The adducts found are: %s", completely_correct_adduct)
+        return completely_correct_adduct[0]
 
 
 def _looks_like_adduct(adduct):
