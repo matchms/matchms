a9e4c4495c770109c5f4b35cc57a373129ff6906
niekdejonge <niekdejonge@gmail.com>
2023-11-13 16:11:21 +0100

    Remove test that is outdated

--- tests/importing/test_load_from_mgf.py
+++ tests/importing/test_load_from_mgf.py
@@ -11,14 +11,3 @@ def test_load_from_mgf_using_filepath():
 
     assert len(spectra) > 0
     assert isinstance(spectra[0], Spectrum)
-
-
-def test_load_from_mgf_using_file():
-    module_root = os.path.join(os.path.dirname(__file__), "..")
-    spectra_filepath = os.path.join(module_root, "testdata", "pesticides.mgf")
-
-    with open(spectra_filepath, "r", encoding="utf-8") as spectra_file:
-        spectra = list(load_from_mgf(spectra_file))
-
-        assert len(spectra) > 0
-        assert isinstance(spectra[0], Spectrum)
