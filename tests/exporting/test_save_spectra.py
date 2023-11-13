1b198a8d565224250d9a8eb5c4d822b0746b4c20
niekdejonge <niekdejonge@gmail.com>
2023-11-13 14:56:35 +0100

    Add tests for save spectra

--- tests/exporting/test_save_spectra.py
+++ tests/exporting/test_save_spectra.py
@@ -1,20 +1,15 @@
 import os
 import tempfile
-from typing import List
-import numpy as np
 import pytest
-from matchms import Spectrum
 from matchms.exporting import save_spectra
-from matchms.importing import load_spectra
-from ..builder_Spectrum import SpectrumBuilder
+from matchms.importing import load_spectra, load_from_mgf
 
 
-@pytest.fixture
-def spectrum_list():
-    mz = np.array([100, 200, 290, 490, 510], dtype="float")
-    intensities = np.array([0.1, 0.2, 1.0, 0.3, 0.4], dtype="float")
-    spectrum = SpectrumBuilder().with_mz(mz).with_intensities(intensities).build()
-    return [spectrum]
+def load_test_spectra_file():
+    module_root = os.path.join(os.path.dirname(__file__), "..")
+    spectrums_file = os.path.join(module_root, "testdata", "testdata.mgf")
+    spectra = list(load_from_mgf(spectrums_file))
+    return spectra
 
 
 @pytest.mark.parametrize("file_name",
@@ -22,7 +17,7 @@ def spectrum_list():
                           "spectra.mgf",
                           "spectra.json",
                           "spectra.pickle"])
-def test_spectra(file_name, spectrum_list: List[Spectrum]):
+def test_spectra(file_name):
     """ Utility function to save spectra to msp and load them again.
 
     Params:
@@ -33,20 +28,17 @@ def test_spectra(file_name, spectrum_list: List[Spectrum]):
     --------
     reloaded_spectra: Spectra loaded from saved msp file.
     """
+    spectrum_list = load_test_spectra_file()
     with tempfile.TemporaryDirectory() as temp_dir:
         filename = os.path.join(temp_dir, file_name)
-    save_spectra(spectrum_list, filename)
-    reloaded_spectra = list(load_spectra(filename))
-    assert save_spectra == reloaded_spectra
-
-
-# def test_wrong_filename_exception():
-#     """ Test for exception being thrown if output file doesn't end with .msp. """
-#     with tempfile.TemporaryDirectory() as temp_dir:
-#         filename = os.path.join(temp_dir, "test.mzml")
-#
-#         with pytest.raises(AssertionError) as exception:
-#             save_as_msp(None, filename)
-#
-#         message = exception.value.args[0]
-#         assert message == "File extension '.mzml' not allowed."
+        save_spectra(spectrum_list, filename)
+        if not os.path.exists(filename):
+            FileNotFoundError("No file was created")
+        reloaded_spectra = list(load_spectra(filename))
+    assert len(reloaded_spectra) == len(spectrum_list)
+    for i, spectrum in enumerate(spectrum_list):
+        reloaded_spectrum = reloaded_spectra[i]
+        # Num_peaks is sometimes added during saving. So to be able to compare it is set to None
+        spectrum.set("num_peaks", None)
+        reloaded_spectrum.set("num_peaks", None)
+        assert spectrum == reloaded_spectrum
