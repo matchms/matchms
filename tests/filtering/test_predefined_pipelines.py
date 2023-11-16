253463f12f0c766634d6b6be55bedd288849836e
niekdejonge <niekdejonge@gmail.com>
2023-11-16 11:45:28 +0100

    Remove outdated test

--- tests/filtering/test_predefined_pipelines.py
+++ tests/filtering/test_predefined_pipelines.py
@@ -6,9 +6,3 @@ def test_run_predefined_filter_sets():
     """Tests if all predefined filter sets can be run"""
     for filter_set in ALL_FILTER_SETS:
         SpectrumProcessor(filter_set)
-
-
-def test_create_predefined_pipelines():
-    """Tests if all predefined pipelines can be run"""
-    for pipeline_name in PREDEFINED_PIPELINES.values():
-        SpectrumProcessor(pipeline_name)
