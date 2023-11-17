ecc224022a4190ba1b81ac3730cf1514abb3d53a
niekdejonge <niekdejonge@gmail.com>
2023-11-17 11:06:51 +0100

    Add export style to save spectra.

--- matchms/exporting/save_spectra.py
+++ matchms/exporting/save_spectra.py
@@ -1,13 +1,16 @@
 import os
+import logging
 import pickle
 from typing import List
 from matchms.exporting import save_as_json, save_as_mgf, save_as_msp
 from matchms.Spectrum import Spectrum
-from matchms.typing import SpectrumType
+
+logger = logging.getLogger("matchms")
 
 
-def save_spectra(spectrums: List[SpectrumType],
-                 file: str) -> None:
+def save_spectra(spectrums: List[Spectrum],
+                 file: str,
+                 export_style: str = "matchms") -> None:
     """Saves spectra as the file type specified.
 
     The following file extensions can be used:
@@ -21,6 +24,9 @@ def save_spectra(spectrums: List[SpectrumType],
         Path to file containing spectra, with file extension "json", "mgf", "msp"
     ftype:
         Optional. Filetype
+    export_style:
+        Converts the keys to the required export style. One of ["matchms", "massbank", "nist", "riken", "gnps"].
+        Default is "matchms"
     """
     if os.path.exists(file):
         raise FileExistsError(f"The specified file: {file} already exists.")
@@ -28,12 +34,14 @@ def save_spectra(spectrums: List[SpectrumType],
     ftype = os.path.splitext(file)[1].lower()[1:]
 
     if ftype == "json":
-        save_as_json(spectrums, file)
+        save_as_json(spectrums, file, export_style)
     elif ftype == "mgf":
-        save_as_mgf(spectrums, file)
+        save_as_mgf(spectrums, file, export_style)
     elif ftype == "msp":
-        save_as_msp(spectrums, file)
+        save_as_msp(spectrums, file, style=export_style)
     elif ftype == "pickle":
+        if export_style != "matchms":
+            logger.error(f"The only available export style for pickle is 'matchms', your export style: {export_style}")
         save_as_pickled_file(spectrums, file)
     else:
         raise TypeError(f"File extension of file: {file} is not recognized")
