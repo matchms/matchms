f1f73f60b3ad43e7e1ed634cb1102730d881b287
niekdejonge <niekdejonge@gmail.com>
2023-11-21 07:11:38 +0100

    Fix test issue with newly defined pipelines

--- tests/filtering/test_spectrum_processor.py
+++ tests/filtering/test_spectrum_processor.py
@@ -291,7 +291,7 @@ def test_add_filter_twice():
 
 
 def test_save_spectra_spectrum_processor(spectrums, tmp_path):
-    processor = SpectrumProcessor("default")
+    processor = SpectrumProcessor("basic")
     filename = os.path.join(tmp_path, "spectra.msp")
 
     _, _ = processor.process_spectrums(spectrums, cleaned_spectra_file=str(filename))
