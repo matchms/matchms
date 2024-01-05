6e331cec88eeae1dd4bcae531738edfdb9c983db
wverastegui <wudmir@gmail.com>
2024-01-05 08:40:03 +0100

    Added RT and RI filters

---
+++ matchms/filtering/metadata_processing/require_retention_time.py
@@ -0,0 +1,28 @@
+import logging
+from typing import Union
+from matchms.typing import SpectrumType
+
+logger = logging.getLogger("matchms")
+
+def require_retention_time(spectrum_in: SpectrumType) -> Union[SpectrumType, None]:
+    """
+    This function checks if the input spectrum has a 'retention_time' in its metadata.
+    If the input spectrum is None or doesn't have a 'retention_time', the function returns None.
+    Otherwise, it returns a clone of the input spectrum.
+
+    Parameters:
+    spectrum_in (SpectrumType): The input spectrum to check.
+
+    Returns:
+    SpectrumType: A clone of the input spectrum if it has a 'retention_time', None otherwise.
+    """
+    if spectrum_in is None:
+        return None
+
+    spectrum = spectrum_in.clone()
+
+    retention_time = spectrum.get("retention_time", None)
+    if retention_time is None:
+        return None
+    else:
+        return spectrum
