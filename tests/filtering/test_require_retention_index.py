1ccfe243c5286f02b028315030a52430d7468417
wverastegui <wudmir@gmail.com>
2024-01-05 08:40:46 +0100

    Implemented unit tests for RT & RI filters

---
+++ tests/filtering/test_require_retention_index.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pytest
+from testfixtures import LogCapture
+from matchms.filtering.metadata_processing.require_retention_index import \
+    require_retention_index
+from matchms.logging_functions import (reset_matchms_logger,
+                                       set_matchms_logger_level)
+from ..builder_Spectrum import SpectrumBuilder
+
+
+@pytest.mark.parametrize("metadata, expected", [
+    ({"retention_index": 1817}, True),
+])
+def test_require_retention_index_with_retention_index(metadata, expected):
+    """Test if function correctly handles spectrum with retention index."""
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
+
+    spectrum = require_retention_index(spectrum_in)
+
+    assert (spectrum is not None) == expected, "Expected a Spectrum object."
+    assert ("retention_index" in spectrum.metadata) == expected, "Expected the 'retention_index' key in the metadata."
+
+@pytest.mark.parametrize("metadata, expected", [
+    ({}, None),
+])
+def test_require_retention_index_without_retention_index(metadata, expected):
+    """Test if function correctly handles spectrum without retention index."""
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
+
+    spectrum = require_retention_index(spectrum_in)
+
+    assert spectrum is expected, "Expected None when retention index is missing."
+
+@pytest.mark.parametrize("spectrum_in, expected", [
+    (None, None),
+])
+def test_require_retention_index_with_input_none(spectrum_in, expected):
+    """Test if function correctly handles None input."""
+    spectrum = require_retention_index(spectrum_in)
+
+    assert spectrum is expected, "Expected None when input is None."
+
+
---
+++ tests/filtering/test_require_retention_time.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pytest
+from testfixtures import LogCapture
+from matchms.filtering.metadata_processing.require_retention_time import \
+    require_retention_time
+from matchms.logging_functions import (reset_matchms_logger,
+                                       set_matchms_logger_level)
+from ..builder_Spectrum import SpectrumBuilder
+
+
+@pytest.mark.parametrize("metadata, expected", [
+    ({"retention_time": 1817}, True),
+])
+def test_require_retention_time_with_retention_time(metadata, expected):
+    """Test if function correctly handles spectrum with retention time."""
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
+
+    spectrum = require_retention_time(spectrum_in)
+
+    assert (spectrum is not None) == expected, "Expected a Spectrum object."
+    assert ("retention_time" in spectrum.metadata) == expected, "Expected the 'retention_time' key in the metadata."
+
+@pytest.mark.parametrize("metadata, expected", [
+    ({}, None),
+])
+def test_require_retention_time_without_retention_time(metadata, expected):
+    """Test if function correctly handles spectrum without retention time."""
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
+
+    spectrum = require_retention_time(spectrum_in)
+
+    assert spectrum is expected, "Expected None when retention time is missing."
+
+@pytest.mark.parametrize("spectrum_in, expected", [
+    (None, None),
+])
+def test_require_retention_time_with_input_none(spectrum_in, expected):
+    """Test if function correctly handles None input."""
+    spectrum = require_retention_time(spectrum_in)
+
+    assert spectrum is expected, "Expected None when input is None."
+
+
