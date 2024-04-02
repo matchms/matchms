e59dc5515195f6c0a00f98b906661e496404d30b
niekdejonge <niekdejonge@gmail.com>
2024-04-02 18:24:11 +0200

    Add additional tests for require retention time

--- tests/filtering/test_require_retention_time.py
+++ tests/filtering/test_require_retention_time.py
@@ -4,8 +4,21 @@ from matchms.filtering.metadata_processing.require_retention_time import \
 from ..builder_Spectrum import SpectrumBuilder
 
 
+@pytest.mark.parametrize("metadata, minimum_rt, maximum_rt, expected", [
+    ({"retention_time": 1817}, 0, 2000, True),
+    ({"retention_time": 2100}, 0, 2000, False),
+    ({"retention_time": 100}, 200, 2000, False)])
+def test_require_retention_time_within_range(metadata, minimum_rt, maximum_rt, expected):
+    """Test if function correctly handles spectrum with retention time."""
+    spectrum_in = SpectrumBuilder().with_metadata(metadata).build()
+    spectrum = require_retention_time(spectrum_in, minimum_rt, maximum_rt)
+    assert (spectrum is not None) == expected, "Expected a Spectrum object."
+
+
 @pytest.mark.parametrize("metadata, expected", [
     ({"retention_time": 1817}, True),
+    ({"retention_time": 500.12}, True),
+    ({"retention_time": "500.12"}, False)
 ])
 def test_require_retention_time_with_retention_time(metadata, expected):
     """Test if function correctly handles spectrum with retention time."""
@@ -14,7 +27,9 @@ def test_require_retention_time_with_retention_time(metadata, expected):
     spectrum = require_retention_time(spectrum_in)
 
     assert (spectrum is not None) == expected, "Expected a Spectrum object."
-    assert ("retention_time" in spectrum.metadata) == expected, "Expected the 'retention_time' key in the metadata."
+    if spectrum is not None:
+        assert ("retention_time" in spectrum.metadata) == expected, "Expected the 'retention_time' key in the metadata."
+
 
 @pytest.mark.parametrize("metadata, expected", [
     ({}, None),
@@ -26,6 +41,7 @@ def test_require_retention_time_without_retention_time(metadata, expected):
     spectrum = require_retention_time(spectrum_in)
 
     assert spectrum is expected, "Expected None when retention time is missing."
+
 
 @pytest.mark.parametrize("spectrum_in, expected", [
     (None, None),
