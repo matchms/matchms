b97a3adf486ac7fea1eb9f67d89d437c97fc1035
niekdejonge <niekdejonge@gmail.com>
2024-04-08 11:13:17 +0200

    Change test

--- tests/filtering/test_repair_parent_mass_from_smiles/test_repair_parent_mass_is_mol_wt.py
+++ tests/filtering/test_repair_parent_mass_from_smiles/test_repair_parent_mass_is_mol_wt.py
@@ -5,12 +5,14 @@ from tests.builder_Spectrum import SpectrumBuilder
 
 
 @pytest.mark.parametrize("smiles, adduct, precursor_mz, parent_mass, expected_precursor_mz, expected_parent_mass",
-                         [("CN1CCCC1C2=CN=CC=C2", "[M+H]+", 163.23, 162.23, 163.122974447999988, 162.115698455),
-                          ("CN1CCCC1C2=CN=CC=C2", "[M+2H]2+", 82.12, 162.23, 82.065125224, 162.115698455),
-                          # Should not be changed
+                         [("CN1CCCC1C2=CN=CC=C2", "[M+H]+", 163.23, 162.23, 163.23, 162.115698455),
+                          ("CN1CCCC1C2=CN=CC=C2", "[M+2H]2+", 82.12, 162.23, 82.12, 162.115698455),
+                          # Should not be changed, since it is not close to the molecular weight
                           ("CN1CCCC1C2=CN=CC=C2", "[M+H]+", 10.0, 100.0, 10.0, 100.0),
                           # negative
-                          ("CN1CCCC1C2=CN=CC=C2", "[M-H]-", 161.23, 162.23, 161.108422448, 162.115698455),
+                          ("CN1CCCC1C2=CN=CC=C2", "[M-H]-", 161.23, 162.23, 161.23, 162.115698455),
+                          # not matching precursor mz (should still be changed
+                          ("CN1CCCC1C2=CN=CC=C2", "[M-H]-", 10, 162.23, 10, 162.115698455),
                           ])
 def test_repair_parent_mass_is_mol_wt(smiles, adduct, precursor_mz, parent_mass, expected_precursor_mz, expected_parent_mass):
     # pylint: disable=too-many-arguments
