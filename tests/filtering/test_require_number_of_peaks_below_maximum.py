73641d9855e25c4497b25c2bcbd2aa89babcfc5c
niekdejonge <niekdejonge@gmail.com>
2024-05-27 15:26:17 +0200

    Add test

---
+++ tests/filtering/test_require_number_of_peaks_below_maximum.py
@@ -0,0 +1,19 @@
+from matchms.Spectrum import Spectrum
+from matchms.filtering.peak_processing.require_number_of_peaks_below_maximum import \
+    require_number_of_peaks_below_maximum
+import numpy as np
+
+
+def test_require_number_of_peaks_below_maximum():
+    mz = np.array([10, 20, 30, 40], dtype="float")
+    intensities = np.array([0, 1, 10, 100], dtype="float")
+    spectrum = Spectrum(mz, intensities)
+    assert require_number_of_peaks_below_maximum(spectrum, 3) is None
+
+
+def test_require_number_of_peaks_below_maximum_not_removed():
+    mz = np.array([10, 20, 30, 40], dtype="float")
+    intensities = np.array([0, 1, 10, 100], dtype="float")
+    spectrum = Spectrum(mz, intensities)
+    assert require_number_of_peaks_below_maximum(spectrum, 10) == spectrum
+
