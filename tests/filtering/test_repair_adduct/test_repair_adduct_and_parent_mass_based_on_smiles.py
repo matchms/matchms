ed4099223b6a659146e816331673fedb15aac1a6
niekdejonge <niekdejonge@gmail.com>
2024-06-04 17:51:19 +0200

    Remove unused parameter to fix linting

--- tests/filtering/test_repair_adduct/test_repair_adduct_and_parent_mass_based_on_smiles.py
+++ tests/filtering/test_repair_adduct/test_repair_adduct_and_parent_mass_based_on_smiles.py
@@ -34,18 +34,18 @@ def test_repair_adduct_and_parent_mass_based_on_smiles(precursor_mz, expected_ad
         assert abs(spectrum_out.get("parent_mass") - 15.9589) < 0.1
 
 
-@pytest.mark.parametrize("precursor_mz, parent_mass, adduct, expected_parent_mass, expected_adduct, ionmode",
+@pytest.mark.parametrize("precursor_mz, parent_mass, adduct, expected_parent_mass, expected_adduct",
                          [
                              # Normal repair
-                             (17.0, 17.0, "[M+H]+", 16.031300127999998, "[M+H]+", "positive"),
+                             (17.0, 17.0, "[M+H]+", 16.031300127999998, "[M+H]+"),
                              # Parent mass should not be repaired if close enough to smiles mass
-                             (17.0, 16.0, "[M+H]+", 16.0, "[M+H]+", "positive"),
+                             (17.0, 16.0, "[M+H]+", 16.0, "[M+H]+"),
                              # Parent mass is incorrect, but no adduct is available
-                             (19.0, 20.0, "[M+H]+", 20.0, "[M+H]+", "positive"),
+                             (19.0, 20.0, "[M+H]+", 20.0, "[M+H]+"),
                          ])
 def test_repair_adduct_and_parent_mass_based_on_smiles_correct_parent_mass(precursor_mz, parent_mass, adduct,
                                                                            expected_parent_mass,
-                                                                           expected_adduct, ionmode):
+                                                                           expected_adduct):
     pytest.importorskip("rdkit")
 
     # CH4 is used as smiles, this has a mass of 16
@@ -53,7 +53,7 @@ def test_repair_adduct_and_parent_mass_based_on_smiles_correct_parent_mass(precu
                                                    "adduct": adduct,
                                                    "precursor_mz": precursor_mz,
                                                    "parent_mass": parent_mass,
-                                                   "ionmode": ionmode}).build()
+                                                   "ionmode": "positive"}).build()
     spectrum_out = repair_adduct_and_parent_mass_based_on_smiles(spectrum_in, mass_tolerance=0.1)
     assert spectrum_out.get("adduct") == expected_adduct
     assert spectrum_out.get("parent_mass") == expected_parent_mass
