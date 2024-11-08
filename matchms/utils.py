693667dc1b2da0a729ad0fb241d8ac2ece677c91
Julian Pollmann <julian.pollmann@hs-duesseldorf.de>
2024-11-08 21:33:55 +0100

    Improved docstring

--- matchms/utils.py
+++ matchms/utils.py
@@ -159,8 +159,9 @@ def to_camel_case(snake_str: str) -> str:
     snake_str
         The input string in snake_case
 
-    Returns:
-        camelCase string.
+    Returns
+    -------
+    camelCased str
     """
     components = snake_str.split('_')
     return components[0] + ''.join(word.capitalize() for word in components[1:])
