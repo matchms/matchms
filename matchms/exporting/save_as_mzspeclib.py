719b8d4e0bc4641eabf4bcd0b6927e8f6b9c7135
hechth <helge.hecht@recetox.muni.cz>
2025-03-11 18:55:21 +0100

    added basic logic to write spectra

--- matchms/exporting/save_as_mzspeclib.py
+++ matchms/exporting/save_as_mzspeclib.py
@@ -3,9 +3,27 @@ from matchms.Spectrum import Spectrum
 import os
 
 def save_as_mzspeclib(spectra: List[Spectrum], filename: str):
+
+    with open(filename, 'w') as file:
+        _write_header(filename, file)
+        for idx, spectrum in enumerate(spectra):
+            _write_spectrum(file, idx, spectrum)
+
+
+def _write_spectrum(file, idx, spectrum):
+    print(f'<Spectrum={idx + 1}>', file=file)
+    print(f'MS:1003059|number of peaks={len(spectrum.peaks)}', file=file)
+    print(f'<Peaks>', file=file)
+    for i in range(len(spectrum.peaks)):
+        print(f'{spectrum.peaks.mz[i]}\t{spectrum.peaks.intensities[i]}\t?', file=file)
+
+
+def _write_header(filename, file):
     basename, ext = os.path.splitext(filename)
     name = basename.split(os.path.sep)[-1]
-    with open(filename, 'w') as file:
-        print('<mzSpecLib>', file=file)
-        print('MS:1003186|library format version=1.0', file=file)
-        print(f'MS:1003188|library name={name}', file=file)
+    print('<mzSpecLib>', file=file)
+    print('MS:1003186|library format version=1.0', file=file)
+    print(f'MS:1003188|library name={name}', file=file)
+    print('<AttributeSet Spectrum=all>', file=file)
+    print('<AttributeSet Analyte=all>', file=file)
+    print('<AttributeSet Interpretation=all>', file=file)
--- tests/exporting/test_save_as_mzspeclib.py
+++ tests/exporting/test_save_as_mzspeclib.py
@@ -5,12 +5,11 @@ import os
 from matchms.exporting import save_as_mzspeclib
 
 def test_creates_file(tmp_path):
-    spectrum = SpectrumBuilder().with_mz(np.array([10, 20, 30], dtype=np.float32)).with_intensities(np.array([100, 150, 80], dtype=np.float32)).build()
     outpath = tmp_path / "test.mzspeclib"
-    save_as_mzspeclib([spectrum], outpath)
+    save_as_mzspeclib([], outpath)
     assert os.path.isfile(outpath)
 
-def test_library_name(tmp_path):
+def test_has_header(tmp_path):
     name = 'test'
     outpath = tmp_path / f"{name}.mzspeclib"
     save_as_mzspeclib([], outpath)
@@ -21,4 +20,19 @@ def test_library_name(tmp_path):
         assert lines[0] == '<mzSpecLib>\n'
         assert lines[1] == 'MS:1003186|library format version=1.0\n'
         assert lines[2] == 'MS:1003188|library name=test\n'
+        assert lines[3] == '<AttributeSet Spectrum=all>\n'
+        assert lines[4] == '<AttributeSet Analyte=all>\n'
+        assert lines[5] == '<AttributeSet Interpretation=all>\n'
+
+def test_has_spectrum(tmp_path):
+    spectrum = SpectrumBuilder().with_mz(np.array([10, 20, 30], dtype=np.float32)).with_intensities(np.array([100, 150, 80], dtype=np.float32)).build()
+    outpath = tmp_path / "test.mzspeclib"
+    save_as_mzspeclib([spectrum], outpath)
+
+    with open(outpath, 'r') as file:
+        lines = file.readlines()
+        assert '<Spectrum=1>\n' in lines
+        assert '<Peaks>\n' in lines
+        assert len(lines) == 12
+ 
 
