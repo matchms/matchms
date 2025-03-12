2d8e783565a47493fe3bb2946f1bd7a835a9fad0
hechth <helge.hecht@recetox.muni.cz>
2025-03-12 18:01:00 +0100

    Added further test data and implemented peak reading

--- matchms/importing/load_from_mzspeclib.py
+++ matchms/importing/load_from_mzspeclib.py
@@ -1,2 +1,60 @@
-def load_from_mzspeclib():
-    pass
+from typing import Generator, List
+
+import numpy as np
+from matchms.Spectrum import Spectrum
+import re
+
+SPECTRUM_ATTRIBUTE_MAPPING = {
+    'MS:1003061|library spectrum name': 'compound_name',
+    'MS:1003208|experimental precursor monoisotopic m/z': 'precursor_mz',
+    'MS:1003059|number of peaks': 'num peaks',
+    'MS:1000866|molecular formula': 'formula',
+    'MS:1002894|InChIKey': 'inchikey',
+    'MS:1003403|InChI': 'inchi',
+}
+
+def load_from_mzspeclib(filename: str) -> Generator[Spectrum, None, None]:
+    spectrum_id = None
+
+    with open(filename, 'r', encoding='UTF-8') as file:
+        for line in file:
+            match = re.compile(r'<Spectrum=(\d+)>').search(line)
+            if match:
+                if spectrum_id is not None:
+                    yield Spectrum(
+                        np.array(mzs, dtype=np.float32),
+                        np.array(intensities, dtype=np.float32),
+                        params)
+                spectrum_id = int(match.group(1))
+                mzs = []
+                intensities = []
+                comments = []
+                params = {}
+                continue
+
+            if spectrum_id is not None:
+                if '<Peaks>' in line:
+                    num_peaks = int(params['num peaks'])
+                    for i in range(num_peaks):
+                        line = file.readline()
+                        mz, intensity, comment = line.split('\t')
+
+                        mzs.append(float(mz))
+                        intensities.append(float(intensity))
+                        comments.append(comment)
+
+                accession = line.split('|')[0]
+                if accession == 'MS:1000868':
+                    continue
+                tokens = line.split('=')
+                if len(tokens) == 2:
+                    term, value = tokens
+                    if term in SPECTRUM_ATTRIBUTE_MAPPING:
+                        params[SPECTRUM_ATTRIBUTE_MAPPING[term]] = _sanitize(value)
+    yield Spectrum(
+        np.array(mzs, dtype=np.float32),
+        np.array(intensities, dtype=np.float32),
+        params)
+
+def _sanitize(value:str) -> str:
+    return value.strip().strip('\n').strip('\t')
--- tests/importing/test_load_from_mzspeclib.py
+++ tests/importing/test_load_from_mzspeclib.py
@@ -1,4 +1,52 @@
+import os
+from types import GeneratorType
+
+import numpy as np
+import pytest
+from matchms.Spectrum import Spectrum
 from matchms.importing import load_from_mzspeclib
 
-def test_dummy():
-    load_from_mzspeclib()
+
+module_root = os.path.join(os.path.dirname(__file__), "..")
+
+@pytest.fixture
+def testdata():
+    return os.path.join(module_root, 'testdata','rcx_gc-ei_ms_20201028_perylene.mzspeclib')
+
+@pytest.fixture
+def spectrum(testdata) -> Spectrum:
+    return list(load_from_mzspeclib(testdata))[0]
+
+def test_is_generator(testdata):
+    actual = load_from_mzspeclib(testdata)
+    assert isinstance(actual, GeneratorType)
+
+
+def test_has_spectrum(testdata):
+    actual = list(load_from_mzspeclib(testdata))
+    assert len(actual) == 1
+
+
+def test_spectrum_has_name(spectrum):
+    assert spectrum.get('compound_name') == 'Perylene'
+
+
+def test_spectrum_has_correct_num_peaks(spectrum):
+    assert len(spectrum.peaks) == 19
+    assert spectrum.get('num_peaks') == '19'
+
+def test_spectrum_has_correct_mzs(spectrum):
+    actual = spectrum.peaks.mz
+    expected = np.array([
+        112.03071,113.03854,124.03076,124.53242,125.03855,
+        125.54019,126.04636,126.54804,222.04645,224.06192,
+        226.04175,246.04646,248.06204,249.07072,250.07765,
+        251.07967,252.09323,253.09656,254.09985
+    ], dtype=np.float32)
+
+    assert np.array_equal(actual, expected)
+
+
+def test_has_multiple_spectra():
+    multi_spectra_file = os.path.join(module_root, 'testdata','rcx_ei_mzspeclib')
+    assert len(list(load_from_mzspeclib(multi_spectra_file))) == 10
