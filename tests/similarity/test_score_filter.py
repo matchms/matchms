777806d1b55add2ea7a57803f0cc41b1bba84333
niekdejonge <niekdejonge@gmail.com>
2025-03-18 14:41:24 +0100

    Added tests for score filter

---
+++ tests/similarity/test_score_filter.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pytest
+
+from matchms.similarity.ScoreFilter import FilterScoreByValue
+
+
+@pytest.mark.parametrize(
+    "value, operator, score, expected",
+    [[0.0, ">", np.array(1.0), True],
+     [1.0, "<", np.array(3.0), False],
+     [2.0, "<", np.array(1.0), True],
+     [2.0, "==", np.array(1.0), False],
+     [2.0, "!=", np.array(1.0), True],
+     [1, "!=", np.array(1.0), False],
+     [2.0, ">=", np.array(2.0), True],
+     [2.0, "<=", np.array(2.0), True],
+     [-1.0, "<", np.asarray((float(0), 0), dtype=[("score", np.float64), ("matches", "int")]), False],
+     [-1.0, ">", np.asarray((float(0), 0), dtype=[("score", np.float64), ("matches", "int")]), True],
+     ])
+def test_filter_score_by_range(value, operator, score, expected):
+    score_filter = FilterScoreByValue(value, operator)
+    should_be_stored = score_filter.keep_score(np.array(score))
+    assert expected == should_be_stored
+
+@pytest.mark.parametrize(
+    "value, operator, score, expected, score_name",
+    [[1.0, ">", np.asarray((float(2.0), 0), dtype=[("score", np.float64), ("matches", "int")]), False, "matches"],
+     [1.0, ">", np.asarray((float(2.0), 0), dtype=[("score", np.float64), ("matches", "int")]), True, "score"],
+     ])
+def test_filter_score_by_range_other_type(value, operator, score, expected, score_name):
+    score_filter = FilterScoreByValue(value, operator, score_name)
+    should_be_stored = score_filter.keep_score(np.array(score))
+    assert expected == should_be_stored
