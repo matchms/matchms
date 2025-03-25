Added additional tests for testing correct masking and filtering for all SimilarityScores

tests/test_scoring_method_consistency.py
@@ -15,7 +15,7 @@ from matchms import Pipeline
 from matchms.importing import load_from_json
 from matchms.Pipeline import create_workflow
 from matchms.similarity.COOIndex import COOIndex
-
+from matchms.similarity.ScoreFilter import FilterScoreByValue
 
 module_root = os.path.dirname(__file__)
 json_file = os.path.join(module_root, "testdata", "gnps_spectra.json")
@@ -23,7 +23,9 @@ json_file = os.path.join(module_root, "testdata", "gnps_spectra.json")
 _score_functions = {
     "cosinegreedy": [mssimilarity.CosineGreedy, {}],
     "cosinehungarian": [mssimilarity.CosineHungarian, {}],
-    #"parentmassmatch": [mssimilarity.ParentMassMatch, {}],
+    # "FingerprintSimilarity": [mssimilarity.FingerprintSimilarity, {}],
+    "IntersectMZ": [mssimilarity.IntersectMz, {}],
+    "parentmassmatch": [mssimilarity.ParentMassMatch, {}],
     "precursormzmatch": [mssimilarity.PrecursorMzMatch, {}],
     "metadatamatch": [mssimilarity.MetadataMatch, {"field": "spectrum_status"}],
     "modifiedcosine": [mssimilarity.ModifiedCosine, {}]}
@@ -97,3 +99,38 @@ def test_consistency_scoring_and_pipeline(spectra, similarity_measure):
     else:
         assert np.allclose(pipeline.scores.to_array(pipeline.scores.score_names[0]),
             computed_scores_matrix[computed_scores_matrix.dtype.names[0]])
+
+@pytest.mark.parametrize("similarity_measure", list(_score_functions.values()))
+def test_matrix_masking_and_filtering(spectra, similarity_measure):
+
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=())
+    computed_scores_matrix_no_mask_no_filter = scoring_method.matrix(spectra, spectra, is_symmetric=False, mask_indices=None)
+
+    filters = (FilterScoreByValue(value=0.2, operator=">"), FilterScoreByValue(value=0.8, operator="<"))
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=filters)
+    computed_scores_matrix_no_mask_with_filter = scoring_method.matrix(spectra, spectra, is_symmetric=False, mask_indices=None)
+
+    mask_indices = COOIndex(np.asarray([0, 1]), np.asarray([0, 1]))
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=filters)
+    computed_scores_matrix_with_mask_with_filters = scoring_method.matrix(spectra, spectra, is_symmetric=False, mask_indices=mask_indices)
+
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=())
+    computed_scores_matrix_with_mask_without_filters = scoring_method.matrix(spectra, spectra, is_symmetric=False, mask_indices=mask_indices)
+
+    # todo add a test to check that the values are correct
+
+@pytest.mark.parametrize("similarity_measure", list(_score_functions.values()))
+def test_sparse_array_masking_and_filtering(spectra, similarity_measure):
+
+    filters = (FilterScoreByValue(value=0.2, operator=">"), FilterScoreByValue(value=0.8, operator="<"))
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=filters)
+    computed_scores_matrix_no_mask_with_filter = scoring_method.sparse_array(spectra, spectra, is_symmetric=False, mask_indices=None)
+
+    mask_indices = COOIndex(np.asarray([0, 1]), np.asarray([0, 1]))
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=filters)
+    computed_scores_matrix_with_mask_with_filters = scoring_method.sparse_array(spectra, spectra, is_symmetric=False, mask_indices=mask_indices)
+
+    scoring_method = similarity_measure[0](**similarity_measure[1], score_filters=())
+    computed_scores_matrix_with_mask_without_filters = scoring_method.sparse_array(spectra, spectra, is_symmetric=False, mask_indices=mask_indices)
+
+    # todo add a test to check that the values are correct