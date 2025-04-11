cfc047667214604f2892152b16df0b1ba42d5036
Julian Pollmann <julian.pollmann@hs-duesseldorf.de>
2025-04-11 14:05:02 +0200

    Test github format

--- matchms/Fingerprints.py
+++ matchms/Fingerprints.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from matchms.typing import SpectrumType
 from typing import Optional
 import numpy as np
 import pandas as pd
@@ -7,7 +8,7 @@ from rdkit import Chem, DataStructs
 from rdkit.Chem.rdchem import Mol
 from rdkit.Chem.rdFingerprintGenerator import GetMorganGenerator, GetRDKitFPGenerator
 from matchms.filtering.filter_utils.smile_inchi_inchikey_conversions import is_valid_inchi, is_valid_inchikey, is_valid_smiles
-from matchms.typing import SpectrumType
+
 from .utils import to_camel_case
 
 
