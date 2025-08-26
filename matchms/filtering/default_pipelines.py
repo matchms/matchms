47902c5d32d2714e55d1f22d9f0ecb083851d6eb
Florian Huber <florian.huber@hs-duesseldorf.de>
2025-08-26 18:26:37 +0200

    add to CLEAN_PEAKS default filters

--- matchms/filtering/default_pipelines.py
+++ matchms/filtering/default_pipelines.py
@@ -54,6 +54,7 @@ REQUIRE_COMPLETE_ANNOTATION = [
 CLEAN_PEAKS = [
     (msfilters.select_by_mz, {"mz_from": 0, "mz_to": 1000}),
     (msfilters.select_by_relative_intensity, {"intensity_from": 0.001}),
+    msfilters.remove_peaks_relative_to_precursor_mz,
     (msfilters.reduce_to_number_of_peaks, {"n_max": 1000}),
     (msfilters.require_minimum_number_of_high_peaks, {"no_peaks": 5, "intensity_percent": 2.0}),
     msfilters.remove_profiled_spectra,
