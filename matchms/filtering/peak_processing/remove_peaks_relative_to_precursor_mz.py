a92b695d571b94e6571202db140b7db00ec456fb
Florian Huber <florian.huber@hs-duesseldorf.de>
2025-08-26 17:06:03 +0200

    fix and update docstring and parameter name

--- matchms/filtering/peak_processing/remove_peaks_relative_to_precursor_mz.py
+++ matchms/filtering/peak_processing/remove_peaks_relative_to_precursor_mz.py
@@ -5,18 +5,21 @@ from matchms.typing import SpectrumType
 
 
 def remove_peaks_relative_to_precursor_mz(
-    spectrum_in: SpectrumType, relative_to_precursor: float = -1.6,
+    spectrum_in: SpectrumType, offset_to_precursor: float = -1.6,
     clone: Optional[bool] = True
 ) -> Optional[SpectrumType]:
-    """Remove peaks that are within mz_tolerance (in Da) of
-       the precursor mz, exlcuding the precursor peak.
+    """Remove all peaks with m/z values > precursor-m/z + offset_to_precursor.
+
+    If offset_to_precursor is negative, this means that all peaks
+    with m/z values greater than (precursor_mz - |offset_to_precursor|).
+    If offset_to_precursor is positive, the precursor_mz peak itself will remain.
 
     Parameters
     ----------
     spectrum_in:
         Input spectrum.
-    relative_to_precursor:
-        All peaks with mz values > precursor_mz + relative_to_precursor will be removed.
+    offset_to_precursor:
+        All peaks with mz values > precursor_mz + offset_to_precursor will be removed.
         Default is -1.6 Da based Flash Entropy article by Li and Fiehn, 2023, Nat. Comm.
         (see https://www.nature.com/articles/s41592-023-02012-9)
     clone:
@@ -42,7 +45,7 @@ def remove_peaks_relative_to_precursor_mz(
             )
 
     mzs, intensities = spectrum.peaks.mz, spectrum.peaks.intensities
-    peaks_to_remove = mzs > (precursor_mz + relative_to_precursor)
+    peaks_to_remove = mzs > (precursor_mz + offset_to_precursor)
     new_mzs, new_intensities = mzs[~peaks_to_remove], intensities[~peaks_to_remove]
     spectrum.peaks = Fragments(mz=new_mzs, intensities=new_intensities)
 
