b89891f8617ace39bd9bd3006e8dc81341266cb7
Adriano Rutz <44283913+Adafede@users.noreply.github.com>
2026-01-25 08:00:33 +0100

    Update test_spectrum_plots.py

--- tests/plotting/test_spectrum_plots.py
+++ tests/plotting/test_spectrum_plots.py
@@ -2,11 +2,11 @@ import matplotlib
 import numpy as np
 import pytest
 
-
-matplotlib.use("Agg")
 from matplotlib import pyplot as plt
 from matchms import Spectrum
 from matchms.plotting import plot_spectra_array, plot_spectra_mirror, plot_spectrum
+
+matplotlib.use("Agg")
 
 
 def _assert_fig_ok(fig, n_plots, dpi, height):
