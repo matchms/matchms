18760b0c04ef981d078f76f53557058fbeceaad8
niekdejonge <niekdejonge@gmail.com>
2026-03-04 09:57:00 +0100

    fix test fingerprint similarity

--- tests/similarity/test_fingerprint_similarity.py
+++ tests/similarity/test_fingerprint_similarity.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-from sparsestack import StackedSparseArray
 from matchms import Spectrum
 from matchms.similarity import FingerprintSimilarity
 
@@ -64,39 +63,4 @@ def test_fingerprint_similarity_parallel(test_method, expected_score, set_empty)
     expected_matrix = np.array(
         [[set_empty, set_empty, set_empty], [set_empty, 1, expected_score], [set_empty, expected_score, 1]]
     )
-    if isinstance(score_matrix, (StackedSparseArray)):
-        score_matrix = score_matrix.to_array()
     assert np.allclose(score_matrix, expected_matrix, equal_nan=True), "Expected different values."
-
-
-def test_fingerprint_similarity_with_scores_sorting():
-    """Test if score works with Scores.scores_by_query and sorting."""
-    spectrum0 = Spectrum(
-        mz=np.array([100.0, 101.0], dtype="float"), intensities=np.array([0.4, 0.5], dtype="float"), metadata={}
-    )
-
-    fingerprint1 = np.array([0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0])
-    spectrum1 = Spectrum(
-        mz=np.array([100.0, 101.0], dtype="float"),
-        intensities=np.array([0.4, 0.5], dtype="float"),
-        metadata={"fingerprint": fingerprint1},
-    )
-
-    fingerprint2 = np.array([0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1])
-    spectrum2 = Spectrum(
-        mz=np.array([100.0, 101.0], dtype="float"),
-        intensities=np.array([0.4, 0.5], dtype="float"),
-        metadata={"fingerprint": fingerprint2},
-    )
-
-    similarity_measure = FingerprintSimilarity(set_empty_scores=0, similarity_measure="cosine")
-
-    scores = create_scores_object_and_calculate_scores(
-        [spectrum0, spectrum1, spectrum2], [spectrum0, spectrum1, spectrum2], similarity_measure
-    )
-
-    scores_by_ref_sorted = scores.scores_by_query(spectrum1, sort=True)
-    expected_scores = np.array([1.0, 0.84515425])
-    assert np.allclose(np.array([x[1] for x in scores_by_ref_sorted]), expected_scores, atol=1e-6), (
-        "Expected different scores and/or order."
-    )
