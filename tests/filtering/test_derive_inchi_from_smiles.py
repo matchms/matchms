ece1f49eff744e2311c62d4ec6d7171fe4071f37
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-05-25 16:07:38 +0200

    add new test case

--- tests/filtering/test_derive_inchi_from_smiles.py
+++ tests/filtering/test_derive_inchi_from_smiles.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import pytest
+from matchms import SpectraCollection
 from matchms.filtering import derive_inchi_from_smiles
 from tests.builder_Spectrum import SpectrumBuilder
 from tests.run_spectrum_and_collection import run_filter_as_spectrum_or_collection
@@ -75,3 +77,18 @@ def test_derive_inchi_from_smiles_empty_spectrum():
     spectrum = derive_inchi_from_smiles(None)
 
     assert spectrum is None, "Expected different handling of None spectrum."
+
+
+@pytest.mark.parametrize("missing_value", [None, np.nan, pd.NA])
+def test_derive_inchi_from_smiles_collection_handles_missing_inchi_values(missing_value):
+    spectrum_in = (
+        SpectrumBuilder()
+        .with_metadata({"smiles": "C1CCCCC1", "inchi": missing_value})
+        .build()
+    )
+
+    collection = SpectraCollection([spectrum_in])
+    processed = derive_inchi_from_smiles(collection)
+
+    inchi = processed.metadata.loc[0, "inchi"]
+    assert inchi.replace('"', "") == "InChI=1S/C6H12/c1-2-4-6-5-3-1/h1-6H2"
