b4d4417f1d6e9431a419adeb7c8d7a60bec127d6
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-08-27 13:26:10 +0200

    add central default parameters file

---
+++ matchms/similarity/default_parameters.py
@@ -0,0 +1,11 @@
+"""Default settings shared by similarity implementations."""
+
+import numpy as np
+
+
+DEFAULT_MZ_TOLERANCE = 0.01
+DEFAULT_MZ_POWER = 0.0
+DEFAULT_INTENSITY_POWER = 1.0
+DEFAULT_NOISE_CUTOFF = 0.01
+DEFAULT_USE_PPM = False
+DEFAULT_DTYPE = np.float64
