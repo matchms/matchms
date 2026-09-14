d3e7d98c30db6fba6f79b67a7934d316ccd9522a
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-09-14 16:30:47 +0200

    expand integration test

--- integration-tests/test_clean_peaks_workflow.py
+++ integration-tests/test_clean_peaks_workflow.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-from matchms import Pipeline
 from matchms.importing import load_ms2_dataset, load_from_mgf
 from matchms import SpectraProcessor
 from matchms.filtering.default_pipelines import BASIC_FILTERS, CLEAN_PEAKS, REPAIR_ANNOTATION
@@ -36,3 +35,21 @@ def test_clean_peaks_workflow_on_collection_and_spectra_list():
     # check report
     df = report.to_dataframe()
     assert df.shape == (25, 5)  # Adjust when filter pipelines change in matchms
+
+    # store cleaned spectra to file
+    output_file = os.path.join(module_root, "tests", "testdata", "pesticides_cleaned.mgf")
+    processed_collection.to_mgf(output_file)
+
+    # load again and check that the number of spectra and peaks is the same
+    reloaded_collection = load_ms2_dataset(output_file)
+    assert reloaded_collection.n_spectra == processed_collection.n_spectra == 58
+
+
+    # run processor on collection again...
+    processor = SpectraProcessor(
+        REPAIR_ANNOTATION
+        )
+    report = processor.create_processing_report()
+    _ = processor.process_collection(
+        reloaded_collection,
+    )
